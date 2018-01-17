6fbd972f770338d83917109a7bee18036072dee7
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-01-17 12:33:52 +0200

    # Updated ix-module. # Updated web3 init based on new ixo-module.

--- src/client/redux/web3/web3_action_creators.ts
+++ src/client/redux/web3/web3_action_creators.ts
@@ -1,38 +1,19 @@
 import {createAction} from "../../../lib/redux_utils/actions";
 import {INIT__WEB3__FAILURE, INIT__WEB3__INIT, INIT__WEB3__SUCCESS} from "./web3_actions";
 
-var loadjs = require('loadjs');
-declare const Web3;
+import Ixo from "/Users/nicolaasvercuiel/repos/ixo-module";
+import {ICredentialProviderResult} from "../../../../types/models";
 
 export function initializeWeb3() {
-  return dispatch => {
-    dispatch(createAction<INIT__WEB3__INIT>(INIT__WEB3__INIT.type, {}));
-
-    loadjs('https://cdn.rawgit.com/ethereum/web3.js/develop/dist/web3.min.js', 'web3');
-    loadjs.ready('web3', {
-      success: function () {
-        window.addEventListener('load', function () {
-          var web3 = window['web3'];
-          // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-          if (typeof web3 !== 'undefined') {
-            // Use Mist/MetaMask's provider.
-            var web3Instance = new Web3(web3.currentProvider);
-            dispatch(createAction<INIT__WEB3__SUCCESS>(INIT__WEB3__SUCCESS.type, {web3Instance: web3Instance}))
-          } else {
-            // Fallback to localhost if no web3 injection. We've configured this to
-            // use the development console's port by default.
-            var web3Instance = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'));
-            dispatch(createAction<INIT__WEB3__SUCCESS>(INIT__WEB3__SUCCESS.type, {web3Instance: web3Instance}))
-          }
-        });
-      },
-      error  : function (depsNotFound) {
-        console.error(depsNotFound);
-        dispatch(createAction<INIT__WEB3__FAILURE>(INIT__WEB3__FAILURE.type, {error: depsNotFound}))
-      }
-    });
-
-  }
+    return dispatch => {
+        dispatch(createAction<INIT__WEB3__INIT>(INIT__WEB3__INIT.type, {}));
+        var ixo = new Ixo('https://arcane-stream-64697.herokuapp.com/api/network');
+        ixo.auth.getCredentialProvider(window['web3'].currentProvider).then((result: ICredentialProviderResult) => {
+            dispatch(createAction<INIT__WEB3__SUCCESS>(INIT__WEB3__SUCCESS.type, {web3Instance: result.credentialProviderInstance}))
+        }).catch((error => {
+            dispatch(createAction<INIT__WEB3__FAILURE>(INIT__WEB3__FAILURE.type, {error: error}))
+        }));
+    }
 }
 
 
