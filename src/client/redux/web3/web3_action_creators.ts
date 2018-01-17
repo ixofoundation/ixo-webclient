de4740a24235b62b25de2cbd242d72defe0adc08
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-01-17 12:39:14 +0200

    # Fixed ping call to use new module init.

--- src/client/redux/web3/web3_action_creators.ts
+++ src/client/redux/web3/web3_action_creators.ts
@@ -1,13 +1,13 @@
 import {createAction} from "../../../lib/redux_utils/actions";
 import {INIT__WEB3__FAILURE, INIT__WEB3__INIT, INIT__WEB3__SUCCESS} from "./web3_actions";
 
-import Ixo from "/Users/nicolaasvercuiel/repos/ixo-module";
+import Ixo from "ixo-module";
 import {ICredentialProviderResult} from "../../../../types/models";
 
 export function initializeWeb3() {
     return dispatch => {
         dispatch(createAction<INIT__WEB3__INIT>(INIT__WEB3__INIT.type, {}));
-        var ixo = new Ixo('https://arcane-stream-64697.herokuapp.com/api/network');
+        var ixo = new Ixo('https://ixo-node.herokuapp.com');
         ixo.auth.getCredentialProvider(window['web3'].currentProvider).then((result: ICredentialProviderResult) => {
             dispatch(createAction<INIT__WEB3__SUCCESS>(INIT__WEB3__SUCCESS.type, {web3Instance: result.credentialProviderInstance}))
         }).catch((error => {
@@ -15,6 +15,9 @@ export function initializeWeb3() {
         }));
     }
 }
+
+
+
 
 
 
