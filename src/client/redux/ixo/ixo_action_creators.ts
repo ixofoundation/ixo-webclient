40bc349b7d7cbb2f8ab3b20e14f515ee655af3f8
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-01-31 22:12:04 +0200

    # Updated to latest version of the ixo-module.

--- src/client/redux/ixo/ixo_action_creators.ts
+++ src/client/redux/ixo/ixo_action_creators.ts
@@ -1,11 +1,11 @@
-import {createAction}                                                  from '../../../lib/redux_utils/actions';
-import {IXO__CREATE__FAILURE, IXO__CREATE__INIT, IXO__CREATE__SUCCESS} from './ixo_actions';
-import Ixo                                                             from 'ixo-module';
+import { createAction } from '../../../lib/redux_utils/actions';
+import { IXO__CREATE__FAILURE, IXO__CREATE__INIT, IXO__CREATE__SUCCESS } from './ixo_actions';
+import { Ixo } from 'ixo-module';
 
 export function initIxo(hostName: string) {
     var ixo = new Ixo(hostName);
     return dispatch => {
-        dispatch(createAction<IXO__CREATE__INIT>(IXO__CREATE__INIT.type, {hostName}));
+        dispatch(createAction<IXO__CREATE__INIT>(IXO__CREATE__INIT.type, { hostName }));
         if (ixo) {
             dispatch(
                 createAction<IXO__CREATE__SUCCESS>(IXO__CREATE__SUCCESS.type, {
