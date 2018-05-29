78e6f8221a3dddca16088116e3b38a67110a4712
donnierisk <donnierisk@gmail.com>
2018-05-29 16:20:14 +0200

    projects list done

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -19,13 +19,30 @@ const Owner = styled.p`
 	margin-bottom: 15px;
 `;
 
-// const EllipseText = styled.p`
-//     white-space: nowrap;
-//     text-overflow: ellipsis;
-//     overflow: hidden;
-//     width: '100%';
-//     font-weight:100;
-// `;
+const Description = styled.div`
+	height: 100%;
+    width: 100%;
+    background: rgba(0,0,0,0.5);
+    opacity: 0;
+    margin: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    padding: 40px 20px 10px;
+	text-align: left;
+	transition: opacity 0.5s ease;
+
+	p {
+		font-size: 13px;
+		color: white;
+		position:relative;
+		top: -15px;
+
+		transition: top 0.6s ease;
+	}
+`;
 
 const Progress = styled.p`
 	font-size: 23px;
@@ -37,14 +54,17 @@ const Impact = styled.p`
 	font-size: 13px;
 `;
 
-const CardTop = styled.div`
-	border-radius:2px 2px 0 0;
-	padding: 10px;
-	height: 170px;
+const SDGs = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 	align-content: flex-start;
 	justify-content: flex-end;
+`;
+
+const CardTop = styled.div`
+	border-radius:2px 2px 0 0;
+	padding: 10px;
+	height: 170px;
 	box-shadow: 0 8px 16px -2px rgba(0,0,0,0.03);
 	background-size: cover;
 	background-repeat: no-repeat;
@@ -54,11 +74,12 @@ const CardTop = styled.div`
 		content: "";
 		position: absolute;
 		width: 100%;
-		height: 60px;
+		height: 33%;
 		top: 0;
 		left: 0;
 		background: linear-gradient(180deg,rgba(0,0,0,0.33) 0%,rgba(0,0,0,0) 100%);
 	}
+	
 
 	i {
 		position: relative;
@@ -78,24 +99,35 @@ const CardBottom = styled.div`
 	border: 1px solid #E2E2E2;
 	border-top: 0;
 	background: white;
-`;
 
-const CardContainer = styled.div`
-
-	margin-bottom: 34px;
-
-    & p {
+	& p {
         font-weight:100;
 		color: ${props => props.theme.fontDarkGrey};
     }
 `;
 
-const ProjectLink = styled(Link) `
-    transition:background 0.3s ease;
+const CardContainer = styled.div`
+	margin-bottom: 34px;
+`;
 
-    &:hover {
-        text-decoration:none;
-    }
+const ProjectLink = styled(Link) `
+	display: block;
+	box-shadow: 0px 10px 25px 0px rgba(0,0,0,0);
+
+	transition: box-shadow 0.3s ease;
+
+	:hover {
+		box-shadow: 0px 10px 25px 0px rgba(0,0,0,0.15);
+		text-decoration:none;
+	}
+
+	:hover ${Description} {
+		opacity: 1;
+	}
+
+	:hover ${Description} p {
+		top: 0;
+	}
 `;
 
 export interface Props {
@@ -108,6 +140,7 @@ export const ProjectCard: React.SFC<Props> = (props) => {
 		<CardContainer className="col-12 col-xl-4 col-lg-3 col-md-4 col-sm-6 ">
 			<ProjectLink to={{pathname: `/${props.project._id}/home`, state: props.project }}>
 				<CardTop style={{background: `url(${props.bg}) no-repeat center top / cover`}}>
+					<SDGs>
 					{SDGArray.map((SDG, SDGi) => {
 						if (SDGi > 4) { 
 							return '';
@@ -116,6 +149,8 @@ export const ProjectCard: React.SFC<Props> = (props) => {
 						<i key={SDGi} className={`icon-${SDGArray[Math.floor(Math.random() * 17)].ico}`} />
 						);
 					})}
+					</SDGs>
+					<Description><p>{excerptText(props.project.about, 20)}</p></Description>
 				</CardTop>
 				<CardBottom>
 					<Title>{excerptText(props.project.name, 10)}</Title>
