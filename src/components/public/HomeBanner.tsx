54c13fb5b663fe68e1fd2a44841477bd1fa6fae2
Murray Williams <murray.williams@ventureweb.com>
2018-06-20 17:44:33 +0200

    Added banner component content + styling

--- src/components/public/HomeBanner.tsx
+++ src/components/public/HomeBanner.tsx
@@ -1,20 +1,65 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+const penguinImg = require('../../assets/images/home/ixo-banner-penguin.png');
+
 const BannerLeft = styled.div`
 	width: 100%;
+
+	img {
+		margin-left: -3%;
+		width: 70%;
+	}
 `;
 const BannerRight = styled.div`
 	width: 100%;
+	color: white;
+	margin-top: 15%;
+	h2 {
+		font-size: 60px;
+		font-family: ${props => props.theme.fontRobotoCondensed};
+		margin-bottom: 0;
+		width: 100%;
+	}
+	h5 {
+		font-size: 23px;
+		font-weight: 300;
+	}
+	p {
+		margin-top: 5%;
+		position: relative;
+		box-sizing: border-box;
+		font-weight: 300;
+		padding-right: 25%;
+	}
+	p::before {
+		content: " ";
+		display: block;
+		position: absolute;
+		height: 1px;
+		background: #00D2FF;
+		width: 100px;
+		top: -20%;
+	}
+	button {
+		background: none;
+		color: white;
+		border: 1px solid #49BFE0;
+		padding: 10px 25px;
+		text-transform: uppercase;
+		font-size: 15px;
+		font-family: ${props => props.theme.fontRobotoCondensed};
+		margin-top: 20px;
+	}
 `;
 export interface ParentProps { }
 
 export const HomeBanner: React.SFC<ParentProps> = (props) => {
 	return (
-		<div className="col-md-12">
+		<div className="row">
 			<div className="col-md-6">
 				<BannerLeft>
-					<img src="" alt="" />
+					<img src={penguinImg} alt="" />
 				</BannerLeft>
 			</div>
 			<div className="col-md-6">
@@ -22,7 +67,7 @@ export const HomeBanner: React.SFC<ParentProps> = (props) => {
 					<div className="row">
 						<h2>What counts</h2>
 						<h5>Impact Data: Trust, Measure & Accountability</h5>
-						<p>ixo provides a trusted global information network that is owned by everyone. </p>
+						<p>ixo provides a trusted global information network that is owned by everyone. Enabling anyone to become the creators of their own impact projects and a stake-holder in the projects they believe in. </p>
 						<button>start your own impact project</button>
 					</div>
 				</BannerRight>
--- src/components/public/HomePage.tsx
+++ src/components/public/HomePage.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import { HomeBanner } from './HomeBanner';
+import styled from 'styled-components';
 
+const bannerBg = require('../../assets/images/home/ixo-banner-bg.jpg');
+
+const Banner = styled.div`
+	background: #00273A url(${bannerBg});
+	background-size: cover;
+	background-position: left 5px bottom -70px;
+	width: 100%;
+	height: 500px;
+
+`;
 export interface ParentProps {
 	title: string;
 }
 
 export const HomePage: React.SFC<ParentProps> = (props) => {
 	return (
-		<div className="container-fluid">
+		<Banner className="container-fluid">
 			<HomeBanner />
-		</div>
+		</Banner>
 	);
 };
