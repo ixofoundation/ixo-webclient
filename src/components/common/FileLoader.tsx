769577f07fa3653fcbdda582936a4c5ee3f4352a
Cedric Franz <cedric.franz@gmail.com>
2018-06-22 09:48:01 +0200

    Add project

---
+++ src/components/common/FileLoader.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import Dropzone from 'react-dropzone'; 
+import { iconUpload } from '../../lib/commonData';
+
+import styled from 'styled-components';
+
+/*
+Creates a dropzone to drop or select files. 
+
+Parameters:
+	fileUrl: The callback function that is called when an image is selected it gets a base64encoded image file
+	placeholder?: (Optional) This is the text to be displayed under the upload-icon. Default: "Choose file"
+
+Example:
+	<FileLoader placeholder="Choose a file" value={this.state.fileUrl} />
+
+*/
+
+const IconImage = styled.img`
+	padding: 3px;
+	margin-top: 10px;
+`;
+
+const styles = {
+	dropzone: {
+		width: '100%',
+		height: '150px',
+		backgroundColor: 'lightgrey',
+		borderWidth: '2px',
+		textAlign: 'center',
+		alignVertical: 'middle',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		borderRadius: '5px',
+	}
+};
+
+export interface StateProps {
+	selectedCallback: Function;
+	acceptType: string;
+	placeholder?: string;
+}
+
+export interface State {
+	filename: string;
+}
+
+export class FileLoader extends React.Component<StateProps, State> {
+
+	state = {
+		filename: null,
+	};
+
+	onDropAccepted = (files) => {
+		let file = files[0];
+		if (!file) {
+			return;
+		}
+	
+		this.setState({filename: file.name});
+		const reader = new FileReader();
+	
+		reader.onload = (e2) => {
+			this.props.selectedCallback(e2.target.result);
+		};
+	
+		reader.readAsDataURL(file);
+
+	}
+
+	showFilename = () => {
+		if (this.state.filename !== null) {
+			return ': "' + this.state.filename + '"';
+		}
+		return '';
+	}
+
+	render() {
+		return (
+			<div>
+				<Dropzone accept={this.props.acceptType} onDropAccepted={this.onDropAccepted} style={styles.dropzone} >
+					<IconImage src={iconUpload()} />
+					<p>{this.props.placeholder || 'Choose file'}{this.showFilename()}</p>
+				</Dropzone>
+			</div>
+		);
+	}
+}
--- src/components/project/ProjectCreate.tsx
+++ src/components/project/ProjectCreate.tsx
@@ -3,7 +3,10 @@ import { ImageLoader } from '../common/ImageLoader';
 import { PublicSiteStoreState } from '../../redux/public_site_reducer';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import { testProjectData, testClaimSchema, testClaimForm } from '../../lib/commonData';
+import { decode as base64Decode } from 'base-64';
+import { testProjectData } from '../../lib/commonData';
+import { DarkButton, buttonTypes } from '../common/Buttons';
+import { FileLoader } from '../common/FileLoader';
 
 const Text = styled.input`
 	margin: 20px 0;
@@ -31,20 +34,34 @@ export interface StateProps {
 export interface State {
 	pdsURL: string;
 	croppedImg: any;
-	projectClaimSchemaJson: string;
-	projectClaimFormJson: string;
-	projectCreateJson: string;
+	imageKey: string;
+	claimSchema: string;
+	claimSchemaKey: string;
+	claimForm: string;
+	claimFormKey: string;
+	projectJson: string;
+	project: Object;
+
+	fetchedImage: string;
+	fetchedFile: string;
 }	
 
 export class ProjectCreate extends React.Component<StateProps, State> {
 
 	state = {
-			pdsURL: 'http://35.225.6.178:5000',
+			pdsURL: 'http://104.155.142.57:5000/', // 'http://192.168.1.125:5000/', // 'http://35.225.6.178:5000/',
 			croppedImg: null,
-			projectClaimSchemaJson: testClaimSchema,
-			projectClaimFormJson: testClaimForm,
-			projectCreateJson: testProjectData,
-	};
+			imageKey: null,
+			claimSchema: '',
+			claimSchemaKey: null,
+			claimForm: '',
+			claimFormKey: null,
+			projectJson: testProjectData,
+			project: JSON.parse(testProjectData),
+
+			fetchedImage: null,
+			fetchedFile: '',
+			};
 
 	handleCreateProject = () => {
 		if (this.props.keysafe === null) {
@@ -53,13 +70,13 @@ export class ProjectCreate extends React.Component<StateProps, State> {
 			// inpageProvider.requestInfoFromIxoCM((error, response) => {
 			// 	// alert(`Dashboard handling received response for INFO response: ${JSON.stringify(response)}, error: ${JSON.stringify(error)}`);
 			// });
-			let message: string = this.state.projectCreateJson;
+			let message: string = this.state.projectJson;
 			this.props.keysafe.requestSigning(message, (error: any, signature: any) => {
 				
 				console.log('MESSAGE IS: ', JSON.parse(message));
 				console.log('SIGNATURE IS: ', signature);
 				// 'http://35.225.6.178:5000/' 'http://localhost:5000/'
-				this.props.ixo.project.createProject(JSON.parse(message), signature, 'http://104.155.142.57:5000/').then((res: any) => {
+				this.props.ixo.project.createProject(JSON.parse(message), signature, this.state.pdsURL).then((res: any) => {
 					console.log('PROJECT CREATE STATUS: ', res);
 				});
 			});
@@ -70,35 +87,94 @@ export class ProjectCreate extends React.Component<StateProps, State> {
 		this.setState({pdsURL: event.target.value});
 	}
 
-	handleProjectClaimSchemaChange = (event: any) => {
-		this.setState({projectClaimSchemaJson: event.target.value});
-	}
-
-	handleProjectClaimFormChange = (event: any) => {
-		this.setState({projectClaimFormJson: event.target.value});
-	}
-
 	handleProjectChange = (event: any) => {
-		this.setState({projectCreateJson: event.target.value});
+		this.setState({project: JSON.parse(event.target.value), projectJson: event.target.value});
 	}
 
 	handleImage = (base64Image) => {
 		this.setState({croppedImg: base64Image });
 	}
 	
+	uploadImage = (event) => {
+		this.props.ixo.project.createPublic(this.state.croppedImg, this.state.pdsURL).then((res: any) => {
+			console.log('Uploaded: ', res);
+			let newProject = this.state.project;
+			newProject.imageLink = res.result;
+			this.setState({project: newProject, projectJson: JSON.stringify(newProject)});
+		});
+	}
+
+	fetchImage = (event) => {
+		this.props.ixo.project.fetchPublic(this.state.imageKey, this.state.pdsURL).then((res: any) => {
+			console.log('Fetched: ', res);
+			let imageSrc = 'data:' + res.contentType + ';base64,' + res.data;
+			this.setState({fetchedImage: imageSrc});
+		});
+	}
+
+	handleFileSelected = (type, base64File) => {
+		if (type === 'schema') {
+			this.setState({claimSchema: base64File });
+		}
+		if (type === 'form') {
+			this.setState({claimForm: base64File });
+		}
+	}
+	
+	uploadFile = (type) => {
+		let fileToUpload: string;
+		if (type === 'schema') {
+			fileToUpload = this.state.claimSchema;
+		}
+		if (type === 'form') {
+			fileToUpload = this.state.claimForm;
+		}
+
+		this.props.ixo.project.createPublic(fileToUpload, this.state.pdsURL).then((res: any) => {
+			console.log('Uploaded: ', res);
+			let newProject = this.state.project;
+			if (type === 'schema') {
+				newProject.templates.claim.schema = res.result;
+			}
+			if (type === 'form') {
+				newProject.templates.claim.form = res.result;
+			}
+			this.setState({project: newProject, projectJson: JSON.stringify(newProject)});
+		});
+	}
+
+	fetchFile = (event) => {
+		this.props.ixo.project.fetchPublic(this.state.claimSchemaKey, this.state.pdsURL).then((res: any) => {
+			console.log('Fetched: ', res);
+			let fileContents = base64Decode(res.data);
+			this.setState({fetchedFile: fileContents});
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<Container className="container">
 					<div className="row">
 						<div className="col-md-12">
-							<Text placeholder="PDS Url: http://" value={this.state.pdsURL} onChange={this.handlePdsUrlChange} />
-							<ImageLoader placeholder="Choose file" imageWidth={960} aspect={16 / 9} imageCallback={this.handleImage}/>
+							<Text placeholder="Project datastore url example: http://104.155.142.57:5000/" value={this.state.pdsURL} onChange={this.handlePdsUrlChange} />
+							<ImageLoader placeholder="Choose project image file" imageWidth={960} aspect={16 / 9} imageCallback={this.handleImage}/>
 							<img src={this.state.croppedImg} />
-							<TextArea value={this.state.projectClaimSchemaJson} onChange={this.handleProjectClaimSchemaChange} />
-							<TextArea value={this.state.projectClaimFormJson} onChange={this.handleProjectClaimFormChange} />
-							<TextArea value={this.state.projectCreateJson} onChange={this.handleProjectChange} />
+							<DarkButton type={buttonTypes.PRIMARY} value="Upload image" onClick={this.uploadImage} />
+							<FileLoader placeholder="Choose claim schema file" acceptType="application/json" selectedCallback={(dataUrl) => this.handleFileSelected('schema', dataUrl)}/>
+							<DarkButton type={buttonTypes.PRIMARY} value="Upload file" onClick={() => this.uploadFile('schema')} />
+							<Text value={this.state.project.templates.claim.schema} />
+							<FileLoader placeholder="Choose claim form file" acceptType="application/json" selectedCallback={(dataUrl) => this.handleFileSelected('form', dataUrl)}/>
+							<DarkButton type={buttonTypes.PRIMARY} value="Upload file" onClick={() => this.uploadFile('form')} />
+							<Text value={this.state.project.templates.claim.form} />
+
+							<TextArea value={this.state.projectJson} onChange={this.handleProjectChange} />
 							<button onClick={this.handleCreateProject}>CREATE PROJECT</button>
+							<br /><br />
+							<DarkButton type={buttonTypes.PRIMARY} value="Fetch image" onClick={this.fetchImage} />
+							<img src={this.state.fetchedImage} />
+							<DarkButton type={buttonTypes.PRIMARY} value="Fetch file" onClick={this.fetchFile} />
+							<TextArea value={this.state.fetchedFile} />
 						</div>
 					</div>
 				</Container>
