e11ee1a7297e74afd960b7d2f2590a9e4dacb036
Murray Williams <murray.williams@ventureweb.com>
2018-06-25 15:57:42 +0200

    Adjusted html structure + responsive styling on pricing banner

--- src/components/public/PricingBanner.tsx
+++ src/components/public/PricingBanner.tsx
@@ -9,18 +9,10 @@ const Banner = styled.div`
 	background: #002233 url(${bannerImg}) no-repeat;
 	background-size: cover;
 	width: 100%;
-	padding-left: 15%;
-	padding-right: 15%;
 	height: 600px;
 	margin: 0;
-	@media (min-width: ${deviceWidth.tablet}px){
-		padding-left: 10%;
-		padding-right: 10%;
-	}
 	@media (max-width: ${deviceWidth.mobile}px){
-		.col-md-2.hidden {
-			display: none;
-		}
+		padding: 6%;
 	}
 `;
 
@@ -43,7 +35,7 @@ const BannerText = styled.div`
 		font-size: 23px;
 		font-weight: 300;
 		margin-bottom: 0;
-		padding-bottom: 20px;
+		padding-bottom: 15px;
 	}
 	@media (max-width: ${deviceWidth.mobile}px){
 		h5 {
@@ -51,15 +43,17 @@ const BannerText = styled.div`
 		}
 	}
 	p {
+		padding-top: 20px;
 		position: relative;
 		box-sizing: border-box;
 		font-weight: 300;
 		padding-right: 55%;
-		padding-top: 10px;
+		margin-bottom: 0;
 	}
 	@media (max-width: ${deviceWidth.mobile}px){
 		p {
-			padding-right: 0;
+			padding-top: 12px;
+			padding-right: 30%;
 		}
 	}
 	p::before {
@@ -69,7 +63,12 @@ const BannerText = styled.div`
 		height: 1px;
 		background: #00D2FF;
 		width: 100px;
-		top: -20%;
+		top: 10%;
+	}
+	@media (max-width: ${deviceWidth.mobile}px){
+		p::before {
+			top: 0;
+		}
 	}
 	button {
 		background: none;
@@ -89,7 +88,7 @@ export interface ParentProps { }
 export const PricingBanner: React.SFC<ParentProps> = (props) => {
 	return (
 		<Banner className="row">
-			<div className="col-md-12">
+			<div className="offset-md-2 col-md-10 col-sm-11 col-xs-11 offset-sm-1 offset-xs-1">
 				<BannerText>
 					<div className="row">
 						<h2>Plans & pricing</h2>
