9f2fa22bcf892320261bbe3b19976ee1effb79ba
donnierisk <donnierisk@gmail.com>
2018-06-26 15:19:49 +0200

    basic progress bar working with dummy data

--- src/components/public/Widgets.tsx
+++ src/components/public/Widgets.tsx
@@ -1,5 +1,27 @@
 import * as React from 'react';
 import { Circle } from 'rc-progress';
+import styled from 'styled-components';
+
+const WidgetContainer = styled.div`
+
+	.col-md-4 {
+		position: relative;
+	}
+
+	.success, .rejected, .pending {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+	.success {
+		z-index: 2;
+	}
+
+	.pending {
+		z-index: 1;
+	}
+`;
 
 export interface ParentProps {
 	title: string;
@@ -8,23 +30,51 @@ export interface ParentProps {
 export class Widgets extends React.Component<ParentProps> {
 
 	state = {
-		percent: 0
+		percentApproved: 0,
+		percentRejected: 0,
+		percentagePending: 0
 	};
 
 	componentDidMount() {
-		this.increase();
+		this.increaseApproved();
+		this.increaseRejected();
+		this.increasePending();
 	}
 
-	increase = () => {
-		const tm = setTimeout(this.increase, 10);
+	increaseApproved = () => {
+		const tm = setTimeout(this.increaseApproved, 30);
 
-		const percent = this.state.percent + 1;
+		const percentApproved = this.state.percentApproved + 1;
 
-		if (percent > 100) {
+		if (percentApproved > 80) {
 			clearTimeout(tm);
 			return;
 		}
-		this.setState({ percent });
+		this.setState({ percentApproved });
+	}
+
+	increaseRejected = () => {
+		const tm = setTimeout(this.increaseRejected, 30);
+
+		const percentRejected = this.state.percentRejected + 1;
+
+		if (percentRejected > 95) {
+			clearTimeout(tm);
+			return;
+		}
+		this.setState({ percentRejected });
+	}
+
+	increasePending = () => {
+		const tm = setTimeout(this.increasePending, 30);
+
+		const percentagePending = this.state.percentagePending + 1;
+
+		if (percentagePending > 85) {
+			clearTimeout(tm);
+			return;
+		}
+		this.setState({ percentagePending });
 	}
 
 		// const total = 100;
@@ -39,11 +89,15 @@ export class Widgets extends React.Component<ParentProps> {
 
 	render() {
 		return (
-			<div className="icon-container-fluid">
-				<div className="icon-row">
-					<div className="icon-col-md-4"><Circle percent={this.state.percent} strokeWidth="4" strokeColor="blue" /></div>
+			<WidgetContainer className="container-fluid">
+				<div className="row">
+					<div className="col-md-4">
+						<Circle className="success" percent={this.state.percentApproved} trailColor="rgba(0,0,0,0)" strokeWidth="4" strokeColor="blue" />
+						<Circle className="pending" percent={this.state.percentagePending} trailColor="rgba(0,0,0,0)" strokeWidth="4" strokeColor="orange" />
+						<Circle className="rejected" percent={this.state.percentRejected} strokeWidth="4" strokeColor="red" />
+					</div>
 				</div>
-			</div>
+			</WidgetContainer>
 		);
 	}
 }
