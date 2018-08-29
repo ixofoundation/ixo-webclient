d5380924311121e97cc215b67edb45ce58d1e49a
source-code-smith <herman@sourcecodesmith.com>
2018-08-29 09:20:55 +0200

    optimized start script

--- bin/start.sh
+++ bin/start.sh
@@ -8,29 +8,21 @@ echo "Build Ixo Web"
 CURRENT_DIR=`dirname $0`
 ROOT_DIR=$CURRENT_DIR/..
 
-if [ "$1" = "dev" ]; then
-  echo "Building Developer images"
-
-  if [[ ! -f $ROOT_DIR/docker-compose.dev.yml  ]] ; then
-      echo 'It appears as if you are trying to run in local development mode without a docker-compose.dev.yml. Make a copy of docker-compose.uat.yml, rename it and adapt it as neccesary. BUT NEVER CHECK IT IN!'
-      exit
-  fi
-
-  cd $ROOT_DIR
-  yarn
-  cd bin
-
-  docker build -t trustlab/ixo-web $ROOT_DIR
-  docker-compose -f $ROOT_DIR/docker-compose.yml -f $ROOT_DIR/docker-compose.dev.yml up --build --no-start
-elif [ "$1" = "uat" ]; then
-  echo "Running with UAT config"
-
-  docker-compose -f $ROOT_DIR/docker-compose.yml -f $ROOT_DIR/docker-compose.uat.yml up --no-start
-else
-  echo "Running with Production config"
-
-  docker-compose -f $ROOT_DIR/docker-compose.yml -f $ROOT_DIR/docker-compose.prod.yml up --no-start
+# if the environment cannot provide the type try to get it from the script argument
+if [ -z "$TARGET_ENVIRONMENT" ];
+then
+  TARGET_ENVIRONMENT=$1
 fi
+
+if [ -z "$TARGET_ENVIRONMENT" ];
+then
+    echo 'UNKNOWN TARGET ENVIRONMENT (dev, qa, uat or prod)'
+    exit
+fi
+
+echo "Pulling $TARGET_ENVIRONMENT images"
+docker-compose -f "$ROOT_DIR/docker-compose.yml" -f "$ROOT_DIR/docker-compose.$TARGET_ENVIRONMENT.yml" up --no-start
+
 
 docker-compose start ixo-web
 echo -n "Starting Ixo Web ..."
