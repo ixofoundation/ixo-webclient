93e2a68e2ca55d6bd055014a534ae05fab2b83ae
donnierisk <donnierisk@gmail.com>
2018-10-11 10:59:02 +0200

    fixed loader

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -15,44 +15,60 @@ const Container = styled.div`
 
 const LoaderContainer = styled.div`
 	height: 80px;
+	width: 80px;
 	display: flex;
 	align-items: center;
+	justify-content: center;
 `;
 
 const LoaderWrapper = styled.div`
-	background: ${props => props.theme.ixoBlue};
 	padding: 0;
 	border-radius: 50%;
-	animation: pulse 1.5s infinite ease;
-		
-	@keyframes pulse {
-		0%   { padding: 0; background: rgba(0,210,255,1);}
-		100% { padding: 20px; background: rgba(0,34,51,1);}
-	}
+	position: relative;
+	width: 40px;
+	height: 40px;
+	display: flex;
+	background: #00c2e4;
+	justify-content: center;
+	align-items: center;
 `;
 
-const LoaderInner = styled.div`
+const Pulse = styled.div`
 
 	border-radius: 50%;
-	padding: 5px;
+	width: 80px;
+	height: 80px;
 	background: ${props => props.theme.ixoBlue};
-
-	i {
-		font-size: 54px;
-		display:block;
-		width: 29px;
-		height: 29px;
-		padding: 0;
-		background: ${props => props.theme.bg.blue};
-		border-radius: 50%;
+	position: absolute;
+	margin: 0;
+	padding: 0;
+	
+	@keyframes pulse {
+		0%   { transform: scale(1); background: rgba(0,210,255,1);}
+		100% { transform: scale(1.2); background: rgba(0,34,51,1);}
 	}
-	.icon-ixo-x:before {
+	animation: pulse 1.5s infinite ease-out;
+
+`;
+
+const IxoIcon = styled.i`
+	font-size: 54px;
+	display:block;
+	width: 29px;
+	height: 29px;
+	padding: 0;
+	background: ${props => props.theme.bg.blue};
+	border-radius: 50%;
+	position: absolute;
+
+	:before {
 		color: ${props => props.theme.ixoBlue};
 		position: relative;
 		top: -13px;
 		left: -12px;
 	}
 `;
+
 export interface Props {
 	info: string;
 }
@@ -61,10 +77,9 @@ export const Spinner: React.SFC<Props> = ({info}) => {
 	return (
 		<Container>
 			<LoaderContainer>
+				<Pulse/>
 				<LoaderWrapper>
-					<LoaderInner>
-					<i className="icon-ixo-x" />
-					</LoaderInner>
+					<IxoIcon className="icon-ixo-x" />
 				</LoaderWrapper>
 			</LoaderContainer>
 			<p>{info}</p>
