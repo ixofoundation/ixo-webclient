e32fda9d10bd7b8340258bbc9300009ef1c279bc
donnierisk <donnierisk@gmail.com>
2018-11-22 17:42:00 +0200

    commented out serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import { createPublicSiteStore } from './redux/store';
 import { AppConnected } from './App';
 
@@ -16,4 +16,4 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+// registerServiceWorker();
