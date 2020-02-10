b7c1c8bed10c6ef822dbd3d284ca5c7a42275588
Divan <divan@nonacreative.com>
2020-02-10 18:07:42 +0200

    chore: improve linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,60 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
   ],
+  ignorePatterns: ['node_modules', 'build'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // '@typescript-eslint/explicit-member-accessibility': [
+    //   'error',
+    //   {
+    //     accessibility: 'explicit',
+    //   },
+    // ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'camelcase': 'error',
+    'id-blacklist': [
+      'error',
+      'any',
+      'Number',
+      'number',
+      'String',
+      'string',
+      'Boolean',
+      'boolean',
+      'Undefined',
+      'undefined',
+    ],
+    'id-match': 'error',
+    'max-classes-per-file': ['off', 10],
+    'no-empty': 'error',
+    'no-underscore-dangle': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 }
