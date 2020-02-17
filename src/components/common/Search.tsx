fb95dfb3ed45d3773ebd8a081e73169a02e26a07
Divan <divan@nonacreative.com>
2020-02-17 16:45:01 +0200

    wip: add search filters

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -12,6 +12,7 @@ const SearchWrapper = styled.div`
   border-radius: 4px;
   transform: translateY(-50%);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Roboto, sans-serif;
   > * {
     flex: 1;
     border-radius: 4px;
@@ -22,15 +23,11 @@ const SearchWrapper = styled.div`
       height: 100%;
     }
   }
-
-  .search-input::placeholder {
-    color: #e8edee;
-  }
 `
 
 const ModalButton = styled.div`
   background: #e8edee;
-  padding: 1rem;
+  padding: 0 1rem;
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
@@ -47,22 +44,91 @@ const ModalButton = styled.div`
   }
   .icon-down {
     margin-left: auto;
+    transition: all 0.3s;
+    transform-origin: center;
   }
 `
 
 const SearchIconWrapper = styled.div`
-  width: 64px;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 50px;
   flex: initial;
-  line-height: 64px;
   text-align: center;
   i {
     color: #83d9f2;
   }
 `
 
+const SearchModal = styled.div`
+  position: absolute;
+  top: calc(100% + 1rem);
+  left: 0;
+  z-index: 10;
+  background: white;
+  width: 100%;
+  padding: 1.5em 2.875em 4.5em;
+`
+
+const SearchHeading = styled.h3`
+  font-weight: 600;
+  font-size: 1.25rem;
+  box-sizing: border-box;
+  margin: 0.825rem 0 1.5rem;
+`
+
+const SearchButtonsWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin: 0 -0.5rem;
+`
+
+const SearchFilterButton = styled.div`
+  border: 1px solid #39c3e6;
+  color: #39c3e6;
+  text-align: center;
+  width: calc(33.333% - 1rem);
+  margin: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: row wrap;
+  padding: 0.75rem;
+  border-radius: 6px;
+  font-weight: 500;
+  i {
+    color: #39c3e6;
+  }
+  > * {
+    display: block;
+    width: 100%;
+  }
+  &.active,
+  &:hover:not(.disabled) {
+    background: linear-gradient(180deg, #04d0fb 0%, #49bfe0 100%);
+    color: white;
+    i {
+      color: white;
+    }
+  }
+  &.disabled {
+    border-color: #a5adb0;
+    color: #a5adb0;
+    cursor: not-allowed;
+    i {
+      color: #a5adb0;
+    }
+  }
+`
+
 export default class Search extends React.Component {
   state = {
     search: '',
+    isModalOpen: false,
+    filterButtonText: 'All projects',
   }
 
   handleChange = (event): void => {
@@ -71,13 +137,25 @@ export default class Search extends React.Component {
     })
   }
 
-  clickModal = (): void => {
-    alert('Open project filter modal')
-  }
-
   handleSubmit = (e): void => {
     e.preventDefault()
     alert(`Search for: ${this.state.search}`)
+  }
+
+  handleToggleModal = (): void => {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    })
+  }
+
+  handleSearchFilter = (filterButtonText: string): void => {
+    const slug = filterButtonText
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .replace(/[^\w-]+/g, '')
+    this.setState({ filterButtonText })
+    this.handleToggleModal()
+    console.log(`filter the things: ${slug}`)
   }
 
   render(): JSX.Element {
@@ -86,10 +164,15 @@ export default class Search extends React.Component {
         <div className="row">
           <div className="col-xs-12 col-md-8 offset-md-2">
             <SearchWrapper>
-              <ModalButton onClick={(): void => this.clickModal()}>
+              <ModalButton onClick={(): void => this.handleToggleModal()}>
                 <i className="icon-world" />
-                All IXO
-                <i className="icon-down" />
+                {this.state.filterButtonText}
+                <i
+                  className="icon-down"
+                  style={{
+                    transform: this.state.isModalOpen ? 'rotateX(180deg)' : '',
+                  }}
+                />
               </ModalButton>
               <form
                 onSubmit={(e): void => this.handleSubmit(e)}
@@ -99,7 +182,7 @@ export default class Search extends React.Component {
                   formStyle={FormStyles.search}
                   id="name"
                   type="text"
-                  text="Search"
+                  text="Search all projects"
                   key="search"
                   onChange={(): void => this.handleChange(event)}
                 />
@@ -107,6 +190,44 @@ export default class Search extends React.Component {
               <SearchIconWrapper onClick={this.handleSubmit}>
                 <i className="icon-search" />
               </SearchIconWrapper>
+              <SearchModal
+                style={{ display: this.state.isModalOpen ? 'block' : 'none' }}
+              >
+                <SearchHeading>Explore</SearchHeading>
+                <SearchButtonsWrapper>
+                  <SearchFilterButton
+                    onClick={(): void =>
+                      this.handleSearchFilter('All projects')
+                    }
+                    className="active"
+                  >
+                    <i className="icon-claims" />
+                    All projects
+                  </SearchFilterButton>
+                  <SearchFilterButton
+                    onClick={(): void => this.handleSearchFilter('My projects')}
+                  >
+                    <i className="icon-claims2" />
+                    My projects
+                  </SearchFilterButton>
+                  <SearchFilterButton className="disabled">
+                    <i className="icon-serviceproviders" />
+                    People
+                  </SearchFilterButton>
+                  <SearchFilterButton className="disabled">
+                    <i className="icon-eye" />
+                    Impact Oracles
+                  </SearchFilterButton>
+                  <SearchFilterButton className="disabled">
+                    <i className="icon-projects" />
+                    Template
+                  </SearchFilterButton>
+                  <SearchFilterButton className="disabled">
+                    <i className="icon-sdg-sustainablecities" />
+                    Data Marketplace
+                  </SearchFilterButton>
+                </SearchButtonsWrapper>
+              </SearchModal>
             </SearchWrapper>
           </div>
         </div>
