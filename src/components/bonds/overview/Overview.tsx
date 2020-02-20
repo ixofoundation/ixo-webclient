8774301fac4761d1d06c466dfad9de9720b7db55
Divan <divan@nonacreative.com>
2020-02-20 17:36:38 +0200

    chore(styling): style funding buy sell section

--- src/components/bonds/overview/Overview.tsx
+++ src/components/bonds/overview/Overview.tsx
@@ -13,10 +13,15 @@ const BondsHomeSectionNav = styled.div`
     font-weight: normal;
     font-size: 1.1875rem;
     text-transform: uppercase;
+    text-decoration: none;
 
     color: #ffffff;
     padding: 0.25rem 1.5rem;
     &.active {
+      color: #87def6;
+    }
+    &:hover {
+      text-decoration: none;
       color: #87def6;
     }
   }
