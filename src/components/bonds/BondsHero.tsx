25018063eea5934276afe136502b322a579397b3
Divan van Tonder <46312800+nonadivan@users.noreply.github.com>
2020-02-24 10:38:25 +0200

    chore(style): style bonds ui components (#58)
    * chore(bonds): style the bonds header
    
    * feat: add bonds hero section
    
    * chore(styling): style charts section
    
    * chore(styling): style funding buy sell section
    
    * chore(style): style bonds ui components

---
+++ src/components/bonds/BondsHero.tsx
@@ -0,0 +1,126 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import { deviceWidth } from '../../lib/commonData'
+
+const HeroContainer = styled.div`
+  background: url(${require('../../assets/images/heroBg.jpg')}) no-repeat center
+    top;
+  color: white;
+  background-size: cover;
+  margin: 0;
+  font-weight: normal;
+
+  .container,
+  .container .row {
+    height: 100%;
+  }
+
+  @media (min-width: ${deviceWidth.tablet}px) {
+    height: 200px;
+  }
+`
+
+const BondsHeroHeading = styled.h1`
+  color: white;
+  width: 100%;
+  margin: 0;
+  display: flex;
+  align-items: center;
+`
+
+const StatusIndicator = styled.span`
+  display: block;
+  width: 0.75rem;
+  height: 0.75rem;
+  margin-right: 0.75rem;
+  border-radius: 50%;
+  background: ${/* eslint-disable-line */ props => props.theme.bg.green};
+`
+
+const StatisticContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  align-content: center;
+  padding: 0;
+
+  &:last-child {
+    justify-content: center;
+    flex-direction: column;
+  }
+  i {
+    font-size: 0.8125em;
+  }
+`
+const OutcomeWrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  margin-top: 1.25rem;
+`
+
+const Outcome = styled.div`
+  font-family: Roboto Condensed;
+  font-size: 0.875rem;
+  line-height: 1.2;
+  letter-spacing: 0.3px;
+  text-transform: uppercase;
+  color: #49bfe0;
+  display: flex;
+  align-items: center;
+  &:not(:last-child) {
+    margin-right: 0.75rem;
+  }
+  i {
+    font-size: 1.5em;
+    margin-right: 0.25rem;
+  }
+`
+
+export class BondsHero extends React.Component<{}, {}> {
+  outcomes = [
+    { title: '3. good health', iconClass: 'icon-sdg-goodhealth' },
+    {
+      title: '6. clean water and sanitation',
+      iconClass: 'icon-sdg-cleanwater',
+    },
+    { title: '15. Life on land', iconClass: 'icon-sdg-lifeonland' },
+  ]
+
+  render(): JSX.Element {
+    return (
+      <HeroContainer>
+        <div className="container">
+          <div className="row">
+            <StatisticContainer className="col-8">
+              <BondsHeroHeading>
+                <StatusIndicator className="active" />
+                Togo water project
+              </BondsHeroHeading>
+              <OutcomeWrapper>
+                {this.outcomes.map(outcome => (
+                  <Outcome key={outcome.title}>
+                    <i className={outcome.iconClass}></i>
+                    {outcome.title}
+                  </Outcome>
+                ))}
+              </OutcomeWrapper>
+            </StatisticContainer>
+            <StatisticContainer className="col-4">
+              <div>
+                <strong>Created:</strong> 24 March 18
+              </div>
+              <div>
+                <strong>By:</strong> Water for Africa
+              </div>
+              <div>
+                <i className="icon-location" />
+                &nbsp; Uganda
+              </div>
+            </StatisticContainer>
+          </div>
+        </div>
+      </HeroContainer>
+    )
+  }
+}
--- src/components/bonds/BondsWrapper.tsx
+++ src/components/bonds/BondsWrapper.tsx
@@ -3,9 +3,10 @@ import Header from './header/Header'
 import { NavLink } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
 import './BondsWrapper.css'
+import { BondsHero } from './BondsHero'
 
-export default function BondsWrapper({ children }) {
-  const isActive = (m: any, l: any, paths: [string]) => {
+export default function BondsWrapper({ children }): JSX.Element {
+  const isActive = (m: any, l: any, paths: [string]): any => {
     let active = m != undefined
 
     paths.forEach((path: string) => {
@@ -16,27 +17,33 @@ export default function BondsWrapper({ children }) {
   }
 
   return (
-    <div className="BondsWrapper">
-      <div className="tablinks sidebar">
-        <NavLink
-          exact
-          to="/bonds"
-          isActive={(m, l) => {
-            return isActive(m, l, ['/bonds/overview'])
-          }}
-          className="tablinks_tablink icon home"
-        />
-        <NavLink
-          to="/bonds/exchange"
-          className="tablinks_tablink icon trades"
-        />
-        <NavLink to="/bonds/orders" className="tablinks_tablink icon orders" />
-        <a className="tablinks_tablink icon wallet" />
-        <a className="tablinks_tablink icon settings" />
-      </div>
-      <div className="pane">
-        <Header />
-        {children}
+    <div>
+      <BondsHero />
+      <div className="BondsWrapper">
+        <div className="tablinks sidebar">
+          <NavLink
+            exact
+            to="/bonds"
+            isActive={(m, l): any => {
+              return isActive(m, l, ['/bonds/overview'])
+            }}
+            className="tablinks_tablink icon home"
+          />
+          <NavLink
+            to="/bonds/exchange"
+            className="tablinks_tablink icon trades"
+          />
+          <NavLink
+            to="/bonds/orders"
+            className="tablinks_tablink icon orders"
+          />
+          <a className="tablinks_tablink icon wallet" />
+          <a className="tablinks_tablink icon settings" />
+        </div>
+        <div className="pane">
+          <Header />
+          {children}
+        </div>
       </div>
     </div>
   )
