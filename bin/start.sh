50d7b52ea6d631f7aef8860ea0237a0193f6dc5e
Erez Greenberg <erez@nona.digital>
2020-04-03 11:21:45 +0200

    chore(docker): file clean up

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,10 @@
- #!/bin/bash -xe
+#!/bin/bash -xe
 #green=`tput setaf 2`
 echo "***********************************"
 echo "* IXO WEB START                   *"
 echo "***********************************"
 echo ""
-echo "Build Ixo Web"
+echo "Build Ixo Web for $TARGET_ENVIRONMENT"
 CURRENT_DIR=`dirname $0`
 ROOT_DIR=$CURRENT_DIR/..
 
@@ -16,19 +16,18 @@ fi
 
 if [ -z "$TARGET_ENVIRONMENT" ];
 then
-    echo 'UNKNOWN TARGET ENVIRONMENT (dev, qa, uat or prod)'
+    echo 'UNKNOWN TARGET ENVIRONMENT'
     exit
 fi
 
 echo "Pulling $TARGET_ENVIRONMENT images"
 docker-compose -f "$ROOT_DIR/docker-compose.yml" -f "$ROOT_DIR/docker-compose.$TARGET_ENVIRONMENT.yml" up --no-start
 
-
-docker-compose start ixo-web
-echo -n "Starting Ixo Web ..."
+docker-compose start ixo-web-$TARGET_ENVIRONMENT
+echo -n "Starting Ixo Web for $TARGET_ENVIRONMENT ..."
 sleep 5
 echo ${green} "done"
-docker-compose logs --tail 13 ixo-web
+docker-compose logs --tail 13 ixo-web-$TARGET_ENVIRONMENT
 echo ""
 echo "***********************************"
 echo "* IXO WEB START COMPLETE          *"
