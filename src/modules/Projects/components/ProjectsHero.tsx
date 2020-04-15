d557c7a1dc535bb36c4e61461902b9fbcde5f4bc
David <djsfdavid@gmail.com>
2020-04-15 11:46:45 +0200

    chore: split out types

--- src/modules/Projects/components/ProjectsHero.tsx
+++ src/modules/Projects/components/ProjectsHero.tsx
@@ -5,27 +5,13 @@ import { StatType } from '../../../types/models'
 import Search from '../../../components/common/Search'
 import { contentType } from '../../../types/models'
 import * as instanceSettings from '../../../instance-settings'
-
+import { Stats } from '../types'
 import {
   ContainerInner,
   StatisticContainer,
   HeroInner,
   HeroContainer,
 } from './ProjectsHero.styles'
-
-export interface Stats {
-  claims: Claims
-  totalServiceProviders: number
-  totalProjects: number
-  totalEvaluationAgents: number
-}
-interface Claims {
-  total: number
-  totalSuccessful: number
-  totalSubmitted: number
-  totalPending: number
-  totalRejected: number
-}
 
 export interface State {
   statistics: Stats
