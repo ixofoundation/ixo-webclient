9bce3ede7a82e29e5fe4b4c07f8577cd1266151b
DavidFransch <35291291+DavidFransch@users.noreply.github.com>
2020-04-15 12:22:16 +0200

    chore: split out types correctly (#129) (#129)
    * chore: split out styles
    
    * chore: seperate styles for each component
    
    * chore: remove duplicate files
    
    * chore: split out types

--- src/modules/Projects/types.ts
+++ src/modules/Projects/types.ts
@@ -22,3 +22,17 @@ export interface GetProjectsSuccessAction {
 }
 
 export type ProjectsActionTypes = GetProjectsAction | GetProjectsSuccessAction
+
+export interface Stats {
+  claims: Claims
+  totalServiceProviders: number
+  totalProjects: number
+  totalEvaluationAgents: number
+}
+interface Claims {
+  total: number
+  totalSuccessful: number
+  totalSubmitted: number
+  totalPending: number
+  totalRejected: number
+}
