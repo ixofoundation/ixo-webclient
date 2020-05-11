50085d812a73443ae62abc54b0f5ac826ccb7b77
Divan <divan@nonacreative.com>
2020-05-11 16:15:28 +0200

    chore: update QR code text

--- src/components/common/QRComponent.tsx
+++ src/components/common/QRComponent.tsx
@@ -58,7 +58,7 @@ export default class QRComponent extends React.Component<ParentProps> {
       <QRContainer>
         <QRInner>
           <QRImg src={this.state.imgSrc} />
-          <p>Connect to project</p>
+          <p>Scan with your ixo Mobile App</p>
         </QRInner>
       </QRContainer>
     )
