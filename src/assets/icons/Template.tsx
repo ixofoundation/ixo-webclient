8a0412beb18460100d57a9d148cafd431e086ee5
Erez Greenberg <erez@nona.digital>
2020-05-13 10:34:21 +0200

    chore(entities): add new search bar icons

--- src/assets/icons/Template.tsx
+++ src/assets/icons/Template.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Template = (props): JSX.Element => {
   return (
     <svg
-      width={props.width || 18}
+      width={props.width || 36}
       viewBox="0 0 18 18"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M6.9 7.33h4.21v9.25H6.9zm5.89 9.25h2.52A1.69 1.69 0 0017 14.89V7.32h-4.21v9.26zm2.52-15.16H2.69A1.69 1.69 0 001 3.11v2.52h16V3.11a1.69 1.69 0 00-1.69-1.69zM1 14.89a1.69 1.69 0 001.69 1.69h2.52V7.32H1v7.57z"
+        d="M13.07,6.48V3.74L7.66.62,2.26,3.74V10l2.67,1.54v2.74l5.41,3.12,5.4-3.12V8ZM4,9V4.74L7.66,2.62l3.67,2.12v.74l-1-.58L4.93,8v1.5Z"
         fill={props.fill || '#fff'}
       />
     </svg>
