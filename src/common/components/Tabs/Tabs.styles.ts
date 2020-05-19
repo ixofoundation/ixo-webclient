609e26f2abcd3dcea3cfd14a11af6eb34be519c8
DavidFransch <35291291+DavidFransch@users.noreply.github.com>
2020-05-19 14:08:26 +0200

    chore(styling): make tabs same width (#168)
    * chore: fix svg sizing
    
    * chore(styling): add min width to make tabs same width
    
    Co-authored-by: Divan <divan@nonacreative.com>

--- src/common/components/Tabs/Tabs.styles.ts
+++ src/common/components/Tabs/Tabs.styles.ts
@@ -1,27 +1,26 @@
 import styled from 'styled-components'
 
 export const createTabsContainer = (activeTabColor: string): any => styled.div`
-  background:${/* eslint-disable-line */ props => props.theme.bg.gradientBlue};
-  border-radius:3px;
+  background: ${/* eslint-disable-line */ props => props.theme.bg.gradientBlue};
+  border-radius: 3px;
   overflow: hidden;
-  display:inline-flex;
-  box-shadow: 0px 10px 50px 0px rgba(0,0,0,0.35);
+  display: inline-flex;
+  box-shadow: 0px 10px 50px 0px rgba(0, 0, 0, 0.35);
 
   a {
-    font-family: ${
-      /* eslint-disable-line */ props => props.theme.fontRobotoCondensed
-    };
-    color:white;
+    font-family: ${/* eslint-disable-line */ props =>
+      props.theme.fontRobotoCondensed};
+    color: white;
     text-transform: uppercase;
-    font-weight:300;
-    font-size:14px;
-    padding:10px 20px;
-    display:flex;
+    font-weight: 300;
+    font-size: 14px;
+    padding: 10px 20px;
+    display: flex;
     align-items: center;
     justify-content: center;
     text-decoration: none;
-
     transition: all 0.3s ease;
+    min-width: 156px;
   }
 
   a:last-child {
@@ -29,7 +28,7 @@ export const createTabsContainer = (activeTabColor: string): any => styled.div`
   }
 
   a i {
-    margin:0 10px 0 0
+    margin: 0 10px 0 0;
     font-size: 18px;
   }
 
@@ -38,24 +37,21 @@ export const createTabsContainer = (activeTabColor: string): any => styled.div`
   }
 
   a p {
-    margin-bottom:0;
+    margin-bottom: 0;
   }
 
   a:hover {
-    text-decoration: none
+    text-decoration: none;
     color: ${/* eslint-disable-line */ props => props.theme.ixoBlue};
-    
   }
 
   a.active {
-    background: ${
-      /* eslint-disable-line */ props =>
-        activeTabColor || props.theme.bg.lightBlue
-    };
-    color:white;
+    background: ${/* eslint-disable-line */ props =>
+      activeTabColor || props.theme.bg.lightBlue};
+    color: white;
   }
 
   img {
-    padding:0 5px;
+    padding: 0 5px;
   }
 `
