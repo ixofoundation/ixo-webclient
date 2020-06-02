106741b91d7bd53a1aa0a491e21858455394b942
David <djsfdavid@gmail.com>
2020-06-02 14:39:18 +0200

    chore: replicate footer on app.ixo

--- src/common/components/Footer/FooterLeft/FooterLeft.styles.ts
+++ src/common/components/Footer/FooterLeft/FooterLeft.styles.ts
@@ -1,27 +1,128 @@
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import { deviceWidth } from '../../../../lib/commonData'
 
-export const ExternalFooterLink = styled.a`
-  font-family: ${/* eslint-disable-line */ props =>
-    props.theme.fontRobotoRegular};
-  color: white;
-  margin: 0;
-  font-size: 13px;
-  font-weight: 400;
-  border: 1px solid #000000;
-  border-radius: 3px;
-  margin: 0 10px;
+export const FooterMenuPosition = styled.div`
+  position: relative;
+  margin-left: 4rem;
+  @media (min-width: ${deviceWidth.tablet}px) {
+    left: 9rem;
+  }
+  @media (min-width: ${deviceWidth.desktop}px) {
+    left: 14rem;
+  }
+  @media (min-width: ${deviceWidth.desktopLarge}px) {
+    left: 20rem;
+  }
+`
+
+export const FooterMenuWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin: 0 -1rem;
+
+  @media (min-width: ${deviceWidth.desktop}px) {
+    margin: 0 -5.6rem;
+  }
+
+  @media (min-width: ${deviceWidth.desktopLarge}px) {
+    margin: 0 -5rem;
+  }
+`
+
+export const FooterMenu = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-content: flex-start;
+  max-width: calc(100% / 2 - 1rem);
+  line-height: 2.25rem;
+  h4 {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    margin-right: 0.625rem;
+    margin-top: 2rem;
+  }
+
+  @media (min-width: ${deviceWidth.mobile}px) {
+    max-width: calc(100% / 4 - 1rem);
+    flex: unset;
+    line-height: unset;
+    margin-right: 1rem;
+  }
+
+  @media (min-width: 420px) {
+    h4 {
+      margin-right: 5rem;
+    }
+  }
+
+  @media (min-width: ${deviceWidth.tablet}px) {
+    h4 {
+      margin-left: 1.875rem;
+    }
+  }
+
+  @media (min-width: ${deviceWidth.desktopLarge}px) {
+    h4 {
+      margin-right: 9rem;
+    }
+  }
+`
+
+export const FooterLink = styled(NavLink)`
+  display: block;
+  font-family: ${(props): string => props.theme.fontRobotoRegular};
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
+  color: #7b8285;
+  margin: 0 1.625rem 0 0;
+  transition: border 0.3s ease;
+
+  &.nowrap {
+    white-space: nowrap;
+  }
 
   :hover {
     text-decoration: none;
-    color: ${/* eslint-disable-line */ props => props.theme.fontBlue};
+    color: ${(props): string => props.theme.fontBlue};
+  }
+
+  @media (min-width: ${deviceWidth.tablet}px) {
+    padding: 0.625rem 1.25rem 0.625rem;
+    margin: 0 0.625rem;
+    font-size: 15px;
+  }
+`
+
+export const ExternalFooterLink = styled.a`
+  display: block;
+  font-family: ${(props): string => props.theme.fontRobotoRegular};
+  margin: 0;
+  font-size: 1rem;
+  font-weight: normal;
+  margin: 0 1.625rem 0 0;
+  white-space: nowrap;
+  color: #7b8285;
+
+  &.mailto {
+    color: #39c3e6;
+  }
+
+  :hover {
+    text-decoration: none;
+    color: ${(props): string => props.theme.fontBlue};
   }
 
   transition: border 0.3s ease;
 
   @media (min-width: ${deviceWidth.tablet}px) {
-    padding: 10px 20px 10px;
-    margin: 0 10px;
+    padding: 0.625rem 1.25rem 0.625rem;
+    margin: 0 0.625rem;
     font-size: 15px;
   }
 
@@ -29,13 +130,13 @@ export const ExternalFooterLink = styled.a`
 `
 
 export const IXOLogo = styled.img`
-  margin-right: 20px;
-  height: 40px;
+  margin-right: 1.25rem;
+  width: 3.125rem;
   margin-top: 2px;
 `
 
 export const FooterTextBlue = styled.span`
-  color: #5cd0fa;
+  color: #39c3e6;
 
   :hover {
     text-decoration: underline;
@@ -43,20 +144,26 @@ export const FooterTextBlue = styled.span`
 `
 
 export const FooterText = styled.div`
-  padding: 19px 0px 20px 15px;
+  padding: 10px 0px 10px 64px;
   color: #808080;
   font-family: Roboto;
   font-size: 14px;
   line-height: 19px;
 
+  @media (min-width: ${deviceWidth.tablet}px) {
+    padding-left: 3.5rem;
+  }
+
   a {
     font-weight: 400;
+    margin-bottom: 1rem;
   }
   p {
     font-size: 13px;
     color: #808080;
     display: block;
     font-weight: 400;
+    margin: 0;
   }
 `
 
@@ -64,20 +171,49 @@ export const Main = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  hr {
+    border: 1px solid #2a2e2f;
+    width: 500%;
+    margin-left: -200%;
+    margin-top: 3rem;
+  }
 `
 
 export const ByLine = styled.div`
   a {
-    color: #808080;
+    color: #7b8285;
   }
 
   a:hover {
     color: white;
   }
 
-  a:before {
+  a:not(:first-child):before {
     content: '|';
+    font-size: 1rem;
     margin: 0 15px;
     color: #808080;
+  }
+
+  p {
+    &.legalInfo {
+      margin-top: 1rem;
+    }
+  }
+
+  @media (min-width: ${deviceWidth.mobile}px) {
+    p {
+      &.loc {
+        :after {
+          content: '.';
+          font-size: 16px;
+          margin: 0 15px;
+          color: #808080;
+        }
+      }
+      &.legalInfo {
+        margin-top: 0;
+      }
+    }
   }
 `
