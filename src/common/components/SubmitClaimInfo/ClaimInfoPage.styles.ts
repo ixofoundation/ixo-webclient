dc91735a867026a7d80f18f844dbffc892c981c7
David <djsfdavid@gmail.com>
2020-06-15 12:26:42 +0200

    feat(styling): style instruction page

---
+++ src/common/components/SubmitClaimInfo/ClaimInfoPage.styles.ts
@@ -0,0 +1,105 @@
+import styled from 'styled-components'
+import { deviceWidth } from 'src/lib/commonData'
+
+export const Container = styled.div`
+  position: relative;
+  z-index: 10;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  max-width: 760px;
+  background: white;
+  border-radius: 4px;
+  padding: 2rem 2.5rem;
+  overflow: hidden;
+  @media (min-width: ${deviceWidth.mobile}px) {
+    padding: 2.5rem 3.75rem;
+  }
+
+  h1 {
+    font-size: 36px;
+    line-height: 42px;
+    margin: 10px 0;
+  }
+
+  h3 {
+    font-size: 18px;
+    line-height: 36px;
+    color: #7b8285;
+    max-width: 77%;
+    margin-bottom: 2.5rem;
+    @media (min-width: ${deviceWidth.mobile}px) {
+      max-width: 100%;
+    }
+  }
+
+  hr {
+    border: 1px solid #dfe3e8;
+    width: 500%;
+    margin-left: -200%;
+    margin-top: 2.5rem;
+  }
+`
+
+export const ListWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+`
+export const ListItems = styled.div`
+  height: unset;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: flex-start;
+  align-content: flex-start;
+  max-width: calc(100% / 2 - 2.5rem);
+  > * {
+    font-size: 16px;
+    font-weight: normal;
+    line-height: 24px;
+    display: flex;
+    color: #000000;
+    white-space: nowrap;
+    margin-bottom: 1.175rem;
+    margin-right: 7.5rem;
+    svg {
+      margin-right: 0.5rem;
+    }
+  }
+  @media (min-width: ${deviceWidth.mobile}px) {
+    height: 300px;
+  }
+`
+export const PositionButtons = styled.div`
+  display: inline-block;
+  align-items: center;
+  margin-top: 2.5rem;
+`
+export const ReturnButton = styled.button`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  color: #a5adb0;
+  background: none;
+  border: none;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+`
+
+export const StartButton = styled.button`
+  width: 120px;
+  height: 50px;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  background: linear-gradient(180deg, #04d0fb 0%, #49bfe0 100%);
+  border-radius: 4px;
+  border-color: transparent;
+  color: #fff;
+  margin-left: 1rem;
+  @media (min-width: ${deviceWidth.mobile}px) {
+    margin-left: 24rem;
+  }
+`
--- src/common/components/SubmitClaimInfo/ClaimInfoPage.tsx
+++ src/common/components/SubmitClaimInfo/ClaimInfoPage.tsx
@@ -1,78 +1,30 @@
 import * as React from 'react'
-import styled from 'styled-components'
-import Calendar from '../../../assets/icons/Calendar'
-
-export const Container = styled.div`
-  position: relative;
-  z-index: 10;
-  left: 303px;
-  bottom: 90px;
-  width: 826px;
-  height: 750px;
-  background: white;
-  border-radius: 4px;
-  padding: 2.5rem;
-  overflow: hidden;
-
-  h1 {
-  }
-
-  h3 {
-  }
-
-  hr {
-    border: 1px solid #dfe3e8;
-    width: 500%;
-    margin-left: -200%;
-    margin-top: 3rem;
-  }
-`
-
-export const ListWrapper = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-`
-export const ListItems = styled.div`
-  height: 300px;
-  display: flex;
-  flex-flow: column wrap;
-  justify-content: flex-start;
-  align-content: flex-start;
-  max-width: calc(100% / 2 - 2.5rem);
-`
-export const ButtonSection = styled.div`
-  display: inline-block;
-`
-export const ReturnButton = styled.button`
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 19px;
-  align-items: center;
-  color: #a5adb0;
-  background: none;
-  border: none;
-  margin: 0;
-  padding: 0;
-  cursor: pointer;
-`
-
-export const StartButton = styled.button`
-  background: linear-gradient(180deg, #04d0fb 0%, #49bfe0 100%);
-  border-radius: 4px;
-  border-color: transparent;
-  color: #fff;
-  margin-left: 33rem;
-`
-// export interface Props {}
+import CalendarSort from '../../../assets/icons/CalendarSort'
+import Location from '../../../assets/icons/Location'
+import UploadFile from '../../../assets/icons/UploadFile'
+import Validation from '../../../assets/icons/Validation'
+import ShortText from '../../../assets/icons/ShortText'
+import LongText from '../../../assets/icons/LongText'
+import Selection from '../../../assets/icons/Selection'
+import UploadImage from '../../../assets/icons/UploadImage'
+import UploadAudio from '../../../assets/icons/UploadAudio'
+import SelectPicture from '../../../assets/icons/SelectPicture'
+import UploadVideo from '../../../assets/icons/UploadVideo'
+import QRcode from '../../../assets/icons/QRcode'
+import {
+  Container,
+  ListItems,
+  PositionButtons,
+  ReturnButton,
+  StartButton,
+} from './ClaimInfoPage.styles'
 
 export const ClaimInfoPage: React.FunctionComponent<{}> = (): JSX.Element => {
   const listItems = [
     { icon: 'short', description: 'Fill in short answer' },
     { icon: 'date', description: 'Date Picker' },
     { icon: 'long', description: 'Give a long text answer' },
-    { icon: 'selector', description: 'Selector rate out of 10' },
+    { icon: 'selection', description: 'Selector rate out of 10' },
     { icon: 'image', description: 'Upload an image' },
     { icon: 'recording', description: 'Upload audio recording' },
     { icon: 'file', description: 'Upload a file/document' },
@@ -87,35 +39,36 @@ export const ClaimInfoPage: React.FunctionComponent<{}> = (): JSX.Element => {
   const renderIcon = (icon: string): JSX.Element => {
     switch (icon) {
       case 'short':
-        return <Calendar fill="#000" />
+        return <ShortText fill="#C3D0E5" />
       case 'date':
-        return <Calendar fill="#000" />
+        return <CalendarSort fill="#C3D0E5" />
       case 'long':
-        return <Calendar fill="#000" />
-      case 'selector':
-        return <Calendar fill="#000" />
+        return <LongText fill="#C3D0E5" />
+      case 'selection':
+        return <Selection fill="#C3D0E5" />
       case 'image':
-        return <Calendar fill="#000" />
+        return <UploadImage fill="#C3D0E5" />
       case 'recording':
-        return <Calendar fill="#000" />
+        return <UploadAudio fill="#C3D0E5" />
       case 'file':
-        return <Calendar fill="#000" />
+        return <UploadFile fill="#C3D0E5" />
       case 'valid':
-        return <Calendar fill="#000" />
+        return <Validation fill="#C3D0E5" />
       case 'location':
-        return <Calendar fill="#000" />
+        return <Location fill="#C3D0E5" />
       case 'options':
-        return <Calendar fill="#000" />
+        return <Selection fill="#C3D0E5" />
       case 'picture':
-        return <Calendar fill="#000" />
+        return <SelectPicture fill="#C3D0E5" />
       case 'video':
-        return <Calendar fill="#000" />
+        return <UploadVideo fill="#C3D0E5" />
       case 'qr':
-        return <Calendar fill="#000" />
+        return <QRcode fill="#C3D0E5" />
       default:
         return null
     }
   }
+
   return (
     <Container>
       <h1>Submit a claim</h1>
@@ -127,10 +80,7 @@ export const ClaimInfoPage: React.FunctionComponent<{}> = (): JSX.Element => {
         {listItems.map(
           (item, index): JSX.Element => {
             return (
-              <div
-                key={index}
-                style={{ marginRight: '4rem', marginBottom: '1rem' }}
-              >
+              <div key={index}>
                 {renderIcon(item.icon)} {item.description}
               </div>
             )
@@ -138,10 +88,10 @@ export const ClaimInfoPage: React.FunctionComponent<{}> = (): JSX.Element => {
         )}
       </ListItems>
       <hr />
-      <ButtonSection>
+      <PositionButtons>
         <ReturnButton>Come back later</ReturnButton>
         <StartButton>Start</StartButton>
-      </ButtonSection>
+      </PositionButtons>
     </Container>
   )
 }
