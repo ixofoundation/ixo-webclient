e5ed507559e14d4683495958d928f1490f7487a6
Divan <divan@nonacreative.com>
2020-06-18 16:54:42 +0200

    chore: style status and error screens

--- src/components/project/ControlPanel/Actions/Actions.styles.ts
+++ src/components/project/ControlPanel/Actions/Actions.styles.ts
@@ -116,8 +116,75 @@ export const AssistantHeader = styled.div`
   }
 `
 
-export const SummaryWrapper = styled.div`
+export const AssistantContentWrapper = styled.div`
   height: 100%;
-  background-color: white;
+  background: white;
+`
+
+export const SummaryWrapper = styled(AssistantContentWrapper)`
   position: relative;
+`
+
+export const AssistantProgress = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    font-size: 2.25rem;
+    line-height: 1.2;
+    letter-spacing: 0.3px;
+    color: black;
+  }
+  .icon-pulse-wrapper {
+    padding: 1rem;
+    border-radius: 50%;
+    position: relative;
+    z-index: 1;
+    background: white;
+    @keyframes iconPulse {
+      0% {
+        transform: scale(1.1);
+        opacity: 1;
+      }
+      100% {
+        transform: scale(1.5);
+        opacity: 0;
+      }
+    }
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      z-index: -1;
+      top: -1px;
+      left: -1px;
+      width: calc(100% + 2px);
+      height: calc(100% + 2px);
+      border-radius: 50%;
+      border: 2px solid #dfe3e8;
+      opacity: 0;
+      animation-delay: 1s;
+      animation: iconPulse 1s ease-in-out;
+      transform-origin: center;
+    }
+    &.repeat:after {
+      animation-delay: 0;
+      animation: iconPulse 1s infinite ease-in-out;
+    }
+  }
+
+  .error {
+    color: firebrick;
+  }
+
+  button {
+    background: none;
+    border: none;
+    outline: none !important;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 1.2;
+  }
 `
