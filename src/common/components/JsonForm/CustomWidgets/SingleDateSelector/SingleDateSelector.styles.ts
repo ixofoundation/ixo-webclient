08d3180b07429364fcac2e6e548fd670e8d51459
David <djsfdavid@gmail.com>
2020-06-29 15:12:35 +0200

    chore(styling): mobile date picker wip

---
+++ src/common/components/JsonForm/CustomWidgets/SingleDateSelector/SingleDateSelector.styles.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export const MobileWrapper = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 9;
+  /* background: firebrick; */
+  .SingleDatePicker .SingleDatePickerInput .SingleDatePicker_picker {
+    top: 30px;
+    width: 100vw;
+    left: 0px;
+    position: relative;
+  }
+`
+export const MobileDateHeader = styled.header`
+  background: #002a3f;
+  padding: 1.5rem 1.25rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #002a3f;
+  position: absolute;
+  z-index: 5;
+  top: 0;
+  right: 100px;
+  left: 0;
+`
+export const HeadingItem = styled.button`
+  cursor: pointer;
+  border: none;
+  outline: none !important;
+  text-decoration: none;
+  background: transparent;
+  color: rgba(255, 255, 255, 0.3);
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  padding: 0;
+`
+
+export const ApplyButtonWrapper = styled.div`
+  position: absolute;
+  top: 704px;
+  left: 0px;
+  z-index: 4;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+export const ApplyButton = styled.button`
+  position: relative;
+  padding: 1rem 2rem;
+  background: #04d0fb;
+  background: linear-gradient(180deg, #04d0fb 0%, #49bfe0 100%);
+  border-radius: 4px;
+  color: white;
+  width: 100%;
+  text-align: center;
+  border: none;
+  outline: none !important;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  margin-top: 1.25rem;
+  &:after {
+    content: '';
+    position: absolute;
+    top: -1.25rem;
+    left: -20px;
+    height: 1px;
+    width: 100vw;
+    background-color: #e8edee;
+  }
+`
--- src/common/components/JsonForm/CustomWidgets/SingleDateSelector/SingleDateSelector.tsx
+++ src/common/components/JsonForm/CustomWidgets/SingleDateSelector/SingleDateSelector.tsx
@@ -3,13 +3,19 @@ import { SingleDatePicker } from 'react-dates'
 import moment from 'moment'
 import MediaQuery from 'react-responsive'
 import { deviceWidth } from '../../../../../lib/commonData'
+import {
+  MobileWrapper,
+  MobileDateHeader,
+  HeadingItem,
+  ApplyButtonWrapper,
+  ApplyButton,
+} from './SingleDateSelector.styles'
+import Back from '../../../../../assets/icons/Back'
 
 interface Props {
   id: string
   value: string
   onChange: (value: string) => void
-  numberOfMonths: number
-  //initialOrientation: string
 }
 
 interface State {
@@ -31,19 +37,44 @@ class SingleDateSelector extends React.Component<Props, State> {
     return (
       <>
         <MediaQuery maxWidth={`${deviceWidth.desktop - 1}px`}>
-          <SingleDatePicker
-            date={value ? moment(value) : null}
-            displayFormat="DD-MMM-YYYY"
-            onDateChange={(date): void =>
-              onChange(date ? date.format('DD-MMM-YYYY') : null)
-            }
-            focused={this.state.focused}
-            onFocusChange={({ focused }): void => this.setState({ focused })}
-            id={id}
-            isOutsideRange={(): boolean => false}
-            numberOfMonths={this.props.numberOfMonths}
-            orientation="vertical" //{this.props.initialOrientation}
-          />
+          <MobileWrapper>
+            {this.state.focused && (
+              <MobileDateHeader>
+                <HeadingItem onClick={(): void => console.log('clicked back')}>
+                  <Back />
+                </HeadingItem>
+                <HeadingItem
+                  onClick={(): void => {
+                    console.log('clicked clear')
+                  }}
+                >
+                  clear
+                </HeadingItem>
+              </MobileDateHeader>
+            )}
+
+            <SingleDatePicker
+              date={value ? moment(value) : null}
+              displayFormat="DD-MMM-YYYY"
+              onDateChange={(date): void =>
+                onChange(date ? date.format('DD-MMM-YYYY') : null)
+              }
+              focused={this.state.focused}
+              onFocusChange={({ focused }): void => this.setState({ focused })}
+              id={id}
+              isOutsideRange={(): boolean => false}
+              numberOfMonths={2}
+              orientation="vertical"
+              verticalHeight={625}
+            />
+            {this.state.focused && (
+              <ApplyButtonWrapper>
+                <ApplyButton onClick={(): void => console.log('clicked apply')}>
+                  Apply
+                </ApplyButton>
+              </ApplyButtonWrapper>
+            )}
+          </MobileWrapper>
         </MediaQuery>
         <MediaQuery minWidth={`${deviceWidth.desktop}px`}>
           <SingleDatePicker
@@ -56,8 +87,8 @@ class SingleDateSelector extends React.Component<Props, State> {
             onFocusChange={({ focused }): void => this.setState({ focused })}
             id={id}
             isOutsideRange={(): boolean => false}
-            numberOfMonths={this.props.numberOfMonths}
-            orientation="horizontal" //{this.props.initialOrientation}
+            numberOfMonths={2}
+            orientation="horizontal"
           />
         </MediaQuery>
       </>
