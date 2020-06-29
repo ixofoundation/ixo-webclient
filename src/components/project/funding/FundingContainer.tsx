dc808100255b68d3b19b68607ba83e66fde45b5e
migueldingli1997 <migueldingli97@gmail.com>
2020-06-29 17:03:47 +0200

    Added use of getSignData to buy, sell, withdrawFunds

--- src/components/project/funding/FundingContainer.tsx
+++ src/components/project/funding/FundingContainer.tsx
@@ -413,25 +413,35 @@ export class Funding extends React.Component<Props, State> {
         },
         senderDid: this.props.userInfo.didDoc.did,
       }
-      // need to add amount: ethAmt property for withdrawel
+      // need to add amount: ethAmt property for withdrawal
 
-      this.props.keysafe.requestSigning(
-        JSON.stringify(payload),
-        (error, signature) => {
-          if (!error) {
-            this.props.ixo.project
-              .payOutToEthWallet(payload, signature)
-              .then((response: any) => {
-                if (response.code === 0) {
-                  successToast('Withdraw requested successfully')
-                } else {
-                  errorToast('Unable to request a withdrawel at this time')
+      this.props.ixo.utils.getSignData(payload, 'project/WithdrawFunds')
+        .then((response: any) => {
+          if (response.sign_bytes && response.fee) {
+            this.props.keysafe.requestSigning(
+              JSON.stringify(payload),
+              (error, signature) => {
+                if (!error) {
+                  this.props.ixo.project
+                    .payOutToEthWallet(payload, signature)
+                    .then((response: any) => {
+                      if (response.code === 0) {
+                        successToast('Withdraw requested successfully')
+                      } else {
+                        errorToast('Unable to request a withdrawel at this time')
+                      }
+                    })
                 }
-              })
+              },
+              'base64',
+            )
+          } else {
+            errorToast('Unable to request a withdrawel at this time')
           }
-        },
-        'base64',
-      )
+        })
+        .catch(() => {
+          errorToast('Unable to request a withdrawel at this time')
+        })
     } else {
       errorToast('we not find your did')
     }
