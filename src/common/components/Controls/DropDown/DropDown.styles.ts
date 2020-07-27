165f530fac3dc69ee5845c21af00aed2a0296ca0
David <djsfdavid@gmail.com>
2020-07-27 10:57:53 +0200

    feat(styling): add content

---
+++ src/common/components/Controls/DropDown/DropDown.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const SelectContainer = styled.select`
+  background: #e8edee;
+  border-radius: 4px;
+  padding: 0.313rem 2.188rem 0.313rem 0.313rem;
+  &.active {
+    background-repeat: no-repeat;
+    padding: 0rem 3rem;
+    background-size: 2rem;
+    background-position: 10px 50%;
+    color: #000;
+  }
+`
--- src/common/components/Controls/DropDown/DropDown.tsx
+++ src/common/components/Controls/DropDown/DropDown.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { DropDownOption } from './types'
+import { SelectContainer } from './DropDown.styles'
+import availableFlags from '../../../../lib/json/availableFlags.json'
+import availableSDGs from '../../../../lib/json/availableSDGs.json'
 
 interface Props {
   selectText: string
   value?: string
   options: DropDownOption[]
   onChange: (value: string) => void
+}
+
+const getURL = (value: string): string => {
+  if (isNaN(parseInt(value))) {
+    if (availableFlags.availableFlags.includes(value)) {
+      return `url(${require(`../../../../assets/images/country-flags/${value.toLowerCase()}.svg`)})`
+    } else if (value == 'AA') {
+      return `url(${require('../../../../assets/images/country-flags/global.svg')})`
+    }
+  } else {
+    if (availableSDGs.availableSDGs.includes(value)) {
+      return `url(${require(`../../../../assets/images/sdg/sdg${value}.svg`)})`
+    }
+  }
+  return ''
 }
 
 const DropDown: React.FunctionComponent<Props> = ({
@@ -15,18 +33,34 @@ const DropDown: React.FunctionComponent<Props> = ({
   onChange,
 }) => {
   return (
-    <select
+    <SelectContainer
       defaultValue={value}
       onChange={(e): void => onChange(e.target.value)}
+      className={
+        value
+          ? value.length > 0 &&
+            value !== '' &&
+            value !== null &&
+            value !== undefined
+            ? 'active'
+            : null
+          : null
+      }
+      id="symbol"
+      style={{
+        backgroundImage:
+          value !== '' && value !== null && value !== undefined
+            ? getURL(`${value}`)
+            : '',
+      }}
     >
       <option value="">{selectText}</option>
       {options.map(opt => (
-        // TODO - icon
         <option key={opt.value} value={opt.value}>
           {opt.text}
         </option>
       ))}
-    </select>
+    </SelectContainer>
   )
 }
 
