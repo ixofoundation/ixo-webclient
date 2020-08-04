35ee890600380efdac95c719577e2423f25562b5
duy khanh nguyen <duykhanh9242@gmail.com>
2020-08-04 07:44:53 -0700

    - fix deepscan issue

--- src/modules/BondChart/components/Chart/Chart.tsx
+++ src/modules/BondChart/components/Chart/Chart.tsx
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-import { Line } from 'react-chartjs-2'
 import ReactApexChart from 'react-apexcharts'
 
 import { ChartContainer, StyledHeader } from './Chart.styles'
