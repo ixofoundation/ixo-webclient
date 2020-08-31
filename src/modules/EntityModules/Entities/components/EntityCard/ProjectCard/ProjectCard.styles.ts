6ea1f158412c867f024d1e643e6e25dcddfa113e
DavidFransch <35291291+DavidFransch@users.noreply.github.com>
2020-08-31 15:45:51 +0200

    style: cards to latest designs (#343)

--- src/modules/EntityModules/Entities/components/EntityCard/ProjectCard/ProjectCard.styles.ts
+++ src/modules/EntityModules/Entities/components/EntityCard/ProjectCard/ProjectCard.styles.ts
@@ -17,14 +17,18 @@ export const Title = styled(TitleBase)``
 export const Progress = styled(ProgressBase)``
 export const ProgressSuccessful = styled(ProgressSuccessfulBase)``
 export const ProgressRequired = styled(ProgressRequiredBase)``
-export const Impact = styled(ImpactBase)``
+export const Impact = styled(ImpactBase)`
+  color: #a5adb0 !important;
+`
 export const StatisticsContainer = styled(StatisticsContainerBase)`
   justify-content: space-between;
 `
 export const Statistic = styled(StatisticBase)``
 export const StatisticLabel = styled(StatisticLabelBase)`
   display: block;
+  font-weight: 400;
   font-size: 12px;
+  color: #a5adb0;
 `
 export const StatisticValue = styled(StatisticValueBase)`
   display: block;
