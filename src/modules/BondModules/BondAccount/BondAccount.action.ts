b3914cca9280458ebdf3175a3fdf7e705a53156c
duy khanh nguyen <duykhanh9242@gmail.com>
2020-09-02 11:30:29 -0700

    - api integration for account page

--- src/modules/BondModules/BondAccount/BondAccount.action.ts
+++ src/modules/BondModules/BondAccount/BondAccount.action.ts
@@ -19,12 +19,20 @@ export const getBondAccounts = (projectDID) => (
     // config
   );
 
+  const getAccountBalance = (param): any => {
+    console.log('param', param)
+    return Axios.get(
+    `${process.env.REACT_APP_GAIA_URL}/auth/accounts/${param}`
+    )
+  }
   return dispatch({
     type: BondAccountActions.GetAccounts,
     payload: getAccountsReq.then(
-      responses => {
-        console.log('getAccounts', responses.data)
-        return { bondAccounts: [] };
+      async responses => {
+        console.log('getAccounts', responses.data.InitiatingNodePayFees)
+        const response = await getAccountBalance(responses.data.InitiatingNodePayFees)
+          console.log('getresponse', response.data.result)
+          return { bondAccounts: [] };
       })
   });
 };
