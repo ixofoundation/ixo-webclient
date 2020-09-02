965c13614c22fbf795c3aeffa703dd219cffdb7c
duy khanh nguyen <duykhanh9242@gmail.com>
2020-09-02 04:43:19 -0700

    - update the funding chat bot

--- src/modules/Funding_Chat/FundingChat.container.tsx
+++ src/modules/Funding_Chat/FundingChat.container.tsx
@@ -51,11 +51,12 @@ class FundingChat extends React.Component<Props & RouteProps> {
     startAssistant('fuel_my_entity')
   }
 
-  onAssistantMessageReceive = (): void => {
+  onAssistantMessageReceive = (utter): void => {
     // TODO - actual event to trigger end
     // if (utter.text === "Sorry, I didn't get that. Could you rephrase?") {
     // TODO - actual response to pass to handleGetOrder
-    this.props.handleGetOrder(null)
+    console.log(utter)
+    // this.props.handleGetOrder(null)
     // }
   }
 
@@ -87,7 +88,6 @@ class FundingChat extends React.Component<Props & RouteProps> {
 
     return (
       <Fragment>
-        {!sending && !sent && !hasOrder && (
           <AssistantWrapper>
             <AssistantHeader>
               <h3 className="assistant-heading">
@@ -99,51 +99,6 @@ class FundingChat extends React.Component<Props & RouteProps> {
             </AssistantHeader>
             <Assistant onMessageReceive={this.onAssistantMessageReceive} />
           </AssistantWrapper>
-        )}
-        {!sending && !sent && hasOrder && !hasError && (
-          <SummaryWrapper>
-            <FundingChatConfirmOrder
-              subscription={subscription}
-              symbol={symbol}
-              amount={amount}
-              fiatAmount={fiatAmount}
-              fiatConversionRate={fiatConversionRate}
-              transactionFee={transactionFee}
-              fiatTransactionFee={fiatTransactionFee}
-              gasFee={gasFee}
-              total={total}
-              fiatTotal={fiatTotal}
-              handleConfirmOrder={(): void => handleConfirmOrder(projectDID)}
-              handleCancelOrder={(): void => handleCancelOrder()}
-            />
-          </SummaryWrapper>
-        )}
-        {sending && (
-          <AssistantContentWrapper>
-            <AssistantProgress>
-              <div className="icon-pulse-wrapper repeat">
-                <SendIcon width="80" fill="#49BFE0" />
-              </div>
-              <h2>Sending...</h2>
-            </AssistantProgress>
-          </AssistantContentWrapper>
-        )}
-        {sent && (
-          <AssistantContentWrapper>
-            <AssistantProgress>
-              <div className="icon-pulse-wrapper">
-                <PaymentSuccessIcon width="132" fill="#6FCF97" />
-              </div>
-              <h2>Payment Successful</h2>
-              <NavLink
-                className="close-button"
-                to={`/projects/${projectDID}/overview`}
-              >
-                Close
-              </NavLink>
-            </AssistantProgress>
-          </AssistantContentWrapper>
-        )}
         {hasError && (
           <AssistantContentWrapper>
             <AssistantProgress>
