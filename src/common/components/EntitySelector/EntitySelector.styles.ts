70e16f444ee8c73f36c0d4b458ee3809f3e57181
David <djsfdavid@gmail.com>
2020-09-03 10:47:22 +0200

    fix: template modal width

--- src/common/components/EntitySelector/EntitySelector.styles.ts
+++ src/common/components/EntitySelector/EntitySelector.styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { deviceWidth } from '../../../lib/commonData'
 
 export const ListWrapper = styled.div`
   max-height: 500px;
@@ -17,5 +18,21 @@ export const NoTemplatePreviewWrapper = styled.div`
 
   > div {
     margin-top: 10px;
+  }
+`
+export const ModalWrapper = styled.div`
+  > div {
+    top: 55%;
+    min-width: 270px;
+    left: -25%;
+    right: -15%;
+    @media (min-width: ${deviceWidth.mobileSmall}px) {
+      min-width: 300px;
+      left: -15%;
+    }
+    @media (min-width: ${deviceWidth.desktop}px) {
+      left: 0;
+      right: -100%;
+    }
   }
 `
