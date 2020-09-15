6520cb0a3e32c931ccadff44fe9a8e3bbc976167
David <djsfdavid@gmail.com>
2020-09-15 12:06:37 +0200

    fix(style): video and document upload centering

--- src/modules/ClaimModules/SubmitEntityClaim/components/Instructions/Instructions.styles.ts
+++ src/modules/ClaimModules/SubmitEntityClaim/components/Instructions/Instructions.styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { deviceWidth } from "../../../../../lib/commonData";
+import styled from 'styled-components'
+import { deviceWidth } from '../../../../../lib/commonData'
 
 export const Container = styled.div`
   border-radius: 4px;
@@ -7,9 +7,9 @@ export const Container = styled.div`
   padding: 3.125rem 1.375rem;
   max-width: 100%;
   @media (min-width: ${deviceWidth.tablet}px) {
-    padding: 2rem 3.75rem;
+    padding: 2rem 2.75rem;
   }
-`;
+`
 
 export const ContentWrapper = styled.div`
   h1 {
@@ -19,7 +19,7 @@ export const ContentWrapper = styled.div`
     letter-spacing: 0.3px;
     color: #000;
   }
-`;
+`
 export const SubHeader = styled.h4`
   font-family: ${(props: any): string => props.theme.fontRoboto};
   font-size: 1.125rem;
@@ -27,12 +27,12 @@ export const SubHeader = styled.h4`
   color: #7b8285;
   font-weight: normal;
   margin-bottom: 2.375rem;
-`;
+`
 
 export const ListWrapper = styled.div`
   display: flex;
   flex-flow: row wrap;
-`;
+`
 export const ListItems = styled.div`
   height: unset;
   display: flex;
@@ -58,9 +58,9 @@ export const ListItems = styled.div`
   }
 
   @media (min-width: ${deviceWidth.mobile}px) {
-    max-height: 328px;
+    max-height: 370px;
   }
-`;
+`
 export const ButtonWrapper = styled.div`
   display: flex;
   flex-flow: row nowrap;
@@ -93,4 +93,4 @@ export const ButtonWrapper = styled.div`
     border: none;
     color: #fff;
   }
-`;
+`
