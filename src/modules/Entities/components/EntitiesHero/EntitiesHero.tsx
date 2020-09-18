ccca43ca42b0c3e77b3b14ca53ee90788e34e7d0
Erez Greenberg <erez@nona.digital>
2020-09-18 16:32:29 +0200

    fix(header): launch dropdown

--- src/modules/Entities/components/EntitiesHero/EntitiesHero.tsx
+++ src/modules/Entities/components/EntitiesHero/EntitiesHero.tsx
@@ -14,6 +14,8 @@ import {
 import { EntityType } from '../../types'
 import { entityTypeMap } from '../../strategy-map'
 import { getHeaderSchema, getHeaderTabButtons } from './EntitiesHero.utils'
+import MediaQuery from 'react-responsive'
+import { deviceWidth } from 'lib/commonData'
 // TODO - when we know what the other entity types headers will look like then possibly refactor this as it's messy with all the conditions
 // or whatever else is needed. For now, just doing it based on entityType
 
@@ -86,7 +88,9 @@ export const EntitiesHero: React.FunctionComponent<Props> = ({
           </HeroIndicatorsWrapper>
         </div>
       </HeroInner>
-      <CreateEntityDropdown />
+      <MediaQuery minWidth={`${deviceWidth.desktop}px`}>
+        <CreateEntityDropdown />
+      </MediaQuery>
       <HeaderTabs
         buttons={headerTabButtons}
         activeTabColor={entityStrategyMap.themeColor}
