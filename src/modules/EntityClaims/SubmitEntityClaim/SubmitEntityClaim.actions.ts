4b9fb8e9a4395c4865b7ebe21c173e286ff7ce9e
Alain Gemenez <45015575+aliangm@users.noreply.github.com>
2020-10-19 00:54:11 -0700

    try to fix test

--- src/modules/EntityClaims/SubmitEntityClaim/SubmitEntityClaim.actions.ts
+++ src/modules/EntityClaims/SubmitEntityClaim/SubmitEntityClaim.actions.ts
@@ -55,17 +55,15 @@ export const getClaimTemplate = (templateDid: string) => (
             fromBase64(resourceData.data),
           )
           
-          /* // For demo, let's use template.
-          if (process.env.JEST_WORKER_ID !== undefined) {
-            attestation = claimTemplate;
-          } */
+          // For demo, let's use template.
+          //attestation = claimTemplate;
 
           return {
             templateDid,
             claimTitle: apiEntity.data.name,
             claimShortDescription: apiEntity.data.description,
             type: attestation.claimInfo.type,
-            questions: claimTemplate.forms,
+            questions: attestation.forms,
           }
         },
       )
