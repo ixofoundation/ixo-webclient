efccf273f337aaa588818d45c7731efd2665a24e
Alain Gemenez <45015575+aliangm@users.noreply.github.com>
2020-11-27 07:03:04 -0800

    deepscan

--- src/modules/Entities/FuelEntity/FuelEntity.container.tsx
+++ src/modules/Entities/FuelEntity/FuelEntity.container.tsx
@@ -1,20 +1,7 @@
 import React, { Dispatch } from 'react'
 import { RouteProps } from 'react-router'
-import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
-
-import Assistant, {
-  startAssistant,
-} from 'common/components/Assistant/Assistant'
-import FuelEntityConfirmOrder from './components/FuelEntityConfirmOrder/FuelEntityConfirmOrder'
 import { RootState } from 'common/redux/types'
-import * as fuelEntitySelectors from './FuelEntity.selectors'
-import { getOrder, confirmOrder, cancelOrder } from './FuelEntity.actions'
-import BackIcon from 'assets/icons/Back'
-import ChatbotIcon from 'assets/icons/Chatbot'
-import StatusMessage, {
-  MessageType,
-} from 'common/components/StatusMessage/StatusMessage'
 
 interface Props {
   assistantPanelToggle: () => void
