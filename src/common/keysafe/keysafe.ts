e26cc25f89e9259d295dc98fb6a6ba7cf6e0d0cc
gwn <dev@gwn.wtf>
2020-11-28 21:52:30 +0300

    Refactor to simplify the keysafe initializer util
    Let's keep it simple, shall we?

--- src/common/keysafe/keysafe.ts
+++ src/common/keysafe/keysafe.ts
@@ -1,21 +1,6 @@
 // TODO - use this instead of the redux keysafe for each component that has been refactored
-const initKeysafe = (): any => {
-  let keysafe: Window
 
-  if (!window['ixoKs']) {
-    keysafe = null
-  } else {
-    const IxoInpageProvider = window['ixoKs']
-    const ixoInpageProvider = new IxoInpageProvider()
+// Initialize the IXO Keysafe browser extension client, if the extension is
+// installed.
 
-    if (ixoInpageProvider) {
-      keysafe = ixoInpageProvider
-    } else {
-      keysafe = null
-    }
-  }
-
-  return keysafe
-}
-
-export default initKeysafe()
+export default window['ixoKs'] && new window['ixoKs']()
