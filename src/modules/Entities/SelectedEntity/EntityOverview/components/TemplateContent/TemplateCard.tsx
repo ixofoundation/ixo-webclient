f5ddf0cd839ea1baf785b4f2368af40723047762
Alain Gemenez <45015575+aliangm@users.noreply.github.com>
2020-12-20 22:35:09 -0800

    rm unused

--- src/modules/Entities/SelectedEntity/EntityOverview/components/TemplateContent/TemplateCard.tsx
+++ src/modules/Entities/SelectedEntity/EntityOverview/components/TemplateContent/TemplateCard.tsx
@@ -4,36 +4,6 @@ import { QuestionForm } from 'modules/EntityClaims/types'
 import SingleControlForm from 'common/components/JsonForm/SingleControlForm/SingleControlForm'
 import { customControls } from 'common/components/JsonForm/types'
 
-const Container = styled.div`
-  background: #F0F3F9;
-  border-radius: 0.25rem;
-  padding: 1rem 0.75rem;
-  margin-top: 1rem;
-  margin-bottom: 1rem;
-`
-
-const Title = styled.div`
-  font-size: 1.375rem;
-  font-weight: 400;
-  color: black;
-  margin-bottom: 0.5rem;
-  display: flex;
-  align-items: center;
-`
-
-const Description = styled.div`
-  font-size: 1rem;
-  font-weight: 400;
-  color: #4A4E50;
-  margin-bottom: 0.5rem;
-`
-
-const Control = styled.span`
-  font-size: 1rem;
-  font-weight: normal;
-  margin-left: 0.5rem;
-`
-
 interface Props {
   question: QuestionForm
 }
