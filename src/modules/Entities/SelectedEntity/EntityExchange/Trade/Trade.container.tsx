c6a46847a06b6535f2284d9bb5da3e36db907274
elite0226 <elite0226@outlook.com>
2021-08-05 15:04:43 -0400

    Exchange/Trade page asset card imported from asset explorer

--- src/modules/Entities/SelectedEntity/EntityExchange/Trade/Trade.container.tsx
+++ src/modules/Entities/SelectedEntity/EntityExchange/Trade/Trade.container.tsx
@@ -1,124 +1,84 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from 'common/redux/types'
 import { changeTradeMethod } from '../EntityExchange.actions'
 import {
-  TradeContainerLayout,
-  CardLayout,
   CardHeader,
-  CardBodyAsset,
   CardBodyWallet,
-  WalletBox
+  WalletBox,
 } from './Trade.container.styles'
 
-import IMG_lock from 'assets/images/lock.svg'
 import IMG_wallet1 from 'assets/images/exchange/wallet1.svg'
 import IMG_wallet2 from 'assets/images/exchange/wallet2.svg'
 import IMG_wallet3 from 'assets/images/exchange/wallet3.svg'
+import DataCard from 'modules/Entities/EntitiesExplorer/components/EntityCard/AssetCard/AssetCard'
+import { TermsOfUseType } from 'modules/Entities/types'
 
 const Trade: React.FunctionComponent = () => {
   const dispatch = useDispatch()
+
+  const selectedEntity = useSelector((state: RootState) => state.selectedEntity)
 
   const handleMethod = (event: any): any => {
     const newMethod = event.target.value
     dispatch(changeTradeMethod(newMethod))
   }
 
+  useEffect(() => {
+    console.log('selectedEntity', selectedEntity)
+  }, [selectedEntity])
+
   return (
-    <TradeContainerLayout>
-      <CardLayout>
-        <CardHeader>I have</CardHeader>
-        <CardBodyAsset style={{ backgroundColor: 'white' }}>
-          <img
-            src='https://pds_pandora.ixo.world/public/srw12bu516jkpy0rzmi'
-            alt='Asset'
-          />
-
-          <div style={{ padding: '20px 20px 30px 20px' }}>
-            <div className='btn-group'>
-              <button type='button' className='btn'>
-                Impact Token
-              </button>
-              <button type='button' className='btn active'>
-                Biodiversity
-              </button>
-            </div>
-            <div className='d-inline-flex justify-content-center align-items-center float-right token-logo'>
-              ixo
-            </div>
-
-            <div style={{ marginTop: '10px' }} />
-
-            <div className='title'>White Rhino</div>
-            <div className='location'>Umgeni Park, South Africa</div>
-
-            <div style={{ marginTop: '10px' }} />
-
-            <div
-              className='progress'
-              style={{ height: '10px', borderRadius: '50px' }}
-            >
-              <div
-                className='progress-bar'
-                role='progressbar'
-                style={{
-                  width: '50%',
-                  background:
-                    'linear-gradient(-90deg, #6FCF97 0%, #036784 100%)',
-                }}
-                aria-valuenow={50}
-                aria-valuemin={0}
-                aria-valuemax={100}
-              />
-            </div>
-            <div className='shares'>
-              <span>412</span>/1,000 Shares of Future Breeding Revenue
-            </div>
-
-            <div style={{ marginTop: '10px' }} />
-
-            <div className='count'>
-              <span>
-                <b>1</b>/8
-              </span>
-              <span>#02</span>
-              <div className='float-right' style={{ marginTop: '7px' }}>
-                <img src={IMG_lock} alt='locked' />
-              </div>
-            </div>
-
-            <div className='verified'>Verified Live and Well</div>
-
-            <div className='datetime'>By MIT • 31/12/2022</div>
+    <div className='container'>
+      {selectedEntity && (
+        <div className='row'>
+          <div className='col-4'>
+            <CardHeader>I have</CardHeader>
+            <DataCard
+              did={selectedEntity.did}
+              name={selectedEntity.name}
+              logo={selectedEntity.logo}
+              image={selectedEntity.image}
+              sdgs={selectedEntity.sdgs}
+              description={selectedEntity.description}
+              badges={[]}
+              version={''}
+              termsType={TermsOfUseType.PayPerUse}
+              isExplorer={false}
+            />
           </div>
-        </CardBodyAsset>
-      </CardLayout>
-      <CardLayout>
-        <CardHeader>Connect My Wallet</CardHeader>
-        {/* <select name='method' onChange={handleMethod}>
-          <option value='Swap'>Swap</option>
-          <option value='Purchase'>Purchase</option>
-          <option value='Sell'>Sell</option>
-          <option value='Auction'>Auction</option>
-          <option value='Bid'>Bid</option>
-        </select> */}
+          <div className='col-4'>
+            <CardHeader>Connect My Wallet</CardHeader>
+            <select
+              name='method'
+              onChange={handleMethod}
+              style={{ display: 'none' }}
+            >
+              <option value='Swap'>Swap</option>
+              <option value='Purchase'>Purchase</option>
+              <option value='Sell'>Sell</option>
+              <option value='Auction'>Auction</option>
+              <option value='Bid'>Bid</option>
+            </select>
 
-        <CardBodyWallet>
-          <WalletBox>
-            <img src={IMG_wallet1} alt='wallet1' />
-            <span>WalletConnect</span>
-          </WalletBox>
-          <WalletBox>
-            <img src={IMG_wallet2} alt='wallet1' />
-            <span>Keplr</span>
-          </WalletBox>
-          <WalletBox>
-            <img src={IMG_wallet3} alt='wallet1' />
-            <span>ixo mobile</span>
-          </WalletBox>
-        </CardBodyWallet>
-
-      </CardLayout>
-    </TradeContainerLayout>
+            <CardBodyWallet>
+              <WalletBox>
+                <img src={IMG_wallet1} alt='wallet1' />
+                <span>WalletConnect</span>
+              </WalletBox>
+              <WalletBox>
+                <img src={IMG_wallet2} alt='wallet1' />
+                <span>Keplr</span>
+              </WalletBox>
+              <WalletBox>
+                <img src={IMG_wallet3} alt='wallet1' />
+                <span>ixo mobile</span>
+              </WalletBox>
+            </CardBodyWallet>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 export default Trade
