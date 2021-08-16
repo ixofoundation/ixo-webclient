d569b03898bf7409467f2de0718367b14e6b1852
elite0226 <elite0226@outlook.com>
2021-08-16 14:05:47 -0400

    fixed styling issues on left handed panel on governance

--- src/modules/Entities/SelectedEntity/EntityEconomy/EconomyGovernance/EconomyGovernance.tsx
+++ src/modules/Entities/SelectedEntity/EntityEconomy/EconomyGovernance/EconomyGovernance.tsx
@@ -5,7 +5,7 @@ import {
   SectionTitleContainer,
   SectionTitle,
 } from '../EntityEconomy.styles'
-import GovernanceProposal from './components/GovernanceProposal'
+import GovernanceProposal, { ProposalType } from './components/GovernanceProposal'
 
 const EconomyGovernance: React.FunctionComponent = () => {
   return (
@@ -14,8 +14,30 @@ const EconomyGovernance: React.FunctionComponent = () => {
         <SectionTitle>Current Governance Proposals</SectionTitle>
       </SectionTitleContainer>
 
-      <GovernanceProposal />
-      <GovernanceProposal />
+      <GovernanceProposal
+        no={999}
+        type={ProposalType.Membership}
+        announce={'Extend the project end-date to September 2020'}
+        remain={412}
+        proposedBy={'Shaun Conway'}
+        submissionDate={'2020-06-23 16:23'}
+        closeDate={'2020-08-24 16:30'}
+        votes={230}
+        available={280}
+        myVote={false}
+      />
+      <GovernanceProposal
+        no={4}
+        type={ProposalType.Budget}
+        announce={'Issue an Alpha Bond for $100,000'}
+        remain={0}
+        proposedBy={'Shaun Conway'}
+        submissionDate={'2020-04-01 10:00'}
+        closeDate={'2020-04-21 10:00'}
+        votes={230}
+        available={280}
+        myVote={true}
+      />
 
       <SectionTitleContainer>
         <SectionTitle>Past Governance Proposals</SectionTitle>
