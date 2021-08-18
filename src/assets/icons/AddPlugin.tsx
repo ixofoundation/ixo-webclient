41928c0662928a67b70b635b737f8ef07a640c60
elite0226 <elite0226@outlook.com>
2021-08-18 09:57:07 -0400

    modify to React DOM element

--- src/assets/icons/AddPlugin.tsx
+++ src/assets/icons/AddPlugin.tsx
@@ -12,7 +12,7 @@ const Apps = ({ width }: Prop): JSX.Element => {
       viewBox="0 0 32 32"
       fill="none"
     >
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M16.0002 8.4999L8.50031 1H1V8.4999L8.50031 15.9998L1 23.4997V31H8.50031L16.0002 23.4997L23.5001 31H31V23.4997L23.5001 15.9998L31 8.4999V1H23.5001L16.0002 8.4999Z" stroke="#D8D8D8" stroke-width="1.4" stroke-dasharray="1 1"/>
+      <path fillRule="evenodd" clipRule="evenodd" d="M16.0002 8.4999L8.50031 1H1V8.4999L8.50031 15.9998L1 23.4997V31H8.50031L16.0002 23.4997L23.5001 31H31V23.4997L23.5001 15.9998L31 8.4999V1H23.5001L16.0002 8.4999Z" stroke="#D8D8D8" strokeWidth="1.4" strokeDasharray="1 1"/>
     </svg>    
   );
 };
