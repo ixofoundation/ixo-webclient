63428fbc9c4b5b23804c039b0fef87e8b2b20183
elite0226 <elite0226@outlook.com>
2021-08-22 11:41:01 -0400

    My portfolio assistant implement

--- src/modules/Entities/SelectedEntity/EntityExchange/Portfolio/Portfolio.container.tsx
+++ src/modules/Entities/SelectedEntity/EntityExchange/Portfolio/Portfolio.container.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleAssistant } from 'modules/Account/Account.actions'
 import AccountCard from 'pages/bond/accounts/components/ProjectAccount'
 import AccountWrapper from 'pages/bond/accounts/components/ProjectAccountWrapper'
 import AccountTransactionTable from 'modules/BondModules/BondAccountTable'
@@ -6,23 +8,41 @@ import AccountTransactionTable from 'modules/BondModules/BondAccountTable'
 // import { RootState } from 'common/redux/types'
 
 const Portfolio: React.FunctionComponent = () => {
+  const dispatch = useDispatch()
   const [selected, setSelected] = useState(0)
   // const accounts = useSelector((state: RootState) => state.projectState.accountsInfo.accounts)
   const accounts = [0, 1, 2, 3, 4, 5, 6, 7]
 
   const handleAddAccount = (e) => {
     console.log('handleAddAccount', e)
+    dispatch(
+      toggleAssistant({
+        fixed: true,
+        intent: `/add_account`
+      }),
+    )
   }
   const handleDownloadCSV = () => {
     console.log('handleDownloadCSV')
   }
   const handleNewTransaction = () => {
     console.log('handleNewTransaction')
+    dispatch(
+      toggleAssistant({
+        fixed: true,
+        intent: `/new_transaction{ "trigger":"proto_msg", "type":"cosmos-sdk/MsgSend", "denom":"uixo","wallet_address":"ixo1yq3jv9fvq9azpt6xsqlf3vnfcw32s8jtx57vjt" }`
+      }),
+    )
   }
 
   useEffect(() => {
-    console.log('accounts', accounts)
-  }, [accounts])
+    dispatch(
+      toggleAssistant({
+        fixed: true,
+        intent: `/my_portfolio{ "source":"app.ixoworld" }`
+      }),
+    )
+  }, [])
   return (
     <>
       {accounts.length > 0 && (
