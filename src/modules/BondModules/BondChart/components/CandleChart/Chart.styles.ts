5670908309e6e3ef375626574d7024d1be581547
goldenstar07 <ssinezdev07@gmail.com>
2021-08-31 09:43:45 -0700

    Completed chart implementation on price page, added buy and sell buttons

--- src/modules/BondModules/BondChart/components/CandleChart/Chart.styles.ts
+++ src/modules/BondModules/BondChart/components/CandleChart/Chart.styles.ts
@@ -19,3 +19,52 @@ export const StyledHeader = styled.h2`
   color: white;
   margin-top: 2em;
 `
+interface FilterContainerProp {
+  color: string
+  backgroundColor: string
+}
+
+export const DateFilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  > a {
+    margin-left: 0.5rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    color: #688EA0 !important;
+    background: #143F54 !important;
+  }
+`
+
+export const FilterContainer = styled.div<FilterContainerProp>`
+  margin-top: 1.8rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > div {
+    > a {
+      font-size: 0.75rem;
+      padding-top: 0.125rem;
+      padding-bottom: 0.125rem;
+      border-radius: 0.25rem;
+      margin-bottom: 0rem;
+      border-color: transparent;
+    }
+  }
+
+  > div:first-of-type {
+    > a {
+      color: ${ props => props.color } !important;
+    }
+  }
+
+  ${DateFilterContainer} {
+    > a.active {
+      color: white !important;
+      background: ${ props => props.backgroundColor } !important;
+      border-color: ${ props => props.color } !important;
+    }
+  }
+`
