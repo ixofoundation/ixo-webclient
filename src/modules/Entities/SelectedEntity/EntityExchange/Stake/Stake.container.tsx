d5178a571fedb318848d1443a50fc697b9be24a6
elite0226 <elite0226@outlook.com>
2021-08-31 14:26:16 -0400

    display inflation and APY

--- src/modules/Entities/SelectedEntity/EntityExchange/Stake/Stake.container.tsx
+++ src/modules/Entities/SelectedEntity/EntityExchange/Stake/Stake.container.tsx
@@ -80,8 +80,10 @@ const Stake: React.FunctionComponent = () => {
   const [chainList, setChainList] = useState<ExplorerEntity[]>([])
   const [selectedChain, setSelectedChain] = useState<number>(-1)
   const [totalRewards, setTotalRewards] = useState<string>('0')
-  const [inflation, setInflation] = useState<string>('0')
-  const [APY, setAPY] = useState<string>('0')
+  const [inflation, setInflation] = useState<number>(0)
+  const [supply, setSupply] = useState<number>(0)
+  const [delegated, setDelegated] = useState<number>(0)
+  const [APY, setAPY] = useState<number>(0)
 
   const mapToValidator = (fetchedData: unknown[]): ValidatorDataType[] => {
     return fetchedData
@@ -196,8 +198,63 @@ const Stake: React.FunctionComponent = () => {
       })
   }
 
+  const getInflation = (): void => {
+    Axios.get(
+      `${process.env.REACT_APP_GAIA_URL}/minting/inflation`,
+    )
+      .then(response => {
+        return response.data
+      })      
+      .then(response => {
+        const { result } = response
+        setInflation(Number(result))
+      })
+      .catch(error => {
+        console.log('Stake.container', error)
+      })
+  }
+
+  const getSupply = (): void => {
+    Axios.get(
+      `${process.env.REACT_APP_GAIA_URL}/cosmos/bank/v1beta1/supply/uixo`,
+    )
+      .then(response => {
+        return response.data
+      })      
+      .then(response => {
+        const { amount: { amount } } = response
+        setSupply(getBalanceNumber(new BigNumber(amount)))
+      })
+      .catch(error => {
+        console.log('Stake.container', error)
+      })
+  }
+
+  const getDelegated = (): void => {
+    Axios.get(
+      `${process.env.REACT_APP_GAIA_URL}/cosmos/staking/v1beta1/pool`,
+    )
+      .then(response => {
+        return response.data
+      })      
+      .then(response => {
+        const { pool } = response
+        const { 
+          bonded_tokens,
+          // not_bonded_tokens
+        } = pool
+        setDelegated(getBalanceNumber(new BigNumber(bonded_tokens)))
+      })
+      .catch(error => {
+        console.log('Stake.container', error)
+      })
+  }
+
   useEffect(() => {
     dispatch(getEntities())
+    getInflation()
+    getSupply()
+    getDelegated()
     // eslint-disable-next-line
   }, [])
 
@@ -329,6 +386,16 @@ const Stake: React.FunctionComponent = () => {
     )
   }
 
+  useEffect(() => {
+    if (supply !== 0 && delegated !== 0 && inflation !== 0) {
+      console.log('11111111')
+      console.log(inflation)
+      console.log(supply)
+      console.log(delegated)
+      setAPY((inflation * supply) / delegated)
+    }
+  }, [supply, delegated, inflation])
+
   return (
     <div className='container-fluid'>
       {selectedChain === -1 && (
@@ -359,10 +426,10 @@ const Stake: React.FunctionComponent = () => {
         <>
           <div className='row pb-4 justify-content-end align-items-center'>
             <StatsLabel className='pr-5'>
-              {`Inflation: ${inflation}%`}
+              {`Inflation: ${(inflation * 100).toFixed(0)}%`}
             </StatsLabel>
             <StatsLabel className='pr-5'>
-              {`APY: ${APY}%`}
+              {`APY: ${(APY).toFixed(1)}%`}
             </StatsLabel>
             <Button onClick={handleClaimRewards}>
               {`Claim Reward: ${thousandSeparator(totalRewards, ',')} IXO`}
