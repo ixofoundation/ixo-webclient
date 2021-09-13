ec9861c8f2f31f7886b5ef3900cf4eb67ab81335
Alain Gemenez <45015575+aliangm@users.noreply.github.com>
2021-09-14 01:42:45 +0800

    fix: Compile

--- src/modules/BondModules/bond/bond.reducer.ts
+++ src/modules/BondModules/bond/bond.reducer.ts
@@ -24,6 +24,7 @@ export const reducer = (
   switch (action.type) {
     case BondActions.GetBalancesSuccess:
       return {
+        ...state,
         ...action.payload,
         trades: state.symbol === action.payload.symbol ? [...state.trades] : [],
       }
