af218c833a539e649fa7ce4b6c2f077d55c55409
elite0226 <elite0226@outlook.com>
2021-10-08 09:17:24 -0400

    Schema git url

--- src/common/utils/constants.ts
+++ src/common/utils/constants.ts
@@ -1,1 +1,1 @@
-export const SchemaGitUrl = "https://raw.githubusercontent.com/ixofoundation/ixo-webclient/audricus-web-224/configs/launchpad/filters.json"
+export const SchemaGitUrl = "https://raw.githubusercontent.com/ixofoundation/ixo-webclient/greg-web-270/configs/launchpad/filters.json"
