4b0b663d30e0b106414501ce2744be864094f0e9
elite0226 <elite0226@outlook.com>
2021-10-24 16:40:34 -0400

    refactor staking table

--- src/common/components/Dashboard/TableCellDelegation.tsx
+++ src/common/components/Dashboard/TableCellDelegation.tsx
@@ -1,33 +1,35 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
-// import { toggleAssistant } from 'modules/Account/Account.actions'
 import AssistantIcon from 'assets/images/icon-assistant.svg'
-// import { useDispatch } from 'react-redux'
 import * as keplr from 'common/utils/keplr'
 interface DelegationProps {
-  value: number
-  moniker: string
-  validatorAddress: string
-  userDid: string
+  delegation: string
+  reward: string
 }
 
 const ValueComponentContainer = styled.div`
-  background: #143F54;
+  background: #143f54;
   display: flex;
   align-items: center;
   min-width: 200px;
 `
 
 const StyledValueContainer = styled.div`
-  padding: 1em 0;
+  padding: 0;
   display: flex;
   line-height: 100%;
   flex-grow: 2;
   justify-content: center;
   font-weight: bold;
-  white-space: pre-line;
-  img {
-    margin-right: 1em;
+  flex-direction: column;
+
+  & > span:first-child {
+    font-size: 16px;
+    line-height: 24px;
+  }
+  & > span:last-child {
+    font-size: 14px;
+    line-height: 24px;
   }
 `
 
@@ -39,35 +41,27 @@ const StyledAssistantContainer = styled.div`
   cursor: pointer;
 `
 
-const Delegation: FunctionComponent<DelegationProps> = ({ 
-  value,
-  // moniker,
-  // validatorAddress,
-  // userDid,
+const Delegation: FunctionComponent<DelegationProps> = ({
+  delegation,
+  reward,
 }) => {
-  // const dispatch = useDispatch()
-
-  const handleAssistance = async () => {
-    // dispatch(
-    //   toggleAssistant({
-    //     fixed: false,
-    //     intent: `/stake{"operator_address":"${validatorAddress}","moniker":"${moniker}","user_did":"${userDid}","trigger":"proto_msg"}`
-    //   }),
-    // )
+  const handleAssistance = async (): Promise<void> => {
     const [accounts, offlineSigner] = await keplr.connectAccount()
 
     console.log(accounts, offlineSigner)
   }
 
   return (
-  <ValueComponentContainer>
-    <StyledValueContainer>
-      {value}
-    </StyledValueContainer>
-    <StyledAssistantContainer onClick={handleAssistance}>
-      <img alt="" src={AssistantIcon} />
-    </StyledAssistantContainer>
-  </ValueComponentContainer>
-)}
+    <ValueComponentContainer>
+      <StyledValueContainer>
+        <span>{delegation}</span>
+        <span>{reward}</span>
+      </StyledValueContainer>
+      <StyledAssistantContainer onClick={handleAssistance}>
+        <img alt="" src={AssistantIcon} />
+      </StyledAssistantContainer>
+    </ValueComponentContainer>
+  )
+}
 
 export default Delegation
--- src/modules/Entities/SelectedEntity/EntityExchange/Stake/Stake.container.tsx
+++ src/modules/Entities/SelectedEntity/EntityExchange/Stake/Stake.container.tsx
@@ -11,12 +11,8 @@ import { EntityType } from 'modules/Entities/types'
 import ChainCard from 'modules/Entities/EntitiesExplorer/components/EntityCard/ChainCard/ChainCard'
 import { ExplorerEntity } from 'modules/Entities/EntitiesExplorer/types'
 import { getEntities } from 'modules/Entities/EntitiesExplorer/EntitiesExplorer.actions'
-import keysafe from 'common/keysafe/keysafe'
-import * as base58 from 'bs58'
-import { sortObject } from 'common/utils/transformationUtils'
-import * as Toast from 'common/utils/Toast'
 import { StatsLabel } from './Stake.container.styles'
-import { getValidators as getValidatorss } from '../EntityExchange.actions'
+import { getValidators } from '../EntityExchange.actions'
 interface ValidatorDataType {
   userDid: string
   validatorAddress: string
@@ -34,25 +30,25 @@ interface ValidatorDataType {
 const columns = [
   {
     Header: 'VALIDATOR',
-    accessor: 'validatorLogo',
+    accessor: 'logo',
   },
   {
     Header: 'NAME',
-    accessor: 'validatorName',
+    accessor: 'name',
     align: 'left',
   },
   {
     Header: 'MISSION',
-    accessor: 'validatorMission',
+    accessor: 'mission',
     align: 'left',
   },
   {
     Header: 'VOTING POWER',
-    accessor: 'validatorVotingPower',
+    accessor: 'votingPower',
   },
   {
     Header: 'COMMISSION',
-    accessor: 'validatorCommission',
+    accessor: 'commission',
   },
   {
     Header: 'MY DELEGATION (+REWARDS)',
@@ -62,149 +58,21 @@ const columns = [
 
 const Stake: React.FunctionComponent = () => {
   const dispatch = useDispatch()
-  const {
-    address: accountAddress,
-    // userInfo: {
-    //   didDoc: {
-    //     did: userDid
-    //   }
-    // }
-    userInfo,
-    accountNumber,
-    sequence,
-  } = useSelector((state: RootState) => state.account)
-
+  const { address: accountAddress } = useSelector(
+    (state: RootState) => state.account,
+  )
   const { entities } = useSelector((state: RootState) => state.entities)
-
-  const [validators, setValidators] = useState<ValidatorDataType[]>([])
-  const [delegations, setDelegations] = useState<string[]>([])
-  const [rewards, setRewards] = useState<string[]>([])
-  const [logos, setLogos] = useState<string[]>([])
+  const { validators } = useSelector(
+    (state: RootState) => state.selectedEntityExchange,
+  )
 
   const [chainList, setChainList] = useState<ExplorerEntity[]>([])
   const [selectedChain, setSelectedChain] = useState<number>(-1)
-  const [totalRewards, setTotalRewards] = useState<string>('0')
+
   const [inflation, setInflation] = useState<number>(0)
   const [supply, setSupply] = useState<number>(0)
   const [delegated, setDelegated] = useState<number>(0)
   const [APY, setAPY] = useState<number>(0)
-
-  const mapToValidator = (fetchedData: unknown[]): ValidatorDataType[] => {
-    return fetchedData
-      .sort((a: any, b: any) => Number(b.tokens) - Number(a.tokens))
-      .map((item: any) => ({
-        userDid: userInfo.didDoc.did,
-        validatorAddress: item.operator_address,
-        validatorLogo: item.description.moniker,
-        validatorName: {
-          text: item.description.moniker,
-          link: item.description.website,
-        },
-        validatorMission: item.description.details,
-        validatorVotingPower: thousandSeparator(
-          getBalanceNumber(new BigNumber(item.tokens)).toFixed(0),
-          ',',
-        ),
-        validatorCommission:
-          Number(item.commission.commission_rates.rate * 100).toFixed(0) + '%',
-        delegation: '0 IXO',
-      }))
-  }
-
-  const getDelegation = (
-    delegatorAddress: string,
-    validatorAddress: string,
-  ): void => {
-    Axios.get(
-      `${process.env.REACT_APP_GAIA_URL}/cosmos/staking/v1beta1/validators/${validatorAddress}/delegations/${delegatorAddress}`,
-    )
-      .then((response) => {
-        return response.data
-      })
-      .then((response) => {
-        const {
-          delegation_response: { balance },
-        } = response
-
-        setDelegations((old) => [
-          ...old,
-          getBalanceNumber(new BigNumber(balance.amount)).toFixed(0),
-        ])
-      })
-      .catch((error) => {
-        console.log('Stake.container', error)
-        setDelegations((old) => [...old, '0'])
-      })
-  }
-  const getReward = (
-    delegatorAddress: string,
-    validatorAddress: string,
-  ): void => {
-    Axios.get(
-      `${process.env.REACT_APP_GAIA_URL}/cosmos/distribution/v1beta1/delegators/${delegatorAddress}/rewards/${validatorAddress}`,
-    )
-      .then((response) => {
-        return response.data
-      })
-      .then((response) => {
-        const { rewards } = response
-
-        if (!rewards) {
-          throw 'no rewards'
-        }
-
-        setRewards((old) => [
-          ...old,
-          getBalanceNumber(new BigNumber(rewards[0].amount)).toFixed(0),
-        ])
-      })
-      .catch((error) => {
-        console.log('Stake.container', error)
-        setRewards((old) => [...old, '0'])
-      })
-  }
-  const getLogo = (identity: string): void => {
-    if (!identity) {
-      setLogos((old) => [...old, require('assets/img/relayer.png')])
-      return
-    }
-    Axios.get(
-      `https://keybase.io/_/api/1.0/user/lookup.json?key_suffix=${identity}&fields=pictures`,
-    )
-      .then((response) => {
-        return response.data
-      })
-      .then((response) => {
-        const { them } = response
-
-        setLogos((old) => [...old, them[0].pictures.primary.url])
-      })
-      .catch((error) => {
-        console.log('Stake.container', error)
-        setLogos((old) => [...old, require('assets/img/relayer.png')])
-      })
-  }
-
-  const getValidators = (): void => {
-    Axios.get(`${process.env.REACT_APP_GAIA_URL}/rest/staking/validators`)
-      .then((response) => {
-        return response.data
-      })
-      .then((response) => {
-        const { result } = response
-        setValidators(mapToValidator(result))
-        result
-          .sort((a: any, b: any) => Number(b.tokens) - Number(a.tokens))
-          .forEach((item: any) => {
-            getDelegation(accountAddress, item.operator_address)
-            getReward(accountAddress, item.operator_address)
-            getLogo(item.description.identity)
-          })
-      })
-      .catch((error) => {
-        console.log('Stake.container', error)
-      })
-  }
 
   const getInflation = (): void => {
     Axios.get(`${process.env.REACT_APP_GAIA_URL}/minting/inflation`)
@@ -261,13 +129,11 @@ const Stake: React.FunctionComponent = () => {
     getInflation()
     getSupply()
     getDelegated()
-
     // eslint-disable-next-line
   }, [])
 
   useEffect(() => {
     //  temporary placeholder
-    console.log(entities)
     if (!entities) {
       return
     }
@@ -280,7 +146,6 @@ const Stake: React.FunctionComponent = () => {
             entityCategory.tags.includes('Chain'), //  'Chain'
         ),
       )
-    console.log(filtered)
     setChainList(filtered)
   }, [entities])
 
@@ -288,112 +153,12 @@ const Stake: React.FunctionComponent = () => {
     if (!accountAddress) {
       return
     }
-    console.log(accountAddress)
-    getValidators()
-    dispatch(getValidatorss(accountAddress))
+    dispatch(getValidators(accountAddress))
     // eslint-disable-next-line
   }, [accountAddress])
 
-  useEffect(() => {
-    if (
-      delegations.length !== 0 &&
-      rewards.length !== 0 &&
-      delegations.length === validators.length &&
-      rewards.length === validators.length
-    ) {
-      setTotalRewards(
-        rewards.reduce((accumulator, currentValue) =>
-          (parseInt(accumulator) + parseInt(currentValue)).toString(),
-        ),
-      )
-      const updatedValidators = validators.map(
-        (item: ValidatorDataType, i: number) => ({
-          ...item,
-          delegation:
-            thousandSeparator(delegations[i], ',') +
-            ' IXO\n(+' +
-            thousandSeparator(rewards[i], ',') +
-            ')',
-        }),
-      )
-      setValidators(updatedValidators)
-    }
-    // eslint-disable-next-line
-  }, [delegations, rewards])
-
-  useEffect(() => {
-    if (logos.length !== 0 && logos.length === validators.length) {
-      const updatedValidators = validators.map(
-        (item: ValidatorDataType, i: number) => ({
-          ...item,
-          validatorLogo: logos[i],
-        }),
-      )
-      setValidators(updatedValidators)
-    }
-    // eslint-disable-next-line
-  }, [logos])
-
   const handleClaimRewards = (): void => {
-    const payload = {
-      msgs: [
-        {
-          type: 'cosmos-sdk/MsgWithdrawDelegationReward',
-          value: {
-            delegator_address: accountAddress,
-            validator_address:
-              'ixovaloper1z7vwqeku3sz34sd8eq4ppg9stkv8ugu97yz4jl',
-          },
-        },
-      ],
-      chain_id: process.env.REACT_APP_CHAIN_ID,
-      fee: {
-        amount: [{ amount: String(5000), denom: 'uixo' }],
-        gas: String(200000),
-      },
-      memo: '',
-      account_number: String(accountNumber),
-      sequence: String(sequence),
-    }
-    const pubKey = base58.decode(userInfo.didDoc.pubKey).toString('base64')
-
-    keysafe.requestSigning(
-      JSON.stringify(sortObject(payload)),
-      (error: any, signature: any) => {
-        !error &&
-          Axios.post(`${process.env.REACT_APP_GAIA_URL}/txs`, {
-            tx: {
-              msg: payload.msgs,
-              fee: payload.fee,
-              signatures: [
-                {
-                  account_number: payload.account_number,
-                  sequence: payload.sequence,
-                  signature: signature.signatureValue,
-                  pub_key: {
-                    type: 'tendermint/PubKeyEd25519',
-                    value: pubKey,
-                  },
-                },
-              ],
-              memo: '',
-            },
-            mode: 'sync',
-          }).then((response) => {
-            if (response.data.txhash) {
-              Toast.successToast(`Transaction Successful`)
-              if (response.data.code === 4) {
-                Toast.errorToast(`Transaction Failed`)
-              }
-              // setDelegateModalOpen(false)
-              return
-            }
-
-            Toast.errorToast(`Transaction Failed`)
-          })
-      },
-      'base64',
-    )
+    console.log('handle claim rewards')
   }
 
   useEffect(() => {
@@ -401,10 +166,6 @@ const Stake: React.FunctionComponent = () => {
       setAPY((inflation * supply) / delegated)
     }
   }, [supply, delegated, inflation])
-
-  useEffect(() => {
-    console.log('validators', validators)
-  }, [validators])
 
   return (
     <div className="container-fluid">
@@ -442,7 +203,7 @@ const Stake: React.FunctionComponent = () => {
               {`APY: ${APY.toFixed(1)}%`}
             </StatsLabel>
             <Button onClick={handleClaimRewards}>
-              {`Claim Reward: ${thousandSeparator(totalRewards, ',')} IXO`}
+              {`Claim Reward: ${thousandSeparator(1, ',')} IXO`}
             </Button>
           </div>
           <div className="row">
