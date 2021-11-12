9e929d2ed037fa7f83b045a067f3e64fbc01b184
elite0226 <elite0226@outlook.com>
2021-11-12 10:50:52 -0500

    open wallet select modal when on modify withdraw address action

--- src/common/components/ControlPanel/Actions/ModifyWithdrawAddressModal.tsx
+++ src/common/components/ControlPanel/Actions/ModifyWithdrawAddressModal.tsx
@@ -3,6 +3,13 @@ import styled from 'styled-components'
 import InputText from 'common/components/Form/InputText/InputText'
 import { FormStyles } from 'types/models'
 
+import { MsgSetWithdrawAddress } from 'cosmjs-types/cosmos/distribution/v1beta1/tx'
+import { broadCastMessage } from 'common/utils/keysafe'
+import { RootState } from 'common/redux/types'
+import { useSelector } from 'react-redux'
+import * as keplr from 'common/utils/keplr'
+import * as Toast from 'common/utils/Toast'
+
 const Container = styled.div`
   padding: 1rem 1rem;
   min-width: 32rem;
@@ -16,25 +23,118 @@ const ButtonContainer = styled.div`
   button {
     border: 1px solid #00d2ff;
     border-radius: 0.25rem;
-    height: 2.25rem;
     background: transparent;
     color: white;
     outline: none;
+    padding: 10px 20px;
+    line-height: 100%;
+
+    &:hover {
+      border: 1px solid #FFF;
+    }
   }
 `
 
 interface Props {
-  handleModifyWithdrawAddress: (address: string) => void
+  walletType: string
+  accountAddress: string
 }
 
-const ModifyWithdrawAddressModal: FunctionComponent<Props> = ({ handleModifyWithdrawAddress }) => {
-  const handleSubmit = (event): void => {
+const ModifyWithdrawAddressModal: FunctionComponent<Props> = ({
+  walletType,
+  accountAddress,
+}) => {
+  const {
+    userInfo,
+    sequence: userSequence,
+    accountNumber: userAccountNumber,
+  } = useSelector((state: RootState) => state.account)
+
+  const generateTXMessage = (type: string, withdrawAddress): object[] => {
+    const msg = []
+    switch (type) {
+      case 'keysafe': {
+        msg.push({
+          type: 'cosmos-sdk/MsgModifyWithdrawAddress',
+          value: {
+            delegator_address: accountAddress,
+            withdraw_address: withdrawAddress,
+          },
+        })
+        break
+      }
+      case 'keplr': {
+        msg.push({
+          typeUrl: '/cosmos.distribution.v1beta1.MsgSetWithdrawAddress',
+          value: MsgSetWithdrawAddress.fromPartial({
+            delegatorAddress: accountAddress,
+            withdrawAddress: withdrawAddress,
+          }),
+        })
+        break
+      }
+      default:
+        break
+    }
+    return msg
+  }
+
+  const handleSubmit = async (event): Promise<void> => {
     event.preventDefault()
+    const newWithdrawAddress = event.target.elements['withdraw_address'].value
 
-    const address = event.target.elements['withdraw_address'].value
+    const msg = generateTXMessage(walletType, newWithdrawAddress)
+    const fee = {
+      amount: [{ amount: String(5000), denom: 'uixo' }],
+      gas: String(200000),
+    }
+    const memo = ''
 
-    if (address) {
-      handleModifyWithdrawAddress(address)
+    switch (walletType) {
+      case 'keysafe': {
+        broadCastMessage(
+          userInfo,
+          userSequence,
+          userAccountNumber,
+          msg,
+          memo,
+          fee,
+          (hash) => {
+            if (hash) {
+              Toast.successToast(`Transaction Successful`)
+            } else {
+              Toast.errorToast(`Transaction Failed`)
+            }
+          },
+        )
+        break
+      }
+      case 'keplr': {
+        const [accounts, offlineSigner] = await keplr.connectAccount()
+        const address = accounts[0].address
+        const client = await keplr.initStargateClient(offlineSigner)
+
+        const payload = {
+          msg,
+          chain_id: process.env.REACT_APP_CHAIN_ID,
+          fee,
+          memo,
+        }
+
+        try {
+          const result = await keplr.sendTransaction(client, address, payload)
+          if (result) {
+            Toast.successToast(`Transaction Successful`)
+          } else {
+            throw 'transaction failed'
+          }
+        } catch (e) {
+          Toast.errorToast(`Transaction Failed`)
+        }
+        break
+      }
+      default:
+        break
     }
   }
 
