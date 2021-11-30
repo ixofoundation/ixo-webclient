bfbe647add91d472afb7cc25899b5e3ba64722a5
goldenstar07 <ssinezdev07@gmail.com>
2021-11-30 11:47:07 -0500

    fix unused variable

--- src/common/components/ControlPanel/Dashboard/Dashboard.tsx
+++ src/common/components/ControlPanel/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Axios from 'axios'
 import { Widget } from '../types'
 import { ControlPanelSection } from '../ControlPanel.styles'
