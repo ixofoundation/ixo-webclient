a9e7924cb32d93dd45a3e05b0fb0379fa0b0915d
elite0226 <elite0226@outlook.com>
2021-12-02 08:03:45 -0500

    add pagination

--- src/modules/BondModules/BondTable/index.tsx
+++ src/modules/BondModules/BondTable/index.tsx
@@ -5,6 +5,7 @@ import {
   StyledButton,
   ButtonsContainer,
 } from './PriceTable/index.style'
+import ReactPaginate from 'react-paginate'
 import Table from './PriceTable'
 import StakeTransactionTable from './StakeTransactionTable'
 import CapitalTransactionTable from './CapitalTransactionTable'
@@ -20,6 +21,7 @@ import { getBalanceNumber } from 'common/utils/currency.utils'
 import BigNumber from 'bignumber.js'
 import BuyModal from 'common/components/ControlPanel/Actions/BuyModal'
 import WalletSelectModal from 'common/components/ControlPanel/Actions/WalletSelectModal'
+import { Pagination } from 'modules/Entities/EntitiesExplorer/EntitiesExplorer.container.styles'
 
 interface Props {
   selectedHeader: string
@@ -79,9 +81,22 @@ export const BondTable: React.SFC<Props> = ({ selectedHeader }) => {
   const [selectedAddress, setSelectedAddress] = useState(null)
   const [modalTitle, setModalTitle] = useState('')
 
+  // pagination
+  const [currentItems, setCurrentItems] = useState([])
+  const [pageCount, setPageCount] = useState(0)
+  const [itemOffset, setItemOffset] = useState(0)
+  const [itemsPerPage] = useState(5)
+  const [selected, setSelected] = useState(0)
+
   const { symbol, reserveDenom } = useSelector(
     (state: RootState) => state.activeBond,
   )
+
+  const handlePageClick = (event): void => {
+    setSelected(event.selected)
+    const newOffset = (event.selected * itemsPerPage) % tableData.length
+    setItemOffset(newOffset)
+  }
 
   const handleWalletSelect = (
     walletType: string,
@@ -94,6 +109,15 @@ export const BondTable: React.SFC<Props> = ({ selectedHeader }) => {
     setBuyModalOpen(true)
     setModalTitle('Buy')
   }
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    if (tableData.length > 0) {
+      const endOffset = itemOffset + itemsPerPage
+      setCurrentItems(tableData.slice(itemOffset, endOffset))
+      setPageCount(Math.ceil(tableData.length / itemsPerPage))
+    }
+  }, [itemOffset, itemsPerPage, tableData])
 
   useEffect(() => {
     setAlphaTableData(alphaMockTableData)
@@ -204,8 +228,30 @@ export const BondTable: React.SFC<Props> = ({ selectedHeader }) => {
             </ButtonsContainer>
           </StyledHeader>
           <TableContainer>
-            <Table columns={columns} data={tableData} />
+            <Table columns={columns} data={currentItems} />
           </TableContainer>
+          <Pagination className="d-flex justify-content-center">
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel="Next"
+              forcePage={selected}
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={3}
+              pageCount={pageCount}
+              previousLabel="Previous"
+              renderOnZeroPageCount={null}
+              pageClassName="page-item"
+              pageLinkClassName="page-link"
+              previousClassName="page-item"
+              previousLinkClassName="page-link"
+              nextClassName="page-item"
+              nextLinkClassName="page-link"
+              breakClassName="page-item"
+              breakLinkClassName="page-link"
+              containerClassName="pagination"
+              activeClassName="active"
+            />
+          </Pagination>
         </Fragment>
       )}
       {selectedHeader === 'stake' && <StakeTransactionTable />}
