231ed7134fdc82a73457d7d8230a1ef379b0d414
elite0226 <elite0226@outlook.com>
2021-12-07 16:43:27 -0500

    comment out unused

--- src/common/components/Dashboard/Table.tsx
+++ src/common/components/Dashboard/Table.tsx
@@ -127,32 +127,32 @@ const renderDesktopTableRow = (row, props): any => (
   </StyledTableRow>
 )
 
-const renderMobileTableRow = (row): any => {
-  return (
-    <StyledMobileRow {...row.getRowProps()}>
-      <StyledMobileBuyCell
-        header={row.cells[1].column.id}
-        type={row.cells[1].value}
-      >
-        {renderCell(row.cells[1])}
-      </StyledMobileBuyCell>
-      <div className="d-flex text-white">
-        <StyledAmountWrapper>
-          <span className="mr-5">{renderCell(row.cells[2])}</span>
-          <span>Quantity</span>
-        </StyledAmountWrapper>
-        <StyledAmountWrapper>
-          <span>{renderCell(row.cells[3])}</span>
-          <span>Price</span>
-        </StyledAmountWrapper>
-      </div>
-      <StyledDateWrapper>
-        <span>{renderCell(row.cells[0])}</span>
-        <span>{renderCell(row.cells[4])}</span>
-      </StyledDateWrapper>
-    </StyledMobileRow>
-  )
-}
+// const renderMobileTableRow = (row): any => {
+//   return (
+//     <StyledMobileRow {...row.getRowProps()}>
+//       <StyledMobileBuyCell
+//         header={row.cells[1].column.id}
+//         type={row.cells[1].value}
+//       >
+//         {renderCell(row.cells[1])}
+//       </StyledMobileBuyCell>
+//       <div className="d-flex text-white">
+//         <StyledAmountWrapper>
+//           <span className="mr-5">{renderCell(row.cells[2])}</span>
+//           <span>Quantity</span>
+//         </StyledAmountWrapper>
+//         <StyledAmountWrapper>
+//           <span>{renderCell(row.cells[3])}</span>
+//           <span>Price</span>
+//         </StyledAmountWrapper>
+//       </div>
+//       <StyledDateWrapper>
+//         <span>{renderCell(row.cells[0])}</span>
+//         <span>{renderCell(row.cells[4])}</span>
+//       </StyledDateWrapper>
+//     </StyledMobileRow>
+//   )
+// }
 
 const Table: React.FunctionComponent<TableProps> = ({ columns, data }) => {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
