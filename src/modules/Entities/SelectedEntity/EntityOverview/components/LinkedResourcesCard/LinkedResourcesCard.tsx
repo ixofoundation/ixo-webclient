9e259df2cbb7928da6dc971bdd89f01b819ddead
elite0226 <elite0226@outlook.com>
2022-02-15 16:26:24 -0500

    display linked resources with real payload data

--- src/modules/Entities/SelectedEntity/EntityOverview/components/LinkedResourcesCard/LinkedResourcesCard.tsx
+++ src/modules/Entities/SelectedEntity/EntityOverview/components/LinkedResourcesCard/LinkedResourcesCard.tsx
@@ -16,10 +16,11 @@ import {
   Impact,
   Pdf,
   SmartContract,
-  Text,
+  // Text,
 } from 'assets/icons/LinkedResources'
 import ResourceDetailModal from './ResourceDetailModal'
 import { PageContentLinkedResources } from 'common/api/blocksync-api/types/page-content'
+import { LinkedResourceType } from 'modules/Entities/CreateEntity/CreateEntityPageContent/types'
 
 interface Props {
   linkedResources: PageContentLinkedResources[]
@@ -37,24 +38,59 @@ const LinkedResourcesCard: FunctionComponent<Props> = ({ linkedResources }) => {
     setIsModalOpened(true)
   }
 
+  const generateResourceColorAndIcon = (
+    type: LinkedResourceType,
+  ): [string, JSX.Element] => {
+    switch (type) {
+      case LinkedResourceType.ALGORITHM:
+        return ['#ED9526', <Algorithm key={1} />]
+      case LinkedResourceType.IMPACT_PROOF:
+        return ['#E2223B', <Impact key={1} />]
+      case LinkedResourceType.CREDENTIAL:
+        return ['#85AD5C', <Credential key={1} />]
+      case LinkedResourceType.IMAGE:
+        return ['#E4BC3D', <Image key={1} />]
+      case LinkedResourceType.DATA_ASSET:
+        return ['#AD245C', <Asset key={1} />]
+      case LinkedResourceType.AUTHORISATION:
+        return ['#7C2740', <Authorisation key={1} />]
+      case LinkedResourceType.PDF:
+        return ['#9F2415', <Pdf key={1} />]
+      case LinkedResourceType.CODE:
+        return ['#39C3E6', <SmartContract key={1} />]
+      default:
+        return ['#FFFFFF', <></>]
+    }
+  }
+
   return (
     <>
       <h2>Linked Resources</h2>
       <Resources>
-        <ResourceContainer>
-          <Resource
-            onClick={(): void => handleResourceClick('#9F2415', <Pdf />)}
-          >
-            <IconWrapper color="#9F2415">
-              <Pdf />
-            </IconWrapper>
-            <div>
-              <Title>This is a pdf</Title>
-              <Description>2D432</Description>
-            </div>
-          </Resource>
-        </ResourceContainer>
-        <ResourceContainer>
+        {linkedResources.map(
+          (
+            linkedResource: PageContentLinkedResources,
+            index: number,
+          ): JSX.Element => {
+            const [color, icon] = generateResourceColorAndIcon(
+              linkedResource.type,
+            )
+            return (
+              <ResourceContainer key={index}>
+                <Resource
+                  onClick={(): void => handleResourceClick(color, icon)}
+                >
+                  <IconWrapper color={color}>{icon}</IconWrapper>
+                  <div>
+                    <Title>{linkedResource.name}</Title>
+                    <Description>{linkedResource.description}</Description>
+                  </div>
+                </Resource>
+              </ResourceContainer>
+            )
+          },
+        )}
+        {/* <ResourceContainer>
           <Resource
             onClick={(): void => handleResourceClick('#E4BC3D', <Image />)}
           >
@@ -161,8 +197,9 @@ const LinkedResourcesCard: FunctionComponent<Props> = ({ linkedResources }) => {
               <Description>2D432</Description>
             </div>
           </Resource>
-        </ResourceContainer>
+        </ResourceContainer> */}
       </Resources>
+
       <ResourceDetailModal
         isOpened={isModalOpened}
         handleToggleModal={(): void => setIsModalOpened(!isModalOpened)}
