a79428c023dbeec17d2df670ba7eaea26394337b
elite0226 <elite0226@outlook.com>
2022-06-01 08:44:48 -0700

    update blockchain url

--- src/modules/relayer/relayer.selectors.ts
+++ src/modules/relayer/relayer.selectors.ts
@@ -3,16 +3,18 @@ import { createSelector } from 'reselect'
 import { CurrencyInfo, PaymentCoins, RelayerInfo } from './types'
 
 const relayerName = process.env.REACT_APP_GAIA_URL.match(
-  /^(http|https):\/\/([^ "]+)$/,
-)?.[2]
+  // /^(http|https):\/\/([^ "]+)$/,
+  /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)/,
+)[1]
 
 export const selectRelayers = (state: RootState): RelayerInfo[] =>
   state.relayers
 
 export const selectMyRelayer = createSelector(
   selectRelayers,
-  (relayers: RelayerInfo[]): RelayerInfo =>
-    relayers.find((relayer) => relayer.name === relayerName),
+  (relayers: RelayerInfo[]): RelayerInfo => {
+    return relayers.find((relayer) => relayer.name === relayerName)
+  },
 )
 
 export const selectPaymentCoins = createSelector(
