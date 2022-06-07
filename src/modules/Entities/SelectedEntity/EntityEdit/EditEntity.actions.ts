679a61fc8bb8f43c95d5260c37d501faf5c91e64
elite0226 <elite0226@outlook.com>
2022-06-07 15:04:43 -0400

    fix: edit project create public to edited cellnode url

--- src/modules/Entities/SelectedEntity/EntityEdit/EditEntity.actions.ts
+++ src/modules/Entities/SelectedEntity/EntityEdit/EditEntity.actions.ts
@@ -13,7 +13,6 @@ import { EntityType } from '../../types'
 import { RootState } from 'common/redux/types'
 import * as editEntitySelectors from './EditEntity.selectors'
 import { editEntityMap } from './strategy-map'
-import { selectCellNodeEndpoint } from '../SelectedEntity.selectors'
 
 export const goToStep = (step: number): GoToStepAction => ({
   type: EditEntityActions.GoToStep,
@@ -22,123 +21,124 @@ export const goToStep = (step: number): GoToStepAction => ({
   },
 })
 
-export const newEntity = (entityType: EntityType, forceNew = false) => (
-  dispatch: Dispatch,
-  getState: () => RootState,
-): NewEntityAction => {
-  const state = getState()
-  const { entityType: currentEntityType, created } = state.editEntity
+export const newEntity =
+  (entityType: EntityType, forceNew = false) =>
+  (dispatch: Dispatch, getState: () => RootState): NewEntityAction => {
+    const state = getState()
+    const { entityType: currentEntityType, created } = state.editEntity
 
-  if (currentEntityType === entityType && !created && !forceNew) {
-    return null
+    if (currentEntityType === entityType && !created && !forceNew) {
+      return null
+    }
+
+    return dispatch({
+      type: EditEntityActions.NewEntity,
+      payload: {
+        entityType,
+      },
+    })
   }
 
-  return dispatch({
-    type: EditEntityActions.NewEntity,
-    payload: {
-      entityType,
-    },
-  })
-}
+export const editEntity =
+  () =>
+  (
+    dispatch: Dispatch,
+    getState: () => RootState,
+  ): EditEntitySuccessAction | EditEntityFailureAction => {
+    dispatch({
+      type: EditEntityActions.EditEntityStart,
+    })
 
-export const editEntity = () => (
-  dispatch: Dispatch,
-  getState: () => RootState,
-): EditEntitySuccessAction | EditEntityFailureAction => {
-  dispatch({
-    type: EditEntityActions.EditEntityStart,
-  })
+    const state = getState()
+    const entityType = state.editEntity.entityType
+    const projectDid = state.selectedEntity.did
+    const createdOn = new Date()
+    // const createdBy = state.selectedEntity.creatorDid
+    const createdBy = state.account.userInfo.didDoc.did
+    const nodeDid = state.selectedEntity.nodeDid
 
-  const state = getState()
-  const entityType = state.editEntity.entityType
-  const projectDid = state.selectedEntity.did
-  const createdOn = new Date()
-  // const createdBy = state.selectedEntity.creatorDid
-  const createdBy = state.account.userInfo.didDoc.did
-  const nodeDid = state.selectedEntity.nodeDid
+    const cellNodeEndpoint = editEntitySelectors.selectCellNodeEndpoint(state)
 
-  const cellNodeEndpoint = selectCellNodeEndpoint(state)
+    // the page content data
+    // the page content data
+    const pageData = `data:application/json;base64,${base64Encode(
+      JSON.stringify(
+        editEntityMap[entityType].selectPageContentApiPayload(state),
+      ),
+    )}`
 
-  // the page content data
-  // the page content data
-  const pageData = `data:application/json;base64,${base64Encode(
-    JSON.stringify(
-      editEntityMap[entityType].selectPageContentApiPayload(state),
-    ),
-  )}`
+    const uploadPageContent = blocksyncApi.project.createPublic(
+      pageData,
+      cellNodeEndpoint,
+    )
 
-  const uploadPageContent = blocksyncApi.project.createPublic(
-    pageData,
-    cellNodeEndpoint,
-  )
+    Promise.all([uploadPageContent])
+      .then((responses: any[]) => {
+        // the entity data with the page content resource id
+        const pageContentId = responses[0].result
 
-  Promise.all([uploadPageContent])
-    .then((responses: any[]) => {
-      // the entity data with the page content resource id
-      const pageContentId = responses[0].result
+        const entityData = {
+          projectDid,
+          data: {
+            createdOn,
+            createdBy,
+            nodeDid,
+            ...editEntitySelectors.selectEntityApiPayload(
+              entityType,
+              pageContentId,
+            )(state),
+          },
+        }
 
-      const entityData = {
-        projectDid,
-        data: {
-          createdOn,
-          createdBy,
-          nodeDid,
-          ...editEntitySelectors.selectEntityApiPayload(
-            entityType,
-            pageContentId,
-          )(state),
-        },
-      }
-
-      keysafe.requestSigning(
-        JSON.stringify(entityData),
-        (signError: any, signature: any): any => {
-          if (signError) {
-            return dispatch({
-              type: EditEntityActions.EditEntityFailure,
-              payload: {
-                error: signError,
-              },
-            })
-          }
-          blocksyncApi.project
-            .updateProjectDoc(entityData, signature, cellNodeEndpoint)
-            .then((res: any) => {
-              if (res.error) {
-                return dispatch({
-                  type: EditEntityActions.EditEntityFailure,
-                  payload: {
-                    error: res.error.message,
-                  },
-                })
-              } else {
-                return setTimeout(() => {
-                  dispatch({
-                    type: EditEntityActions.EditEntitySuccess,
-                  })
-                }, 10000)
-              }
-            })
-            .catch((error) => {
+        keysafe.requestSigning(
+          JSON.stringify(entityData),
+          (signError: any, signature: any): any => {
+            if (signError) {
               return dispatch({
                 type: EditEntityActions.EditEntityFailure,
                 payload: {
-                  error: error.message,
+                  error: signError,
                 },
               })
-            })
-        },
-        'base64',
-      )
-    })
-    .catch((error) => {
-      return dispatch({
-        type: EditEntityActions.EditEntityFailure,
-        payload: {
-          error: error.message,
-        },
+            }
+            blocksyncApi.project
+              .updateProjectDoc(entityData, signature, cellNodeEndpoint)
+              .then((res: any) => {
+                if (res.error) {
+                  return dispatch({
+                    type: EditEntityActions.EditEntityFailure,
+                    payload: {
+                      error: res.error.message,
+                    },
+                  })
+                } else {
+                  return setTimeout(() => {
+                    dispatch({
+                      type: EditEntityActions.EditEntitySuccess,
+                    })
+                  }, 10000)
+                }
+              })
+              .catch((error) => {
+                return dispatch({
+                  type: EditEntityActions.EditEntityFailure,
+                  payload: {
+                    error: error.message,
+                  },
+                })
+              })
+          },
+          'base64',
+        )
       })
-    })
+      .catch((error) => {
+        return dispatch({
+          type: EditEntityActions.EditEntityFailure,
+          payload: {
+            error: error.message,
+          },
+        })
+      })
 
-  return null
-}
+    return null
+  }
