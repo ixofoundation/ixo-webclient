e9186bb7029d2f0ead4a0e3f6944437d8e6c0c12
elite0226 <elite0226@outlook.com>
2022-08-24 08:06:42 -0400

    fix: exchange config url hardcod

--- src/common/utils/constants.ts
+++ src/common/utils/constants.ts
@@ -4,5 +4,5 @@ export const AssetListConfigUrl =
   process.env.REACT_APP_CONFIG_ASSETLIST_URL ||
   'https://raw.githubusercontent.com/ixofoundation/ixo-webclient/feature/exchange/configs/testzone/asset-list.json'
 export const ExchangeConfigUrl =
-  process.env.REACT_APP_CONFIG_EXCHANGE_URL ||
-  'https://raw.githubusercontent.com/ixofoundation/ixo-webclient/dev/configs/testzone/exchange.json'
+  // process.env.REACT_APP_CONFIG_EXCHANGE_URL ||
+  'https://raw.githubusercontent.com/ixofoundation/ixo-webclient/feature/exchange/configs/testzone/exchange.json'
