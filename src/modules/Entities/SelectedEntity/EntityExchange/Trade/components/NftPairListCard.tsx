4d83fca2744d74f3ad7ffe8c6ea0f355da17ce1c
elite0226 <elite0226@outlook.com>
2022-09-16 17:00:36 -0400

    fix: resolve merge conflict

--- src/modules/Entities/SelectedEntity/EntityExchange/Trade/components/NftPairListCard.tsx
+++ src/modules/Entities/SelectedEntity/EntityExchange/Trade/components/NftPairListCard.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import Lottie from 'react-lottie'
 
 import assistanceAnimation from 'assets/animations/assistant/hover.json'
