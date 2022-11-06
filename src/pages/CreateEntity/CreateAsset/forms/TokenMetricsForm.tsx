e821ace629689b5f1fa97139c1b1f185921afd12
gsSuper <gregory.santini@ixo.world>
2022-11-06 16:28:06 -0500

    feat: multiple metrics

--- src/pages/CreateEntity/CreateAsset/forms/TokenMetricsForm.tsx
+++ src/pages/CreateEntity/CreateAsset/forms/TokenMetricsForm.tsx
@@ -1,5 +1,6 @@
-import { theme, Typography } from 'modules/App/App.styles'
+import { Box, theme, Typography } from 'modules/App/App.styles'
 import React from 'react'
+import { AddLink } from './TokenAttributesForm.styles'
 import {
   FormInput,
   FormMetricRow,
@@ -21,6 +22,7 @@ const TokenMetricsForm: React.FC<Props> = ({
   metrics = [{}],
   setMetrics,
 }): JSX.Element => {
+  const handlAddMetric = (): void => setMetrics([...metrics, {}])
   const handleUpdateMetric = (metricIdx: number, obj: object): void => {
     setMetrics(
       metrics.map((_, index) => {
@@ -31,33 +33,39 @@ const TokenMetricsForm: React.FC<Props> = ({
       }),
     )
   }
+  const handleRemoveAttribute = (index): void => {
+    if (metrics.length === 1) {
+      setMetrics([{}])
+    } else {
+      setMetrics(metrics.filter((_, i) => index !== i))
+    }
+  }
 
   return (
     <FormWrapper>
-      <FormRow style={{ justifyContent: 'space-between' }}>
-        <Typography
-          color={theme.ixoColor2}
-          fontWeight={400}
-          fontSize="16px"
-          lineHeight="24px"
-          style={{ margin: 'auto 10px' }}
-        >
-          Example:
-        </Typography>
-        <Typography
-          color={theme.ixoBlack}
-          fontWeight={400}
-          fontSize="16px"
-          lineHeight="24px"
-          style={{ margin: 'auto 10px' }}
-        >
-          $USD | 200 | Historical Price | (max)
-        </Typography>
-      </FormRow>
-
       {metrics.map((metric, index) => (
         <FormMetricRow key={index}>
-          <FormRow style={{ justifyContent: 'space-between' }}>
+          <FormRow>
+            <Typography
+              color={theme.ixoColor2}
+              fontWeight={400}
+              fontSize="16px"
+              lineHeight="24px"
+              style={{ margin: 'auto 10px' }}
+            >
+              Example:
+            </Typography>
+            <Typography
+              color={theme.ixoBlack}
+              fontWeight={400}
+              fontSize="16px"
+              lineHeight="24px"
+              style={{ margin: 'auto 10px' }}
+            >
+              $USD | 200 | Historical Price | (max)
+            </Typography>
+          </FormRow>
+          <FormRow>
             <FormInput
               placeholder={'Prefix'}
               inputValue={metric?.prefix}
@@ -89,8 +97,7 @@ const TokenMetricsForm: React.FC<Props> = ({
               }
             />
           </FormRow>
-
-          <FormRow style={{ justifyContent: 'space-between' }}>
+          <FormRow>
             <Typography
               color={theme.ixoColor2}
               fontWeight={400}
@@ -108,8 +115,25 @@ const TokenMetricsForm: React.FC<Props> = ({
               }
             />
           </FormRow>
+          <Box
+            className="remove"
+            onClick={(): void => handleRemoveAttribute(index)}
+          >
+            —
+          </Box>
         </FormMetricRow>
       ))}
+
+      <AddLink
+        color={theme.ixoNewBlue}
+        fontWeight={700}
+        fontSize="12px"
+        lineHeight="16px"
+        onClick={handlAddMetric}
+        style={{ marginLeft: 16 }}
+      >
+        + Add another Metric
+      </AddLink>
     </FormWrapper>
   )
 }
