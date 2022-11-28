f1fdcc288bb8646efb7d74001517025b2b78b2a4
Michael Pretorius <michaelpretorius96@gmail.com>
2022-11-28 14:18:10 +0200

    fix esbuild tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,9 @@ import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   roots: ['<rootDir>/src'],
-  // transform: {
-  //   '^.+\\.tsx?$': 'ts-jest',
-  // },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   setupFiles: ['<rootDir>/src/setupTests.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'mjs', 'cjs', 'json', 'node'],
 }
