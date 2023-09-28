f3212504537afab50e05b632ad468418c481a6f0
zach-ixo <zach@ixo.world>
2023-09-28 10:44:56 +0200

    chore: remove useeffect

--- src/pages/CurrentEntity/CurrentEntity.tsx
+++ src/pages/CurrentEntity/CurrentEntity.tsx
@@ -1,32 +1,13 @@
-import { Spinner } from 'components/Spinner/Spinner'
-import useCurrentEntity from 'hooks/currentEntity'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Redirect, Route, Switch, useParams } from 'react-router-dom'
 import DashboardPage from './Dashboard/Dashboard'
 import OverviewPage from './Overview/Overview'
 import TreasuryPage from './Treasury/Treasury'
 import ProposalOverviewPage from './Proposal/Overview'
-import { useGetEntityById } from 'graphql/entities'
 
 const CurrentEntityPage: React.FC = (): JSX.Element => {
   const { entityId } = useParams<{ entityId: string }>()
-  const { entityType, updateEntity, clearEntity } = useCurrentEntity()
 
-  const { data: entity } = useGetEntityById(entityId)
-
-  useEffect(() => {
-    if (entity) {
-      updateEntity(entity)
-    }
-
-    return () => {
-      clearEntity()
-    }
-  }, [entity])
-
-  if (!entityType) {
-    return <Spinner info='Loading Entity...' />
-  }
   return (
     <Switch>
       <Route exact path='/entity/:entityId/overview' component={OverviewPage} />
