3b3e6cc35ba4ff05b1d305f49d9c5ce6d6bc32b5
greg-ixo <114163967+gsSuper@users.noreply.github.com>
2023-10-13 08:39:22 -0400

    Revert "feat: implement multi contract batch call"

--- src/utils/dao/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './thresholdToTQData'
-export * from './getDaoContractInfo'
-export * from './getContractNameByCodeId'
-export * from './proposalMsgToActionConfig'
-export * from './getDaoContractMembersInfo'
