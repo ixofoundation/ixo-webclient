bd72890cacb230348cc2e8827dfe3722407b228b
gsSuper <gregory.santini@ixo.world>
2023-11-30 07:32:09 -0500

    feat: profile tab with dynamic data

--- src/components/ControlPanel/Account/index.tsx
+++ src/components/ControlPanel/Account/index.tsx
@@ -2,15 +2,19 @@ import { FlexBox, SvgBox } from 'components/App/App.styles'
 import { useTheme } from 'styled-components'
 import { Typography } from 'components/Typography'
 import { Avatar } from 'pages/CurrentEntity/Components'
+import CopyToClipboard from 'react-copy-to-clipboard'
 import { ReactComponent as ProfileIcon } from 'assets/images/icon-profile.svg'
 import { ReactComponent as CopyIcon } from 'assets/images/icon-copy.svg'
 import { ReactComponent as StarIcon } from 'assets/images/icon-star.svg'
 import { ReactComponent as UserAstronautIcon } from 'assets/images/icon-user-astronaut-solid.svg'
 import { ReactComponent as UserNinjaIcon } from 'assets/images/icon-user-ninja-solid.svg'
 import { Card } from '../Card'
+import { useAccount } from 'hooks/account'
+import { successToast } from 'utils/toast'
 
 const AccountCard = () => {
   const theme: any = useTheme()
+  const { name, address } = useAccount()
 
   return (
     <Card
@@ -31,10 +35,12 @@ const AccountCard = () => {
               borderRadius='8px'
               background='#F7F8F9'
             >
-              <Typography size='md'>Jane Longer Name</Typography>
-              <SvgBox color={theme.ixoNewBlue} cursor='pointer' svgWidth={6} svgHeight={6}>
-                <CopyIcon />
-              </SvgBox>
+              <Typography size='md'>{name}</Typography>
+              <CopyToClipboard text={address} onCopy={() => successToast(`Copied to clipboard`)}>
+                <SvgBox color={theme.ixoNewBlue} cursor='pointer' svgWidth={6} svgHeight={6}>
+                  <CopyIcon />
+                </SvgBox>
+              </CopyToClipboard>
             </FlexBox>
           </FlexBox>
 
