f2ba005905c60abcd719087453399a0643441f97
Zach-ixo <zach@ixo.world>
2024-02-21 01:11:30 +0200

    chore: update singleton query client

--- apps/web/src/lib/queryClient/queryClient.ts
+++ apps/web/src/lib/queryClient/queryClient.ts
@@ -1,20 +1,12 @@
-
-import { invariant } from '@apollo/client/utilities/globals';
 import { createQueryClient } from '@ixo/impactxclient-sdk';
-import { singletonAsync } from 'lib/singleton';
+import { RPC_ENDPOINT } from 'lib/protocol';
 
-async function initQueryClient() {
-  const { RPC_ENDPOINT } = process.env;
-  invariant(typeof RPC_ENDPOINT === "string", "RPC_ENDPOINT env var not set");
+let queryClientInstance: Awaited<ReturnType<typeof createQueryClient>>;
 
-  const rpcEndpoint = RPC_ENDPOINT;
-  return createQueryClient(rpcEndpoint); // No need to await here, just return the promise
+export const getQueryClient = async () => {
+  if (!queryClientInstance) {
+
+    queryClientInstance = await createQueryClient(RPC_ENDPOINT || "");
+  }
+  return queryClientInstance;
 }
-
-export const getQueryClient = singletonAsync("queryClient", initQueryClient);
-
-// Usage of queryClientPromise in an async context
-// Example: (async () => {
-//   const queryClient = await queryClientPromise;
-//   // Use queryClient here
-// })();
