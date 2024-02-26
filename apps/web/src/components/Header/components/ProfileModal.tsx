5729811fc78d24eb02cf8f67e528e5ed2f522394
Gregory Santini <gregory.santini@ixo.world>
2024-02-26 13:43:38 -0500

    fix: send funds on my portfolio

--- apps/web/src/components/Header/components/ProfileModal.tsx
+++ apps/web/src/components/Header/components/ProfileModal.tsx
@@ -1,4 +1,3 @@
-import { customQueries } from '@ixo/impactxclient-sdk'
 import { ReactComponent as AssetIcon } from 'assets/images/icon-asset.svg'
 import { ReactComponent as BondIcon } from 'assets/images/icon-bond.svg'
 import { ReactComponent as CoinsIcon } from 'assets/images/icon-coins-solid.svg'
@@ -6,97 +5,33 @@ import { ReactComponent as CopyIcon } from 'assets/images/icon-copy.svg'
 import { ReactComponent as DisconnectIcon } from 'assets/images/icon-disconnect.svg'
 import { ReactComponent as ImpactTokenIcon } from 'assets/images/icon-impact-token2.svg'
 import { ReactComponent as WalletIcon } from 'assets/images/icon-wallet-solid.svg'
-import BigNumber from 'bignumber.js'
 import { FlexBox, ScrollBox, SvgBox } from 'components/App/App.styles'
-import { BalanceCard } from 'components'
 import { Typography } from 'components/Typography'
 import { useAccount } from 'hooks/account'
 import { Avatar, TabButton } from 'pages/CurrentEntity/Components'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { truncateString } from 'utils/formatters'
 import { successToast } from 'utils/toast'
-import NativTokenViewModal from './NativeTokenViewModal'
+import NativeTokenViewModal from './NativeTokenViewModal'
 import Cw20TokenViewModal from './Cw20TokenViewModal'
-import { TokenType } from 'types/tokens'
+import { Cw20Token, NativeToken, TokenType } from 'types/tokens'
 import { useTheme } from 'styled-components'
-import { IxoCoinCodexRelayerApi } from 'hooks/configs'
+import { TokensTableColumns, TokensTableWrapper } from 'pages/MyAccount/MyPortfolioPage/BalanceView/NativeTokensCard'
+import { Table } from 'components/Table'
 
 const ProfileModal: React.FC = () => {
   const theme: any = useTheme()
   const { address, name, cw20Tokens, nativeTokens, connectedWallet, disconnect } = useAccount()
+  const tokens = [...nativeTokens, ...cw20Tokens.filter((v) => Number(v.balance) > 0)]
   const [showAssetType, setShowAssetType] = useState('Coins')
-  const [coinBalanceData, setCoinBalanceData] = useState<{
-    [denom: string]: {
-      type: TokenType
-      balance: string
-      network: string
-      coinDenom: string
-      coinMinimalDenom: string
-      coinImageUrl: string
-      coinDecimals: number
-      lastPriceUsd: number
-    }
-  }>({})
-  const [selectedDenom, setSelectedDenom] = useState<string>('')
-  const selectedAsset = useMemo(
-    () => (selectedDenom ? coinBalanceData[selectedDenom] : undefined),
-    [selectedDenom, coinBalanceData],
-  )
+  const [selectedToken, setSelectedToken] = useState<NativeToken | Cw20Token | undefined>(undefined)
 
-  useEffect(() => {
-    if (nativeTokens.length > 0) {
-      nativeTokens.forEach((token) => {
-        setCoinBalanceData((pre) => ({
-          ...pre,
-          [token.denomOrAddress]: {
-            type: TokenType.Native,
-            balance: token.balance,
-            network: 'IXO Network',
-            coinDenom: token.symbol,
-            coinMinimalDenom: token.denomOrAddress,
-            coinImageUrl: token.imageUrl!,
-            coinDecimals: token.decimals,
-            lastPriceUsd: 0,
-          },
-        }))
-        customQueries.currency
-          .findTokenInfoFromDenom(token.denomOrAddress, true, IxoCoinCodexRelayerApi)
-          .then((response) => {
-            const { lastPriceUsd } = response
-            setCoinBalanceData((pre) => ({
-              ...pre,
-              [token.denomOrAddress]: { ...pre[token.denomOrAddress], lastPriceUsd },
-            }))
-          })
-      })
-    }
-    return () => {
-      setCoinBalanceData((coinBalanceData) =>
-        Object.fromEntries(Object.entries(coinBalanceData).filter(([key, value]) => value.type !== TokenType.Native)),
-      )
-    }
-  }, [nativeTokens])
+  const handleRowClick = (state: any) => () => {
+    const original: NativeToken | Cw20Token = state.original
 
-  useEffect(() => {
-    if (cw20Tokens.length > 0) {
-      cw20Tokens.forEach((item) => {
-        setCoinBalanceData((pre) => ({
-          ...pre,
-          [item.denomOrAddress]: {
-            type: TokenType.Cw20,
-            balance: item.balance,
-            network: 'IXO Network',
-            coinDenom: item.symbol,
-            coinMinimalDenom: item.denomOrAddress,
-            coinImageUrl: item.imageUrl!,
-            coinDecimals: item.decimals,
-            lastPriceUsd: 0,
-          },
-        }))
-      })
-    }
-  }, [cw20Tokens])
+    setSelectedToken(original)
+  }
 
   return (
     <FlexBox $direction='column' $gap={8} width='100%' color={theme.ixoWhite}>
@@ -180,16 +115,30 @@ const ProfileModal: React.FC = () => {
 
         {showAssetType === 'Coins' && (
           <ScrollBox $direction='column' $gap={2} width='100%' height='250px' $overflowY='auto'>
-            {Object.values(coinBalanceData)
-              .filter((item) => new BigNumber(item.balance).isGreaterThan(new BigNumber(0)))
-              .map((item, index) => (
-                <BalanceCard key={index} {...item} onClick={() => setSelectedDenom(item.coinMinimalDenom)} />
-              ))}
-            {selectedAsset?.type === 'native' && (
-              <NativTokenViewModal open={!!selectedAsset} token={selectedAsset} onClose={() => setSelectedDenom('')} />
+            <TokensTableWrapper>
+              <Table
+                columns={TokensTableColumns}
+                data={tokens}
+                getRowProps={(state) => ({
+                  style: { height: 70, cursor: 'pointer' },
+                  onClick: handleRowClick(state),
+                })}
+                getCellProps={() => ({ style: { background: '#023044' } })}
+              />
+            </TokensTableWrapper>
+            {selectedToken?.type === TokenType.Native && (
+              <NativeTokenViewModal
+                open={!!selectedToken}
+                token={selectedToken}
+                onClose={() => setSelectedToken(undefined)}
+              />
             )}
-            {selectedAsset?.type === 'cw20' && (
-              <Cw20TokenViewModal open={!!selectedAsset} token={selectedAsset} onClose={() => setSelectedDenom('')} />
+            {selectedToken?.type === TokenType.Cw20 && (
+              <Cw20TokenViewModal
+                open={!!selectedToken}
+                token={selectedToken as Cw20Token}
+                onClose={() => setSelectedToken(undefined)}
+              />
             )}
           </ScrollBox>
         )}
