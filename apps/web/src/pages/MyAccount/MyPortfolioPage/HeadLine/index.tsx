974759eae6ed9531abb31d1de8ac11eb3f963867
Zach-ixo <zach@ixo.world>
2024-03-06 03:22:55 +0200

    chore: add did next to name in portfolio so its easier to find when needed - only in development

--- apps/web/src/pages/MyAccount/MyPortfolioPage/HeadLine/index.tsx
+++ apps/web/src/pages/MyAccount/MyPortfolioPage/HeadLine/index.tsx
@@ -10,6 +10,7 @@ import { ReactComponent as CopyIcon } from 'assets/images/icon-copy.svg'
 import { ReactComponent as DisconnectIcon } from 'assets/images/icon-disconnect.svg'
 import { SvgBox } from 'components/App/App.styles'
 import { friendlyWalletNames } from '@ixo-webclient/wallet-connector'
+import { DisplayInDevelopmentMode } from 'components'
 
 const HeadLine: React.FC = () => {
   const theme: any = useTheme()
@@ -20,6 +21,22 @@ const HeadLine: React.FC = () => {
       <Flex align={'center'} gap={16}>
         <Avatar src={null} alt='no image here' size={80} radius={100} />
         <Typography size='2xl'>{name}</Typography>
+        <DisplayInDevelopmentMode>
+          <CopyToClipboard text={connectedWallet?.did || ''}>
+            <Flex justify='center' align='center' gap={8}>
+              <Typography>{truncateString(connectedWallet?.did || '', 20, 'middle')}</Typography>
+              <SvgBox
+                $svgWidth={6}
+                $svgHeight={6}
+                color={theme.ixoDarkBlue}
+                hover={{ color: theme.ixoNewBlue }}
+                cursor='pointer'
+              >
+                <CopyIcon />
+              </SvgBox>
+            </Flex>
+          </CopyToClipboard>
+        </DisplayInDevelopmentMode>
       </Flex>
       <Flex align={'center'} gap={12}>
         <Flex
@@ -37,7 +54,7 @@ const HeadLine: React.FC = () => {
                 <img width={'100%'} height={'100%'} src={connectedWallet.wallet.imageUrl} alt='' />
               </Flex>
             )}
-            <Typography>{friendlyWalletNames(connectedWallet?.wallet.type || "")}</Typography>
+            <Typography>{friendlyWalletNames(connectedWallet?.wallet.type || '')}</Typography>
           </Flex>
           <Flex align={'center'} gap={8}>
             <Typography>{truncateString(address, 20, 'middle')}</Typography>
