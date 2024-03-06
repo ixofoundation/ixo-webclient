8908e97272aa9243b38afb86b434938475b54311
Zach-ixo <zach@ixo.world>
2024-03-06 03:21:59 +0200

    feat: create hook method to handle transfer and clean up transferEntity

--- apps/web/src/hooks/transferEntity.ts
+++ apps/web/src/hooks/transferEntity.ts
@@ -1,3 +1,4 @@
+import { customQueries, utils } from '@ixo/impactxclient-sdk'
 import { useAppDispatch, useAppSelector } from 'redux/hooks'
 import {
   updateBreadCrumbsAction,
@@ -11,9 +12,17 @@ import {
   selectTransferEntitySubtitle,
   selectTransferEntityTitle,
 } from 'redux/transferEntity/transferEntity.selectors'
+import { chainNetwork } from './configs'
+import { LinkedResource, VerificationMethod } from '@ixo/impactxclient-sdk/types/codegen/ixo/iid/v1beta1/types'
+import { LinkedResourceProofGenerator, LinkedResourceServiceEndpointGenerator } from 'utils/entities'
+import { CheckIidDoc, CreateIidDocForGroup, GetAddLinkedResourcePayload, TransferEntityMessage, UpdateEntityMessage, fee } from 'lib/protocol'
+import { useAccount } from './account'
+import { useWallet } from '@ixo-webclient/wallet-connector'
 
 export function useTransferEntityState() {
   const dispatch = useAppDispatch()
+  const { signer } = useAccount()
+  const { execute } = useWallet()
 
   const breadCrumbs: { text: string; link?: string }[] = useAppSelector(selectTransferEntityBreadCrumbs)
   const title: string = useAppSelector(selectTransferEntityTitle)
@@ -33,6 +42,86 @@ export function useTransferEntityState() {
     dispatch(updateRecipientDidAction(recipientDid))
   }
 
+  const handleTransfer = async ({ reEnableKeys, keys, entityId }: { reEnableKeys: boolean, keys: Array<VerificationMethod & { description?: string }>, entityId: string }) => {
+    const getDocumentPayload = async ({ reEnableKeys, keys, entityId }: { reEnableKeys: boolean, keys: Array<VerificationMethod & { description?: string }>, entityId: string }) => {
+      try {
+        const payload = {
+          reEnableKeys,
+          keys,
+        }
+        const buff = Buffer.from(JSON.stringify(payload))
+        const res = await customQueries.cellnode.uploadWeb3Doc(
+          utils.common.generateId(12),
+          'application/ld+json',
+          buff.toString('base64'),
+          undefined,
+          chainNetwork,
+        )
+
+        const linkedResource: LinkedResource = {
+          id: '{id}#verificationMethods',
+          type: 'VerificationMethods',
+          description: 'Verification Methods',
+          mediaType: 'application/ld+json',
+          serviceEndpoint: LinkedResourceServiceEndpointGenerator(res),
+          proof: LinkedResourceProofGenerator(res),
+          encrypted: 'false',
+          right: '',
+        }
+
+        const addLinkedResourceMessagePayload = GetAddLinkedResourcePayload(entityId, signer, linkedResource)
+        return addLinkedResourceMessagePayload.messages
+      } catch (error) {
+        console.error('getDocumentPayload', error)
+        throw error
+      }
+    }
+    const getStatusToTransferredPayload = async ({ entityId }: { entityId: string }) => {
+      try {
+        const transactionData = await UpdateEntityMessage(signer, { id: entityId, entityStatus: 2 })
+        return transactionData.messages
+      } catch (error) {
+        console.error('getStatusToTransferredPayload', error)
+        throw error
+      }
+    }
+
+    const getSigningTransferPayload = async ({ entityId, recipientDid }: { entityId: string, recipientDid: string }) => {
+      try {
+        const signingTransferMessages = []
+        if (!entityId || !recipientDid) {
+          // eslint-disable-next-line no-throw-literal
+          throw 'EntityId or RecipientDid is invalid'
+        }
+        if (!(await CheckIidDoc(recipientDid))) {
+          const createIidDocForGroupPayload = CreateIidDocForGroup(signer, recipientDid)
+          signingTransferMessages.push(...createIidDocForGroupPayload.messages)
+        }
+        const transactionData = TransferEntityMessage(signer, { id: entityId, recipientDid })
+        signingTransferMessages.push(...transactionData.messages)
+
+        return signingTransferMessages
+      } catch (error) {
+        console.error('getSigningTransferPayload', error)
+        throw error
+      }
+    }
+
+    if (reEnableKeys) {
+      const signingTransferPayload = await getSigningTransferPayload({ entityId, recipientDid })
+
+      const transferPayload = [...signingTransferPayload]
+      return await execute({ messages: transferPayload, fee: fee, memo: undefined })
+    }
+
+    const createDocumentPayload = await getDocumentPayload({ reEnableKeys, keys, entityId })
+    const updateStatusToTransferredPayload = await getStatusToTransferredPayload({ entityId })
+    const signingTransferPayload = await getSigningTransferPayload({ entityId, recipientDid })
+
+    const transferPayload = [...createDocumentPayload, ...updateStatusToTransferredPayload, ...signingTransferPayload]
+    return await execute({ messages: transferPayload, fee: fee, memo: undefined })
+  }
+
   return {
     breadCrumbs,
     title,
@@ -42,5 +131,6 @@ export function useTransferEntityState() {
     updateTitle,
     updateSubtitle,
     updateRecipientDid,
+    handleTransfer
   }
 }
