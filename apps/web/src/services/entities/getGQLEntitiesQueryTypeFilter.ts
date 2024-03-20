776094edc317ea758d6289826baa4de97c53a573
Zach-ixo <zach@ixo.world>
2024-03-20 20:43:44 +0200

    chore: map types to entities accoring to ixo terminology

--- apps/web/src/services/entities/getGQLEntitiesQueryTypeFilter.ts
+++ apps/web/src/services/entities/getGQLEntitiesQueryTypeFilter.ts
@@ -4,7 +4,13 @@ import { EntityFilter } from "generated/graphql";
 export const getGQLEntitiesQueryTypeFilter = (type: string): EntityFilter["type"] => {
     if (type === "protocol") {
         return {
-            in: ["protocol", "protocol/deed", "protocol/claim", "protocol/impact", "deed", "deed/offer"]
+            in: ["protocol", "protocol/deed", "protocol/claim"]
+        }
+    }
+
+    if (type === "oracle") {
+        return {
+            in: ["oracle", "oracle/evaluation"]
         }
     }
 
