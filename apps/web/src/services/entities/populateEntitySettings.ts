e17875abc258cbe1f1a630d4f24f6d88a4f9da48
Zach-ixo <zach@ixo.world>
2024-03-21 20:03:02 +0200

    chore: split external data fetching and batch dependent on use case

--- apps/web/src/services/entities/populateEntitySettings.ts
+++ apps/web/src/services/entities/populateEntitySettings.ts
@@ -1,5 +1,6 @@
 import { getMappedNewURL } from "@ixo-webclient/utils"
-import { serviceEndpointToUrl } from "utils/entities"
+import { Entity } from "generated/graphql"
+import { replacePDSWithCellNode, serviceEndpointToUrl } from "utils/entities"
 
 export const getEntityProfile = async (profile: any, service: any) => {
   if (!profile) return
@@ -44,20 +45,89 @@ export const getEntityProfile = async (profile: any, service: any) => {
     })
 }
 
-export const getCredentialSubject = async ({ setting, service }: { setting: any, service: any }) => {
-  if (!setting) return
-  const url = serviceEndpointToUrl(setting.serviceEndpoint, service)
+export const getCredentialSubject = async ({ resource, service }: { resource: any, service: any }) => {
+  if (!resource) return
+  const url = serviceEndpointToUrl(resource.serviceEndpoint, service)
 
   return fetch(getMappedNewURL(url))
     .then((response) => response.json())
-    .then((response) => response.credentialSubject).catch(e => e)
+    .then((response) => response.credentialSubject).catch((e) => {
+      return undefined
+    })
 }
 
 export const getTags = async ({ setting, service }: { setting: any, service: any }) => {
   if (!setting) return
   const url = serviceEndpointToUrl(setting.serviceEndpoint, service)
 
- return  fetch(getMappedNewURL(url))
-  .then((response) => response.json())
-  .then((response) => response.entityTags ?? response.ddoTags)
+  return fetch(getMappedNewURL(url))
+    .then((response) => response.json())
+    .then((response) => response.entityTags ?? response.ddoTags).catch((e) => {
+      return undefined
+    })
 }
+
+export const getPage = async ({ setting, service }: { setting: any, service: any }) => {
+  if (!setting) return
+  const url = serviceEndpointToUrl(setting.serviceEndpoint, service)
+
+  return fetch(getMappedNewURL(url))
+    .then((response) => response.json())
+    .then((response) => {
+      if ('@context' in response && 'page' in response) {
+        return { 'page': response.page }
+      }
+
+      return { 'pageLegacy': replacePDSWithCellNode(response) }
+    }
+    )
+}
+
+export const getLinkedResource = async ({ resource, service }: { resource: any, service: any }) => {
+  if (!resource) return
+  const url = serviceEndpointToUrl(resource.serviceEndpoint, service)
+
+  return fetch(getMappedNewURL(url))
+    .then((response) => response.json())
+}
+
+export const getSurveyJsResource = async ({ resource, service }: { resource: any, service: any }) => {
+  if (!resource) return
+  const url = serviceEndpointToUrl(resource.serviceEndpoint, service)
+
+  return fetch(getMappedNewURL(url))
+    .then((response) => response.json())
+    .then((response) => response.question)
+}
+
+export const getLinkedClaim = async ({ claim, service }: { claim: any, service: any }) => {
+  const url = serviceEndpointToUrl(claim.serviceEndpoint, service)
+
+  if (claim.proof && url) {
+    return fetch(getMappedNewURL(url))
+      .then((response) => response.json())
+      .then((response) => {
+        return response.entityClaims[0]
+      })
+      .then((claim) => {
+        return { [claim.id]: claim }
+      })
+      .catch(() => undefined)
+  }
+}
+
+export const populateEntityForEntityExplorer = async (entity: Entity) => {
+  const entityService = entity.service
+  const profile = getEntityProfile(entity.settings?.Profile, entityService)
+  const tags = getTags({ setting: entity.settings?.Tags, service: entityService })
+  const [profileResult, tagsResult] = await Promise.all([profile, tags])
+
+  return { profile: profileResult, tags: tagsResult }
+}
+
+export const populateEntitiesForEntityExplorer = async (entities: Entity[]) => {
+  const populatedEntities = await Promise.all(entities.map(async entity => ({ ...entity, ...(await populateEntityForEntityExplorer(entity)) })))
+
+  return populatedEntities
+}
+
