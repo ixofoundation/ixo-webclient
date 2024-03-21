5f4ccc15ab2c27d6c7d07d9574be84bce0198255
Zach-ixo <zach@ixo.world>
2024-03-21 10:52:58 +0200

    chore: split queries when transforming entity

--- apps/web/src/services/entities/populateEntitySettings.ts
+++ apps/web/src/services/entities/populateEntitySettings.ts
@@ -1,10 +1,10 @@
 import { getMappedNewURL } from "@ixo-webclient/utils"
 import { serviceEndpointToUrl } from "utils/entities"
 
-export const populateEntityProfile = async (profile: any, service: any) => {
-    if(!profile) return
-    const url = serviceEndpointToUrl(profile.serviceEndpoint, service)
-    return await fetch(getMappedNewURL(url))
+export const getEntityProfile = async (profile: any, service: any) => {
+  if (!profile) return
+  const url = serviceEndpointToUrl(profile.serviceEndpoint, service)
+  return await fetch(getMappedNewURL(url))
     .then((response) => response.json())
     .then((response) => {
       const context = response['@context']
@@ -14,27 +14,27 @@ export const populateEntityProfile = async (profile: any, service: any) => {
       if (image && !image.startsWith('http')) {
         const [identifier] = image.split(':')
         let endpoint = ''
-        ;(Array.isArray(context)
-          ? context
-          : Object.entries(context).map(([key, value]) => ({ [key]: value }))
-        ).forEach((item: any) => {
-          if (typeof item === 'object' && identifier in item) {
-            endpoint = item[identifier]
-          }
-        })
+          ; (Array.isArray(context)
+            ? context
+            : Object.entries(context).map(([key, value]) => ({ [key]: value }))
+          ).forEach((item: any) => {
+            if (typeof item === 'object' && identifier in item) {
+              endpoint = item[identifier]
+            }
+          })
         image = image.replace(identifier + ':', endpoint)
       }
       if (logo && !logo.startsWith('http')) {
         const [identifier] = logo.split(':')
         let endpoint = ''
-        ;(Array.isArray(context)
-          ? context
-          : Object.entries(context).map(([key, value]) => ({ [key]: value }))
-        ).forEach((item: any) => {
-          if (typeof item === 'object' && identifier in item) {
-            endpoint = item[identifier]
-          }
-        })
+          ; (Array.isArray(context)
+            ? context
+            : Object.entries(context).map(([key, value]) => ({ [key]: value }))
+          ).forEach((item: any) => {
+            if (typeof item === 'object' && identifier in item) {
+              endpoint = item[identifier]
+            }
+          })
         logo = logo.replace(identifier + ':', endpoint)
       }
       return { ...response, image, logo }
@@ -42,4 +42,22 @@ export const populateEntityProfile = async (profile: any, service: any) => {
     .catch((e) => {
       return undefined
     })
+}
+
+export const getCredentialSubject = async ({ setting, service }: { setting: any, service: any }) => {
+  if (!setting) return
+  const url = serviceEndpointToUrl(setting.serviceEndpoint, service)
+
+  return fetch(getMappedNewURL(url))
+    .then((response) => response.json())
+    .then((response) => response.credentialSubject).catch(e => e)
+}
+
+export const getTags = async ({ setting, service }: { setting: any, service: any }) => {
+  if (!setting) return
+  const url = serviceEndpointToUrl(setting.serviceEndpoint, service)
+
+ return  fetch(getMappedNewURL(url))
+  .then((response) => response.json())
+  .then((response) => response.entityTags ?? response.ddoTags)
 }
