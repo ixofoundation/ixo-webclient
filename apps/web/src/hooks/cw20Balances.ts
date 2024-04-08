a32619cb702c60bbf0e12b911dbb5b0da926a94c
Zach-ixo <zach@ixo.world>
2024-04-08 22:13:34 +0200

    chore: refactor cw20Balances hook to make 2 RPC request instead of stakingGroups length * 4

--- apps/web/src/hooks/cw20Balances.ts
+++ apps/web/src/hooks/cw20Balances.ts
@@ -1,90 +1,138 @@
 import { useCallback } from 'react'
 import { selectStakingGroups } from 'redux/entitiesExplorer/entitiesExplorer.selectors'
-import { useAppSelector } from 'redux/hooks'
-import { contracts } from '@ixo/impactxclient-sdk'
+import { useAppDispatch, useAppSelector } from 'redux/hooks'
 import { claimAvailable } from 'utils/tokenClaim';
 import { plus } from 'utils/currency';
 import { convertMicroDenomToDenomWithDecimals } from 'utils/conversions';
-import { Cw20Token, TokenType } from 'types/tokens';
+import { TokenType } from 'types/tokens';
 import { TDAOGroupModel } from 'types/entities';
 import { CosmWasmClient } from '@ixo/impactxclient-sdk/node_modules/@cosmjs/cosmwasm-stargate'
 import { useAccount } from './account';
 import { DaoVotingCw20StakedQueryClient } from 'adapters/DaoVotingCw20StakedAdapter';
 import { queryMultipleContracts } from 'utils/multiContractCall';
+import { getCosmwasmClient } from 'lib/cosmWasmClient/cosmWasmClient';
+import { RPC_ENDPOINT } from 'lib/protocol';
+import { updateCw20TokensAction } from 'redux/account/account.actions';
 
-type ProcessStakingGroupProps = {
-    stakingGroup: TDAOGroupModel,
-    cwClient: CosmWasmClient,
-    address: string,
-    updateCw20Tokens: (balances: { [addr: string]: Cw20Token }) => void
-}
-async function processStakingGroup({ stakingGroup, cwClient, updateCw20Tokens, address }: ProcessStakingGroupProps) {
-    const {
-        token,
-        coreAddress,
-        votingModule: { votingModuleAddress },
-    } = stakingGroup;
+const getStakingAndTokenContracts = async (stakingGroups: TDAOGroupModel[], cwClient: CosmWasmClient, address: string) => {
+    const response = stakingGroups.map(stakingGroup => {
+        const {
+            votingModule: { votingModuleAddress },
+        } = stakingGroup;
 
-    if (!token) return;
 
-    const daoVotingCw20StakedClient = new DaoVotingCw20StakedQueryClient(
-        cwClient as any,
-        votingModuleAddress
-    );
+        const daoVotingCw20StakedClient = new DaoVotingCw20StakedQueryClient(
+            cwClient as any,
+            votingModuleAddress
+        );
 
-    const [stakingContract, tokenContract] = await queryMultipleContracts([daoVotingCw20StakedClient.stakingContractRequest(), daoVotingCw20StakedClient.tokenContractRequest()])
+        return [daoVotingCw20StakedClient.stakingContractRequest(), daoVotingCw20StakedClient.tokenContractRequest()]
+    })
 
-    const cw20StakeClient = new contracts.Cw20Stake.Cw20StakeQueryClient(cwClient, stakingContract);
-    const cw20BaseClient = new contracts.Cw20Base.Cw20BaseQueryClient(cwClient, tokenContract);
+    const stakingAndTokenContractAddress = await queryMultipleContracts(response.flat())
 
-    const { value: microStakedValue } = await cw20StakeClient.stakedValue({ address });
-    const { claims } = await cw20StakeClient.claims({ address });
+    const groupedAddresses: string[][] = stakingAndTokenContractAddress.reduce((acc: string[][], curr: string, index: number, array: string[]) => {
+        if (index % 2 === 0) acc.push(array.slice(index, index + 2));
+        return acc;
+    }, []);
 
-    const microUnstakingValue = claims
-        .filter((claim) => !claimAvailable(claim, 0)) //  TODO: TBD blockHeight
-        .reduce((acc, cur) => plus(acc, cur.amount), '0');
+    const balances = groupedAddresses.map((stakingGroupAddresses) => {
+        const [stakingAddress, tokenAddress] = stakingGroupAddresses
 
-    const microClaimableValue = claims
-        .filter((claim) => claimAvailable(claim, 0)) //  TODO: TBD blockHeight
-        .reduce((acc, cur) => plus(acc, cur.amount), '0');
+        const stakedValue = {
+            address: stakingAddress,
+            data: {
+                staked_value: {
+                    address
+                }
+            }
+        }
 
-    const { balance: microBalance } = await cw20BaseClient.balance({ address });
+        const claims = {
+            address: stakingAddress,
+            data: {
+                claims: {
+                    address
+                }
+            }
+        }
 
-    const [balance, stakedValue, unstakingValue, claimableValue] = await Promise.all([
-        convertMicroDenomToDenomWithDecimals(microBalance, token.tokenInfo.decimals),
-        convertMicroDenomToDenomWithDecimals(microStakedValue, token.tokenInfo.decimals),
-        convertMicroDenomToDenomWithDecimals(microUnstakingValue, token.tokenInfo.decimals),
-        convertMicroDenomToDenomWithDecimals(microClaimableValue, token.tokenInfo.decimals),
-    ]);
+        const balance = {
+            address: tokenAddress,
+            data: {
+                balance: {
+                    address
+                }
+            }
+        }
 
-    const payload = {
-        groupContract: coreAddress,
-        type: TokenType.Cw20,
-        symbol: token.tokenInfo.symbol,
-        denomOrAddress: token.config.token_address,
-        imageUrl: (token.marketingInfo?.logo !== 'embedded' && token.marketingInfo.logo?.url) || '',
-        decimals: token.tokenInfo.decimals,
-        balance: balance.toString(),
-        staked: stakedValue.toString(),
-        unbonding: unstakingValue.toString(),
-        claimable: claimableValue.toString(),
-    };
+        return [stakedValue, claims, balance]
+    })
 
-    updateCw20Tokens({ [payload.denomOrAddress]: payload });
+    const balancesResponse = await queryMultipleContracts(balances.flat())
+
+    const balancesResult = balancesResponse.reduce((acc: string[][], curr: string, index: number, array: string[]) => {
+        if (index % 3 === 0) acc.push(array.slice(index, index + 3));
+        return acc;
+    }, []);
+
+    const utimateResult = stakingGroups.map((stakingGroup, index: any) => {
+        const {
+            token,
+            coreAddress,
+        } = stakingGroup;
+
+        if (!token) return undefined
+
+        const [microStakedResponse, claimsResponse, microBalanceResponse] = balancesResult[index]
+
+        const microUnstakingValue = claimsResponse.claims
+            .filter((claim: any) => !claimAvailable(claim, 0)) //  TODO: TBD blockHeight
+            .reduce((acc: any, cur: any) => plus(acc, cur.amount), '0');
+
+        const microClaimableValue = claimsResponse.claims
+            .filter((claim: any) => claimAvailable(claim, 0)) //  TODO: TBD blockHeight
+            .reduce((acc: any, cur: any) => plus(acc, cur.amount), '0');
+
+        const balance = convertMicroDenomToDenomWithDecimals(microBalanceResponse.balance, token.tokenInfo.decimals)
+        const stakedValue = convertMicroDenomToDenomWithDecimals(microStakedResponse.balance, token.tokenInfo.decimals)
+        const unstakingValue = convertMicroDenomToDenomWithDecimals(microUnstakingValue, token.tokenInfo.decimals)
+        const claimableValue = convertMicroDenomToDenomWithDecimals(microClaimableValue, token.tokenInfo.decimals)
+
+        return {
+            groupContract: coreAddress,
+            type: TokenType.Cw20,
+            symbol: token.tokenInfo.symbol,
+            denomOrAddress: token.config.token_address,
+            imageUrl: (token.marketingInfo?.logo !== 'embedded' && token.marketingInfo.logo?.url) || '',
+            decimals: token.tokenInfo.decimals,
+            balance: balance.toString(),
+            staked: stakedValue.toString(),
+            unbonding: unstakingValue.toString(),
+            claimable: claimableValue.toString(),
+        };
+    })
+
+    return utimateResult.filter(element => element !== undefined).reduce((acc, cur) => {
+       acc[cur?.denomOrAddress as any] = cur
+       return acc
+    }, {})
 }
 
 
 export function useUpdate20Balances() {
     const stakingGroups = useAppSelector(selectStakingGroups)
+    const dispatch = useAppDispatch()
     const {
         address,
-        cwClient,
-        updateCw20Tokens,
     } = useAccount()
 
     const update = useCallback(async () => {
-        await Promise.all(stakingGroups.map((stakingGroup) => processStakingGroup({ stakingGroup, cwClient, address, updateCw20Tokens })))
-    }, [address, cwClient, stakingGroups, updateCw20Tokens])
+        const cwClient = await getCosmwasmClient(RPC_ENDPOINT ?? "")
+        const response = await getStakingAndTokenContracts(stakingGroups, cwClient, address)
+        dispatch(updateCw20TokensAction(response))
+
+    }, [address, dispatch, stakingGroups])
 
     return { update }
 }
