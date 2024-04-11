cbfa4e01d3e295f602d0e02c34e3ee40752cc8f2
Zach-ixo <zach@ixo.world>
2024-04-11 23:27:58 +0200

    chore: add styling option to get EntityIcon util

--- apps/web/src/utils/getEntityIcon.tsx
+++ apps/web/src/utils/getEntityIcon.tsx
@@ -1,21 +1,28 @@
-import { ReactComponent as DAOIcon } from 'assets/images/icon-dao.svg'
 import { ReactComponent as ProjectIcon } from 'assets/images/icon-project.svg'
 import { ReactComponent as InvestmentIcon } from 'assets/images/icon-investment.svg'
 import { ReactComponent as OracleIcon } from 'assets/images/icon-oracle.svg'
 import { ReactComponent as AssetIcon } from 'assets/images/icon-asset.svg'
+import DaoIcon from 'assets/icons/DaoIcon'
 
-export const getEntityIcon = (type: string | undefined): React.ReactNode | JSX.Element | null => {
+interface Style {
+  color?: string
+  fill?: string
+  stroke?: string
+}
+
+export const getEntityIcon = (type: string | undefined, style?: Style): React.ReactNode | JSX.Element | null => {
+  const svgStyle = style || {}
   switch (type) {
     case 'asset/device':
-      return <AssetIcon transform='scale(.8)' />
+      return <AssetIcon transform='scale(.8)' {...svgStyle} />
     case 'dao':
-      return <DAOIcon transform='scale(.8)' />
+      return <DaoIcon transform='scale(.8)'  {...svgStyle} />
     case 'project':
-      return <ProjectIcon transform='scale(.8)' />
+      return <ProjectIcon transform='scale(.8)' {...svgStyle} />
     case 'investment':
-      return <InvestmentIcon transform='scale(.8)' />
+      return <InvestmentIcon transform='scale(.8)' {...svgStyle} />
     case 'oracle':
-      return <OracleIcon transform='scale(.8)' />
+      return <OracleIcon transform='scale(.8)' {...svgStyle} />
     default:
       return null
   }
