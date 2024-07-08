85b57ed1b3f890e72b9fc451b3786681de041638
yousefhany77 <yousefhany2014@gmail.com>
2024-07-08 22:09:38 +0300

    Add end claim collection

--- apps/web/src/components/ControlPanel/ActionPanel/ClaimPanel/ClaimActions.tsx
+++ apps/web/src/components/ControlPanel/ActionPanel/ClaimPanel/ClaimActions.tsx
@@ -1,15 +1,21 @@
-import { Box, Button, ButtonProps, Text } from '@mantine/core'
-import { FC } from 'react'
+import { useWallet } from '@ixo-webclient/wallet-connector'
+import { ixo } from '@ixo/impactxclient-sdk'
+import { Box, Button, Text } from '@mantine/core'
+import { useCurrentEntityAdminAccount } from 'hooks/currentEntity'
+import { fee } from 'lib/protocol'
+import { FC, useCallback, useState } from 'react'
+import { ButtonProps } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom'
+import { getEntityById } from 'redux/entities/entities.selectors'
+import { useAppSelector } from 'redux/hooks'
+import { errorToast, successToast } from 'utils/toast'
 
-const ActionButton: FC<
-  ButtonProps & {
-    renderRoot?: (props: any) => JSX.Element
-  }
-> = (props) => <Button w='100%' radius={4} size='md' mt='md' {...props} />
-
-const ClaimActions: FC = () => {
+interface ClaimActionsProps {
+  collectionId: string
+}
+const ClaimActions: FC<ClaimActionsProps> = ({ collectionId }) => {
   const { entityId } = useParams<{ entityId: string }>()
+
   return (
     <Box mt={15} bg='#fff' p={20} style={{ borderRadius: 12 }}>
       <Text size='12px'>Claim Admin</Text>
@@ -18,9 +24,70 @@ const ClaimActions: FC = () => {
       </ActionButton>
       <ActionButton disabled>Manage Payments</ActionButton>
       <ActionButton disabled>Update Collection</ActionButton>
-      <ActionButton disabled>End Claim Collection</ActionButton>
+      <EndClaimCollectionButton collectionId={collectionId} />
     </Box>
   )
 }
 
 export default ClaimActions
+
+const EndClaimCollectionButton: FC<{
+  collectionId: string
+}> = ({ collectionId }) => {
+  const { execute, close } = useWallet()
+  const [mutationState, setMutationState] = useState<'idle' | 'pending' | 'success' | 'error'>('idle')
+  const { entityId } = useParams<{ entityId: string }>()
+  const { accounts } = useAppSelector(getEntityById(entityId ?? ''))
+
+  const adminAddress = useCurrentEntityAdminAccount(accounts)
+
+  const handleEndClaimCollection = useCallback(async () => {
+    try {
+      setMutationState('pending')
+      await execute({
+        data: {
+          messages: [
+            {
+              typeUrl: '/ixo.claims.v1beta1.MsgUpdateCollectionState',
+              value: ixo.claims.v1beta1.MsgUpdateCollectionState.fromPartial({
+                adminAddress: adminAddress ?? '',
+                state: ixo.claims.v1beta1.CollectionState.CLOSED,
+                collectionId,
+              }),
+            },
+          ],
+          fee,
+          memo: undefined,
+        },
+      })
+      successToast('Success', 'Claim collection ended successfully')
+    } catch (error) {
+      setMutationState('error')
+      let message = 'Failed to end claim collection'
+      if (error instanceof Error) {
+        message = error.message
+      }
+      errorToast('Failed', message, {
+        type: 'error',
+      })
+    } finally {
+      setMutationState('idle')
+      close()
+    }
+  }, [adminAddress, close, collectionId, execute])
+  return (
+    <ActionButton
+      disabled={mutationState !== 'idle' || !adminAddress}
+      loading={mutationState === 'pending'}
+      onClick={handleEndClaimCollection}
+    >
+      End Claim Collection
+    </ActionButton>
+  )
+}
+
+interface ActionButtonProps extends ButtonProps, React.ComponentPropsWithoutRef<'button'> {
+  renderRoot?: (props: any) => JSX.Element
+  loading?: boolean
+}
+const ActionButton: FC<ActionButtonProps> = (props) => <Button w='100%' radius={4} size='md' mt='md' {...props} />
--- apps/web/src/components/ControlPanel/ActionPanel/ClaimPanel/ClaimPanel.tsx
+++ apps/web/src/components/ControlPanel/ActionPanel/ClaimPanel/ClaimPanel.tsx
@@ -1,25 +1,27 @@
 import { Flex, Text } from '@mantine/core'
 import { ReactComponent as ClaimIcon } from 'assets/images/icon-claim.svg'
 import { SvgBox } from 'components/App/App.styles'
+import { useIsEntityOwner } from 'hooks/claim'
+import React from 'react'
+import { useParams } from 'react-router-dom'
 import { selectEntityConfig } from 'redux/configs/configs.selectors'
 import { useAppSelector } from 'redux/hooks'
 import { useTheme } from 'styled-components'
 import ApplicationSubmissionCard from './ApplicationSubmissionCard'
-import ClaimActions from './ClaimActions'
+
 import WithdrawEarningsCard from './WithdrawEarningsCard'
-import { useIsEntityOwner } from 'hooks/claim'
-import { useParams } from 'react-router-dom'
+const ClaimActions = React.lazy(() => import('./ClaimActions'))
 
 const ClaimPanel = ({ data }: { data: any }) => {
   const theme = useTheme()
   const config = useAppSelector(selectEntityConfig)
   const primaryColor = config.theme.primaryColor ?? theme.ixoNewBlue
-   const { entityId = '' } = useParams<{ entityId: string }>()
+  const { entityId = '' } = useParams<{ entityId: string }>()
 
   const isEntityOwner = useIsEntityOwner({
-   collectionId:data?.collection?.id ,
-   entityId,
- })
+    collectionId: data?.collection?.id,
+    entityId,
+  })
   return (
     <Flex style={{ borderRadius: 12 }} direction={'column'}>
       <Flex align={'center'}>
@@ -31,7 +33,7 @@ const ClaimPanel = ({ data }: { data: any }) => {
         </Text>
       </Flex>
       <ApplicationSubmissionCard data={data} />
-      {isEntityOwner && <ClaimActions />}
+      {isEntityOwner && data.collection?.id && <ClaimActions collectionId={data.collection?.id} />}
       <WithdrawEarningsCard data={data} />
     </Flex>
   )
