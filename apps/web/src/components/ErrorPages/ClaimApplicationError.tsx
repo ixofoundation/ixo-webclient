ceba846b4a5d3e1ccb8370514b41e3a31fc31035
Zac <zach@ixo.world>
2024-07-09 02:15:14 +0200

    chore: reset side panel on back

--- apps/web/src/components/ErrorPages/ClaimApplicationError.tsx
+++ apps/web/src/components/ErrorPages/ClaimApplicationError.tsx
@@ -1,12 +1,15 @@
 import { Button, Flex, Text } from '@mantine/core'
+import { useKeyValueViewerContext } from 'contexts/KeyValueViewerContext'
 import { LiaArrowLeftSolid } from 'react-icons/lia'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 const ClaimApplicationError = () => {
   const navigate = useNavigate()
   const { pathname } = useLocation()
+  const { resetKeyValue } = useKeyValueViewerContext()
 
   const handleBack = () => {
+    resetKeyValue()
     const search = new URLSearchParams()
     search.delete('collectionId')
     search.delete('agentRole')
