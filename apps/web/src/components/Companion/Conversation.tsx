1ecf852491ae3d20745d43be079505d439901b2c
yousefhany77 <yousefhany2014@gmail.com>
2024-07-22 21:45:06 +0300

    chore: Refactor Companion component and fix NavigationTabs bugs

--- apps/web/src/components/Companion/Conversation.tsx
+++ apps/web/src/components/Companion/Conversation.tsx
@@ -1,50 +1,49 @@
-import { Box, Flex, ScrollArea } from '@mantine/core';
-import { useCompanion } from 'hooks/useCompanion';
-import { ReactComponent as AssistantIcon } from 'assets/images/icon-assistant.svg';
-import Markdown from 'react-markdown';
-import { useEffect } from 'react';
-import { MessageBox } from './MessageBox';
+import { Box, Flex, ScrollArea } from '@mantine/core'
+import { ReactComponent as AssistantIcon } from 'assets/images/icon-assistant.svg'
+import { useCompanion } from 'hooks/useCompanion'
+import { useEffect } from 'react'
+import Markdown from 'react-markdown'
+import { MessageBox } from './MessageBox'
 
 const Conversation = () => {
-  const { messages, newChat, assistant, sendMessage } = useCompanion();
+  const { messages, newChat, assistant, sendMessage } = useCompanion()
 
   useEffect(() => {
-    if(assistant){
-      newChat();
+    if (assistant) {
+      newChat()
     }
-  }, [assistant, newChat]);
-
+  }, [assistant, newChat])
 
   return (
-    <Flex bg="black" h="100%" w="100%" align="center" direction="column">
-      <ScrollArea style={{ height: '100%', width: '100%', flex: 1, }}>
-        <Flex w="100%" mt={0}  direction="column" align="center">
-          <Flex w="750px" mt={150} direction="column">
+    <Flex bg='black' h='100%' w='100%' align='center' direction='column'>
+      <ScrollArea style={{ height: '100%', width: '100%', flex: 1 }}>
+        <Flex w='100%' mt={0} direction='column' align='center'>
+          <Flex w='750px' mt={150} direction='column'>
             {messages.map((message) => {
               if (message.role === 'assistant') {
                 return (
-                  <Flex key={message.content} align="flex-start" justify="flex-start">
+                  <Flex key={message.content} align='flex-start' justify='flex-start'>
                     <Box mt={10} mr={10}>
-                      <AssistantIcon fill="white" stroke="white" />
+                      <AssistantIcon fill='white' stroke='white' />
                     </Box>
-                    <Flex mt={8} direction="column">
+                    <Flex mt={8} direction='column'>
                       <Markdown>{message.content}</Markdown>
                     </Flex>
                   </Flex>
-                );
+                )
               }
               return (
-                <Flex key={message.content} justify="flex-end">
+                <Flex key={message.content} justify='flex-end'>
                   {message.content}
                 </Flex>
-              );
+              )
             })}
           </Flex>
         </Flex>
       </ScrollArea>
-      <MessageBox sendMessage={sendMessage}/>
+      <MessageBox sendMessage={sendMessage} />
     </Flex>
-  );
-};
+  )
+}
 
-export default Conversation;
+export default Conversation
