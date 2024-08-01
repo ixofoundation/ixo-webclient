32d84cd988a328f95b1aa113fff92c9106efc36b
Zac <zach@ixo.world>
2024-08-01 08:28:00 +0200

    chore: add marketplace title

--- apps/web/src/components/Layout/ExploreLayout/ExploreLayout.tsx
+++ apps/web/src/components/Layout/ExploreLayout/ExploreLayout.tsx
@@ -2,15 +2,19 @@ import { Box, Flex, ScrollArea, Text, TextInput, rem } from '@mantine/core'
 import { Outlet } from 'react-router-dom'
 import { ReactComponent as AssistantIcon } from 'assets/images/icon-assistant.svg'
 import { useRequestsContext } from 'contexts/RequestsContext'
+import { useSelector } from 'react-redux'
+import { selectEntityHeadUIConfig } from 'redux/entities/entities.selectors'
 
 const ExploreLayout = () => {
+  const headConfig = useSelector(selectEntityHeadUIConfig)
+  const title = headConfig?.title
   const { setSearchString } = useRequestsContext()
   return (
     <Flex w='100%' h='calc(-74px + 100vh)' direction={'column'}>
       <Flex w='100%' h={200} align={'center'} bg='linear-gradient(135deg, #05324C 0%, #149FBD 100%)' pos='relative'>
         <Box w='90%' mx='auto'>
           <Text size={rem(30)} c='white'>
-            MarketPlace Name
+            {title}
           </Text>
           <Text size={rem(20)} c='white' mt={10}>
             Requests
