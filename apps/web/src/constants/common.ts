00a522904f976f58df927ed894c72f84855ce936
Zac <zach@ixo.world>
2024-08-15 01:07:55 +0200

    chore: export chain id from common constants

--- apps/web/src/constants/common.ts
+++ apps/web/src/constants/common.ts
@@ -1,2 +1,3 @@
 export const isDevelopment = !process.env.NODE_ENV || process.env.REACT_APP_CHAIN_ID === 'devnet-1'
 export const currentRelayerNode = process.env.REACT_APP_RELAYER_NODE
+export const currentChainId = process.env.REACT_APP_CHAIN_ID
