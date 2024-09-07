90b47409168bb69a74d43f7b0833cf98825b39a2
yousefhany77 <yousefhany2014@gmail.com>
2024-09-07 16:59:20 +0300

    Fix enity flow

---
+++ apps/web/src/hooks/algolia/insert-to-algolia-index.ts
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { algoliaIndexName } from 'constants/common'
+import { SearchResult } from 'pages/ExploreNew/ExploreNew'
+import { TEntityModel } from 'types/entities'
+import { errorToast } from 'utils/toast'
+
+export const insertToAlgoliaIndex: (entity: TEntityModel) => Promise<void> = async (entity) => {
+  try {
+    const record = convertEntityToAlgoliaObject(entity)
+
+    return await axios.post(
+      'https://algolia-worker.ixo-api.workers.dev',
+      {
+        indexName: algoliaIndexName,
+        record,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-PLATFORM-NAME': 'ixo-web-client',
+        },
+      },
+    )
+  } catch (error) {
+    console.error('Error inserting to Algolia index', error)
+    errorToast('Error inserting to Algolia index')
+  }
+}
+
+const convertEntityToAlgoliaObject = (entity: TEntityModel): SearchResult => ({
+  objectID: entity.id,
+  id: entity.id,
+  brand: entity?.profile?.brand || '',
+  name: entity?.profile?.name || '',
+  image: entity?.profile?.image || '',
+  logo: entity?.profile?.logo || '',
+  description: entity?.profile?.description || '',
+  filter: `${entity.type}: ${entity?.profile?.name}`,
+  type: entity.type,
+  entityVerified: entity.entityVerified,
+  startDate: entity?.startDate ? new Date(entity.startDate as any).toISOString() : '',
+  endDate: entity?.endDate ? new Date(entity.endDate as any).toISOString() : '',
+  owner: entity.owner,
+  relayerNode: entity.relayerNode,
+  alsoKnownAs: entity.alsoKnownAs,
+})
